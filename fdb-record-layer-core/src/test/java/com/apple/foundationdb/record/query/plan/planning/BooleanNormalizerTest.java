359866b5d4c497b38f2e29edc94f5d65a973df3d
Mike McMahon <mike_mcmahon@apple.com>
2020-07-15 13:30:51 -0700

    Resolves #993: BooleanNormalizer could eliminate some redundant disjuncts

--- fdb-record-layer-core/src/test/java/com/apple/foundationdb/record/query/plan/planning/BooleanNormalizerTest.java
+++ fdb-record-layer-core/src/test/java/com/apple/foundationdb/record/query/plan/planning/BooleanNormalizerTest.java
@@ -24,11 +24,10 @@ import com.apple.foundationdb.record.metadata.Key;
 import com.apple.foundationdb.record.query.expressions.OrComponent;
 import com.apple.foundationdb.record.query.expressions.Query;
 import com.apple.foundationdb.record.query.expressions.QueryComponent;
-import com.apple.foundationdb.record.query.plan.RecordQueryPlanner;
+import com.apple.foundationdb.record.query.plan.RecordQueryPlannerConfiguration;
 import org.junit.jupiter.api.Test;
 
 import javax.annotation.Nonnull;
-
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -104,6 +103,18 @@ public class BooleanNormalizerTest {
     }
 
     @Test
+    public void redundant() {
+        final BooleanNormalizer eliminator = BooleanNormalizer.forConfiguration(
+                RecordQueryPlannerConfiguration.builder().setCheckForDuplicateConditions(true).build());
+
+        assertExpectedNormalization(Query.or(Query.and(P1, P2, P3), Query.and(P1, P2, P4, P3)),
+                Query.and(P1, Query.or(Query.and(P2, P3), Query.and(P2, P4, P3))));
+
+        assertExpectedNormalization(eliminator, Query.and(P1, P2, P3),
+                Query.and(P1, Query.or(Query.and(P2, P3), Query.and(P2, P4, P3))));
+    }
+
+    @Test
     public void cnf() {
         List<QueryComponent> conjuncts = new ArrayList<>();
         for (int i = 0; i < 5; i++) {
@@ -134,9 +145,9 @@ public class BooleanNormalizerTest {
                                         Query.field("str_value_indexed").equalsValue("foo")))
                                 .collect(Collectors.toList())))
                         .collect(Collectors.toList()));
-        final BooleanNormalizer lowLimitNormalizer = BooleanNormalizer.withLimit(RecordQueryPlanner.DEFAULT_COMPLEXITY_THRESHOLD);
-        assertThrows(BooleanNormalizer.DNFTooLargeException.class, () -> lowLimitNormalizer.normalize(cnf));
-        assertEquals(cnf, lowLimitNormalizer.normalizeIfPossible(cnf));
+        final BooleanNormalizer plannerNormalizer = BooleanNormalizer.forConfiguration(RecordQueryPlannerConfiguration.builder().build());
+        assertThrows(BooleanNormalizer.DNFTooLargeException.class, () -> plannerNormalizer.normalize(cnf));
+        assertEquals(cnf, plannerNormalizer.normalizeIfPossible(cnf));
     }
 
     @Test
@@ -158,10 +169,16 @@ public class BooleanNormalizerTest {
     }
 
     protected static void assertExpectedNormalization(@Nonnull final QueryComponent expected, @Nonnull final QueryComponent given) {
-        final BooleanNormalizer normalizer = BooleanNormalizer.getDefaultInstance();
+        assertExpectedNormalization(BooleanNormalizer.getDefaultInstance(), expected, given);
+    }
+
+    protected static void assertExpectedNormalization(@Nonnull final BooleanNormalizer normalizer,
+                                                      @Nonnull final QueryComponent expected, @Nonnull final QueryComponent given) {
         final QueryComponent normalized = normalizer.normalize(given);
         assertFilterEquals(expected, normalized);
-        assertEquals(numberOfTerms(expected), normalizer.getNormalizedSize(given));
+        if (!normalizer.isCheckForDuplicateConditions()) {
+            assertEquals(numberOfTerms(expected), normalizer.getNormalizedSize(given));
+        }
     }
 
     // Query components do not implement equals, but they have distinctive enough printed representations.
