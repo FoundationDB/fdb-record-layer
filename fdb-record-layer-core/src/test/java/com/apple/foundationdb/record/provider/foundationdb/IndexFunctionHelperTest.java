2b3d9fd60929ce54561fcd7cb95347779f019737
Mike McMahon <mike_mcmahon@apple.com>
2020-10-22 13:03:12 -0700

    Expand test cases

--- fdb-record-layer-core/src/test/java/com/apple/foundationdb/record/provider/foundationdb/IndexFunctionHelperTest.java
+++ fdb-record-layer-core/src/test/java/com/apple/foundationdb/record/provider/foundationdb/IndexFunctionHelperTest.java
@@ -20,11 +20,15 @@
 
 package com.apple.foundationdb.record.provider.foundationdb;
 
-import com.apple.foundationdb.record.metadata.Key;
 import com.apple.foundationdb.record.metadata.expressions.GroupingKeyExpression;
 import com.apple.foundationdb.record.metadata.expressions.KeyExpression;
 import org.junit.jupiter.api.Test;
 
+import static com.apple.foundationdb.record.metadata.Key.Expressions.concat;
+import static com.apple.foundationdb.record.metadata.Key.Expressions.concatenateFields;
+import static com.apple.foundationdb.record.metadata.Key.Expressions.empty;
+import static com.apple.foundationdb.record.metadata.Key.Expressions.field;
+import static com.apple.foundationdb.record.metadata.expressions.KeyExpression.FanType.FanOut;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 /**
@@ -34,20 +38,54 @@ public class IndexFunctionHelperTest {
 
     @Test
     public void groupSubKeysBasic() {
-        final KeyExpression ungrouped = Key.Expressions.field("value").ungrouped();
-        assertEquals(Key.Expressions.field("value"), IndexFunctionHelper.getGroupedKey(ungrouped));
-        assertEquals(Key.Expressions.empty(), IndexFunctionHelper.getGroupingKey(ungrouped));
+        final KeyExpression ungrouped = field("value").ungrouped();
+        assertEquals(field("value"), IndexFunctionHelper.getGroupedKey(ungrouped));
+        assertEquals(empty(), IndexFunctionHelper.getGroupingKey(ungrouped));
 
-        final KeyExpression group = Key.Expressions.field("value").groupBy(Key.Expressions.field("group"));
-        assertEquals(Key.Expressions.field("value"), IndexFunctionHelper.getGroupedKey(group));
-        assertEquals(Key.Expressions.field("group"), IndexFunctionHelper.getGroupingKey(group));
+        final KeyExpression group = field("value").groupBy(field("group"));
+        assertEquals(field("value"), IndexFunctionHelper.getGroupedKey(group));
+        assertEquals(field("group"), IndexFunctionHelper.getGroupingKey(group));
     }
 
     @Test
     public void groupSubKeysNested() {
-        final KeyExpression nested = new GroupingKeyExpression(Key.Expressions.field("values", KeyExpression.FanType.FanOut).nest(Key.Expressions.concatenateFields("one", "two")), 1);
-        assertEquals(Key.Expressions.field("values", KeyExpression.FanType.FanOut).nest(Key.Expressions.field("one")), IndexFunctionHelper.getGroupingKey(nested));
-        assertEquals(Key.Expressions.field("values", KeyExpression.FanType.FanOut).nest(Key.Expressions.field("two")), IndexFunctionHelper.getGroupedKey(nested));
+        final KeyExpression wholeKey = concat(
+                field("a", FanOut).nest(concatenateFields("b", "c")),
+                field("d"),
+                field("e", FanOut).nest(concatenateFields("f", "g")));
+        final KeyExpression group0 = new GroupingKeyExpression(wholeKey, 0);
+        assertEquals(wholeKey, IndexFunctionHelper.getGroupingKey(group0));
+        assertEquals(empty(), IndexFunctionHelper.getGroupedKey(group0));
+        final KeyExpression group1 = new GroupingKeyExpression(wholeKey, 1);
+        assertEquals(concat(
+                field("a", FanOut).nest(concatenateFields("b", "c")),
+                field("d"),
+                field("e", FanOut).nest(field("f"))),
+                IndexFunctionHelper.getGroupingKey(group1));
+        assertEquals(field("e", FanOut).nest(field("g")),
+                IndexFunctionHelper.getGroupedKey(group1));
+        final KeyExpression group2 = new GroupingKeyExpression(wholeKey, 2);
+        assertEquals(concat(
+                field("a", FanOut).nest(concatenateFields("b", "c")),
+                field("d")),
+                IndexFunctionHelper.getGroupingKey(group2));
+        assertEquals(field("e", FanOut).nest(concatenateFields("f", "g")),
+                IndexFunctionHelper.getGroupedKey(group2));
+        final KeyExpression group3 = new GroupingKeyExpression(wholeKey, 3);
+        assertEquals(field("a", FanOut).nest(concatenateFields("b", "c")),
+                IndexFunctionHelper.getGroupingKey(group3));
+        assertEquals(concat(
+                field("d"),
+                field("e", FanOut).nest(concatenateFields("f", "g"))),
+                IndexFunctionHelper.getGroupedKey(group3));
+        final KeyExpression group4 = new GroupingKeyExpression(wholeKey, 4);
+        assertEquals(field("a", FanOut).nest(field("b")),
+                IndexFunctionHelper.getGroupingKey(group4));
+        assertEquals(concat(
+                field("a", FanOut).nest(field("c")),
+                field("d"),
+                field("e", FanOut).nest(concatenateFields("f", "g"))),
+                IndexFunctionHelper.getGroupedKey(group4));
     }
 
 }
