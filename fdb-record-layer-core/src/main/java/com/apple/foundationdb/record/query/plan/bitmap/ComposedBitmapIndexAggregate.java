6f58496eef949df4ff49c8208eed4813669940da
Mike McMahon <mike_mcmahon@apple.com>
2021-01-22 20:44:20 -0800

    Resolves #1110: Covering aggregate scans and duplicates

--- fdb-record-layer-core/src/main/java/com/apple/foundationdb/record/query/plan/bitmap/ComposedBitmapIndexAggregate.java
+++ fdb-record-layer-core/src/main/java/com/apple/foundationdb/record/query/plan/bitmap/ComposedBitmapIndexAggregate.java
@@ -217,8 +217,7 @@ public class ComposedBitmapIndexAggregate {
                 queryBuilder.setFilter(indexNode.filter);
                 final Index index = planner.getRecordMetaData().getIndex(indexNode.indexName);
                 final KeyExpression wholeKey = ((GroupingKeyExpression)index.getRootExpression()).getWholeKey();
-                final RecordQueryCoveringIndexPlan indexScan = planner.planCoveringAggregateIndex(queryBuilder.build(), index,
-                        wholeKey, true); // Partial view of repeated field is okay for composition.
+                final RecordQueryCoveringIndexPlan indexScan = planner.planCoveringAggregateIndex(queryBuilder.build(), index, wholeKey);
                 if (indexScan == null) {
                     return null;
                 }
