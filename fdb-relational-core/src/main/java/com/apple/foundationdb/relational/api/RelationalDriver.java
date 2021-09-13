bdda4fe761e67adeeb623d7df2134a5d7b84b679
Scott Fines <scottfines@gmail.com>
2021-09-13 09:25:32 -0500

    Fixing javadoc

--- fdb-relational-core/src/main/java/com/apple/foundationdb/relational/api/RelationalDriver.java
+++ fdb-relational-core/src/main/java/com/apple/foundationdb/relational/api/RelationalDriver.java
@@ -59,8 +59,7 @@ public interface RelationalDriver {
     /**
      * Determine if this driver can be used for the specified scheme string.
      *
-     * @param scheme the scheme to use, or {@code null} if no scheme is specified.
-     * @param url
+     * @param url the URL to check.
      * @return true if this driver can interpret this scheme, {@code false} otherwise.
      */
     boolean acceptsURL(URI url);
