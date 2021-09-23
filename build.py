60cd88f4276ea85e3902e7a7cd15eee67ad884ed
Aaron Molitor <amolitor@apple.com>
2021-09-23 09:58:58 -0700

    fix print function calls, format py files with black, address pylint issues

--- build.py
+++ build.py
@@ -1,7 +1,4 @@
-#!/usr/bin/python
-#
-# build.py
-#
+#!/usr/bin/env python3
 # This source file is part of the FoundationDB open source project
 #
 # Copyright 2015-2018 Apple Inc. and the FoundationDB project authors
@@ -18,190 +15,300 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+"""
+build.py
 
-import collections, datetime, os, sys, time, traceback, shutil, subprocess
+wrapper for running gradle to build this project
+"""
 
-TEMP_ROOT = '.tmp'
-PUBLISH_ROOT = '.dist'
+import collections
+import datetime
+import os
+import shutil
+import subprocess
+import sys
+import time
+
+TEMP_ROOT = ".tmp"
+PUBLISH_ROOT = ".dist"
 
 dir_path = os.path.abspath(os.path.dirname(__file__))
 
+
 def clear(path):
-    print_with_date('Clearing {0}'.format(path))
+    """
+
+    :param path:
+    :return:
+    """
+    print_with_date("Clearing {0}".format(path))
     if os.path.exists(path):
         shutil.rmtree(path)
 
+
 def mkdirp(path):
+    """
+
+    :param path:
+    :return:
+    """
     if not os.path.exists(path):
         os.makedirs(path)
 
-# Produce a human-readable date string that includes a UNIX timestamp.
+
 def date_string():
-    return '{0} ({1})'.format(datetime.datetime.now().strftime('%a %b %d %H:%M:%S %Y'), str(time.time()))
+    """
+    Produce a human-readable date string that includes a UNIX timestamp.
+    :return:
+    """
+    return "{0} ({1})".format(
+        datetime.datetime.now().strftime("%a %b %d %H:%M:%S %Y"), str(time.time())
+    )
+
 
 def print_with_date(text):
-    print ('{0}   {1}'.format(date_string(), text))
+    """
 
-# Constructs the gradle run path given the gradle args.
+    :param text:
+    :return:
+    """
+    print("{0}   {1}".format(date_string(), text))
+
+
 def run_gradle(proto_version, *args):
+    """
+    Constructs the gradle run path given the gradle args.
+    :param proto_version:
+    :param args:
+    :return:
+    """
+    return_value = None
     env = dict(os.environ)
-    full_args = [os.path.join(dir_path, 'gradlew'), '--console=plain', '-b', os.path.join(dir_path, 'build.gradle'), ] + list(args)
-    print_with_date('Running gradle build: {0}; Proto version: {1}'.format(' '.join(full_args), proto_version))
-    env['PROTO_VERSION'] = str(proto_version)
+    full_args = [
+        os.path.join(dir_path, "gradlew"),
+        "--console=plain",
+        "-b",
+        os.path.join(dir_path, "build.gradle"),
+    ] + list(args)
+    print_with_date(
+        "Running gradle build: {0}; Proto version: {1}".format(
+            " ".join(full_args), proto_version
+        )
+    )
+    env["PROTO_VERSION"] = str(proto_version)
     proc = subprocess.Popen(full_args, env=env)
     proc.communicate()
 
     if proc.returncode != 0:
-        print_with_date('Could not successfully run gradle build!')
-        return False
+        print_with_date("Could not successfully run gradle build!")
+        return_value = False
     else:
-        return True
+        return_value = True
+    return return_value
 
-# This looks in the gradle.properties file to find the version (for local builds).
+
 def parse_version():
-    properties_path = os.path.abspath(os.path.join(os.path.dirname(__file__), 'gradle.properties'))
-    print_with_date('Looking through properties file {0}...'.format(properties_path))
+    """
+    This looks in the gradle.properties file to find
+    the version (for local builds).
+    :return:
+    """
+    return_value = None
+    properties_path = os.path.abspath(
+        os.path.join(os.path.dirname(__file__), "gradle.properties")
+    )
+    print_with_date("Looking through properties file {0}...".format(properties_path))
 
     if os.path.exists(properties_path):
         try:
-            with open(properties_path, 'r') as fin:
+            with open(properties_path, "r") as fin:
                 contents = fin.read()
-            lines = filter(lambda x: len(x) > 0 and x[0] != '#', map(str.strip, contents.split('\n')))
+            lines = filter(
+                lambda x: len(x) > 0 and x[0] != "#",
+                map(str.strip, contents.split("\n")),
+            )
             prop_dict = {}
             for line in lines:
-                if '=' in line:
-                    split = line.find('=')
-                    prop_dict[line[:split]] = line[split+1:]
+                if "=" in line:
+                    split = line.find("=")
+                    prop_dict[line[:split]] = line[split + 1 :]
 
-            if 'version' in prop_dict:
-                print_with_date('Version found in properties file: {0}'.format(prop_dict['version']))
-                return prop_dict['version']
+            if "version" in prop_dict:
+                print_with_date(
+                    "Version found in properties file: {0}".format(prop_dict["version"])
+                )
+                return_value = prop_dict["version"]
             else:
-                print_with_date('No version found in properties file.')
-                return None
-        except Exception as e:
-            print_with_date('Error occurred while parsing file.')
-            print (traceback.format_exc())
-            return None
+                print_with_date("No version found in properties file.")
+                return_value = None
+        except Exception as ex:
+            print_with_date("Error occurred while parsing file.")
+            print(ex)
+            return_value = None
     else:
-        print ('File not found.')
-        return None
+        print("File not found.")
+        return_value = None
 
-# Look for a version number and possibly add things.
+    return return_value
+
+
 def get_version(release=False):
-    if 'CODE_VERSION' in os.environ:
-        version_base = os.environ['CODE_VERSION']
+    """
+    Look for a version number and possibly add things.
+    :param release:
+    :return:
+    """
+    if "CODE_VERSION" in os.environ:
+        version_base = os.environ["CODE_VERSION"]
     else:
-        print_with_date('CODE_VERSION environment variable not set. Looking in properties file...')
+        print_with_date(
+            "CODE_VERSION environment variable not set. Looking in properties file..."
+        )
         version_base = parse_version()
 
     if version_base is None:
-        print_with_date('Could not determine version number!')
+        print_with_date("Could not determine version number!")
         return None
 
-    if 'ARTIFACT_VERSION' in os.environ:
-        version = str(os.environ['ARTIFACT_VERSION'])
-    elif 'RECORD_LAYER_BUILD_NUMBER' in os.environ:
-        version = version_base + '.' + str(os.environ['RECORD_LAYER_BUILD_NUMBER'])
+    if "ARTIFACT_VERSION" in os.environ:
+        version = str(os.environ["ARTIFACT_VERSION"])
+    elif "RECORD_LAYER_BUILD_NUMBER" in os.environ:
+        version = version_base + "." + str(os.environ["RECORD_LAYER_BUILD_NUMBER"])
     else:
         version = version_base
 
     if not release:
-        version = version + '-SNAPSHOT'
+        version = version + "-SNAPSHOT"
 
-    print_with_date('Building version: {0}'.format(version))
+    print_with_date("Building version: {0}".format(version))
 
     return version
 
-# Move all of the publishable files to a temporary directory.
-def build(release=False, proto2=False, proto3=False, publish=False):
-    print_with_date('Running build script within directory: {0}'.format(dir_path))
 
-    print_with_date('Clearing temporary directory.')
+def build(release=False, proto2=False, proto3=False, publish=False):
+    """
+    Move all of the publishable files to a temporary directory.
+
+    :param release:
+    :param proto2:
+    :param proto3:
+    :param publish:
+    :return:
+    """
+    return_value = True
+    print_with_date("Running build script within directory: {0}".format(dir_path))
+
+    print_with_date("Clearing temporary directory.")
     shutil.rmtree(os.path.join(dir_path, TEMP_ROOT), ignore_errors=True)
 
     # Get the correct version.
     version = get_version(release)
     if version is None:
-        return False
+        return_value = False
 
     # Clear before.
     clear(os.path.join(dir_path, TEMP_ROOT))
 
     if proto2:
         # Make with protobuf 2.
-        success = run_gradle(2, 'clean', 'build', 'destructiveTest',
-                                '-PreleaseBuild={0}'.format('true' if release else 'false'))
+        success = run_gradle(
+            2,
+            "clean",
+            "build",
+            "destructiveTest",
+            "-PreleaseBuild={0}".format("true" if release else "false"),
+        )
         if not success:
-            return False
+            return_value = False
 
         if publish:
-            success = run_gradle(2, 'artifactoryPublish', '-PpublishBuild=true',
-                                    '-PreleaseBuild={0}'.format('true' if release else 'false'))
+            success = run_gradle(
+                2,
+                "artifactoryPublish",
+                "-PpublishBuild=true",
+                "-PreleaseBuild={0}".format("true" if release else "false"),
+            )
             if not success:
-                return False
+                return_value = False
 
-        success = run_gradle(2, 'fdb-record-layer-core:clean')
+        success = run_gradle(2, "fdb-record-layer-core:clean")
         if not success:
-            return False
+            return_value = False
 
     if proto3:
         # Make with protobuf 3.
-        success = run_gradle(3, 'build', 'destructiveTest', '-PcoreNotStrict',
-                                '-PreleaseBuild={0}'.format('true' if release else 'false'),
-                                '-PpublishBuild={0}'.format('true' if publish else 'false'))
+        success = run_gradle(
+            3,
+            "build",
+            "destructiveTest",
+            "-PcoreNotStrict",
+            "-PreleaseBuild={0}".format("true" if release else "false"),
+            "-PpublishBuild={0}".format("true" if publish else "false"),
+        )
         if not success:
-            return False
+            return_value = False
 
         if publish:
-            # These are enumerated rather than just using the full project artifactoryPublish command to avoid uploading
-            # the fdb-extensions subproject twice. (Note that as overwrite is not supported, doing so would result
-            # in the build failing.)
-            success = run_gradle(3, ':fdb-record-layer-core-pb3:artifactoryPublish',
-                                    ':fdb-record-layer-core-pb3-shaded:artifactoryPublish',
-                                    ':fdb-record-layer-icu-pb3:artifactoryPublish',
-                                    ':fdb-record-layer-spatial-pb3:artifactoryPublish',
-                                    ':fdb-record-layer-lucene-pb3:artifactoryPublish',
-                                    '-PcoreNotStrict',
-                                    '-PreleaseBuild={0}'.format('true' if release else 'false'),
-                                    '-PpublishBuild=true')
+            # These are enumerated rather than just using the full project
+            # artifactoryPublish command to avoid uploading the fdb-extensions
+            # subproject twice. (Note that as overwrite is not supported, doing
+            # so would result in the build failing.)
+            success = run_gradle(
+                3,
+                ":fdb-record-layer-core-pb3:artifactoryPublish",
+                ":fdb-record-layer-core-pb3-shaded:artifactoryPublish",
+                ":fdb-record-layer-icu-pb3:artifactoryPublish",
+                ":fdb-record-layer-spatial-pb3:artifactoryPublish",
+                ":fdb-record-layer-lucene-pb3:artifactoryPublish",
+                "-PcoreNotStrict",
+                "-PreleaseBuild={0}".format("true" if release else "false"),
+                "-PpublishBuild=true",
+            )
             if not success:
-                return False
+                return_value = False
+
+    return return_value
 
 
-    return True
-
-usage = """
+USAGE = """
 Usage: python build.py <release|snapshot> [--proto2] [--proto3] [--publish]
 Builds the packages for this project. You must specify release or snapshot
 The --proto2 flag indicates that the build should use protobuf 2.
 The --proto3 flag indicates that the build should use protobuf 3.
 The --publish flag indicates that the build should be published upon completion.
 """
+
+
 def parse_args():
-    ret = collections.namedtuple('Arguments', ['release', 'proto2', 'proto3', 'publish'])
+    """
+
+    :return:
+    """
+    ret = collections.namedtuple(
+        "Arguments", ["release", "proto2", "proto3", "publish"]
+    )
     ret.proto2 = False
     ret.proto3 = False
     ret.publish = False
 
     if len(sys.argv) < 2:
-        print usage
-        quit(2)
+        print(USAGE)
+        sys.exit(2)
 
-    ret.release = (sys.argv[1] == 'release')
+    ret.release = sys.argv[1] == "release"
 
     for i in range(2, len(sys.argv)):
         arg = sys.argv[i]
 
-        if arg == '--proto2':
+        if arg == "--proto2":
             ret.proto2 = True
-        elif arg == '--proto3':
+        elif arg == "--proto3":
             ret.proto3 = True
-        elif arg == '--publish':
+        elif arg == "--publish":
             ret.publish = True
         else:
-            print ('Unrecognized argument: {0}'.format(arg))
-            quit(3)
+            print("Unrecognized argument: {0}".format(arg))
+            sys.exit(3)
 
     # run both proto2 and proto3 by default
     if not ret.proto2 and not ret.proto3:
@@ -210,14 +317,20 @@ def parse_args():
 
     return ret
 
-if __name__ == "__main__":
-    print_with_date('Starting build')
-    success = True
-    args = parse_args()
-    success = success and build(release=args.release, proto2=args.proto2, proto3=args.proto3, publish=args.publish)
 
-    if not success:
-        print_with_date('Build failed!')
-        quit(1)
+if __name__ == "__main__":
+    print_with_date("Starting build")
+    SUCCESS = True
+    Args = parse_args()
+    SUCCESS = SUCCESS and build(
+        release=Args.release,
+        proto2=Args.proto2,
+        proto3=Args.proto3,
+        publish=Args.publish,
+    )
+
+    if not SUCCESS:
+        print_with_date("Build failed!")
+        sys.exit(1)
     else:
-        print_with_date('Build finished successfully.')
+        print_with_date("Build finished successfully.")
--- build/update_release_notes.py
+++ build/update_release_notes.py
@@ -1,8 +1,4 @@
-#!/bin/python
-
-#
-# update_release_notes.py
-#
+#!/usr/bin/env python3
 # This source file is part of the FoundationDB open source project
 #
 # Copyright 2015-2018 Apple Inc. and the FoundationDB project authors
@@ -20,26 +16,28 @@
 # limitations under the License.
 #
 
-#
-# Utility script for updating the release notes as part of a release.
-#
-# The release notes for the Record Layer should generally be updated as
-# features are added. However, as the build number is not known until the
-# actual release is cut, the release notes generally include only a reference
-# to the NEXT_RELEASE. This script should then be run as soon as the release
-# version is known. It updates the release notes file and replaces the
-# information for the "next release" with the actual release version and cleans
-# up some cruft that is included in the (not yet updated) release section.
-# It also adds a fresh version of the release notes template directly above
-# where the new release notes have been added in preparation for the following
-# release.
-#
+"""
+update_release_notes.py
+
+Utility script for updating the release notes as part of a release.
+
+The release notes for the Record Layer should generally be updated as
+features are added. However, as the build number is not known until the
+actual release is cut, the release notes generally include only a reference
+to the NEXT_RELEASE. This script should then be run as soon as the release
+version is known. It updates the release notes file and replaces the
+information for the "next release" with the actual release version and cleans
+up some cruft that is included in the (not yet updated) release section.
+It also adds a fresh version of the release notes template directly above
+where the new release notes have been added in preparation for the following
+release.
+"""
 
 import argparse
 import sys
 
 
-template = '''### NEXT_RELEASE
+TEMPLATE = """### NEXT_RELEASE
 
 * **Bug fix** Fix 1 [(Issue #NNN)](https://github.com/FoundationDB/fdb-record-layer/issues/NNN)
 * **Bug fix** Fix 2 [(Issue #NNN)](https://github.com/FoundationDB/fdb-record-layer/issues/NNN)
@@ -61,70 +59,92 @@ template = '''### NEXT_RELEASE
 * **Breaking change** Change 3 [(Issue #NNN)](https://github.com/FoundationDB/fdb-record-layer/issues/NNN)
 * **Breaking change** Change 4 [(Issue #NNN)](https://github.com/FoundationDB/fdb-record-layer/issues/NNN)
 * **Breaking change** Change 5 [(Issue #NNN)](https://github.com/FoundationDB/fdb-record-layer/issues/NNN)
-'''
+"""
+
 
 def extract_endpoints(lines, name):
-    '''Find the beginning and ending line numbers of the section with the given name'''
+    """Find the beginning and ending line numbers of the section with the given name"""
     start = 0
-    template_lines = []
     i = 0
-    while i < len(lines) and not lines[i].startswith('// begin ' + name):
+    while i < len(lines) and not lines[i].startswith("// begin " + name):
         i += 1
     start = i + 1
-    while i < len(lines) and not lines[i].startswith('// end ' + name):
+    while i < len(lines) and not lines[i].startswith("// end " + name):
         i += 1
     end = i
     return start, end
 
 
 def get_next_release_endpoints(lines):
-    '''Get the beginning and ending line numbers of the section corresponding to the next release.'''
-    return extract_endpoints(lines, 'next release') 
+    """
+    Get the beginning and ending line numbers of the section corresponding to
+    the next release.
+    """
+    return extract_endpoints(lines, "next release")
 
 
 def update_next_release_lines(next_release_lines, new_version):
-    '''Update the contents of the next release by filling in the version number and removing any placeholder lines.'''
+    """
+    Update the contents of the next release by filling in the version number
+     and removing any placeholder lines.
+    """
     updated_lines = []
     for line in next_release_lines:
-        new_line = line.replace('NEXT_RELEASE', new_version)
+        new_line = line.replace("NEXT_RELEASE", new_version)
         # Placeholder lines have #NNN to indicate where the issue number should go.
         # If they still have that line, then they weren't used.
-        if '#NNN' not in new_line:
+        if "#NNN" not in new_line:
             updated_lines.append(new_line)
     return updated_lines
 
 
 def get_new_contents(filename, new_version):
-    '''Given the name of the release notes file and the new version, generate the new contents of the file.'''
-    with open(filename, 'r') as fin:
-        lines = fin.read().split('\n')
+    """
+    Given the name of the release notes file and the new version,
+    generate the new contents of the file.
+    """
+    with open(filename, "r") as fin:
+        lines = fin.read().split("\n")
     next_release_start, next_release_end = get_next_release_endpoints(lines)
-    updated_next_release_notes = update_next_release_lines(lines[next_release_start:next_release_end], new_version)
+    updated_next_release_notes = update_next_release_lines(
+        lines[next_release_start:next_release_end], new_version
+    )
 
-    return '\n'.join(lines[:next_release_start-2]
-        + ['<!--', '// begin next release']
-        + [template]
-        + ['// end next release', '-->', '']
+    return "\n".join(
+        lines[: next_release_start - 2]
+        + ["<!--", "// begin next release"]
+        + [TEMPLATE]
+        + ["// end next release", "-->", ""]
         + updated_next_release_notes
-        + lines[next_release_end+3:])
+        + lines[next_release_end + 3 :]
+    )
 
 
 def main(argv):
-    '''Replace placeholder release notes with the final release notes for a version.'''
+    """
+    Replace placeholder release notes with the final release notes for a version.
+    """
     parser = argparse.ArgumentParser()
-    parser.add_argument('filename', help='Path to release notes document')
-    parser.add_argument('new_version', help='New version to use when updating the document')
-    parser.add_argument('--overwrite', action='store_true', default=False, help='Overwrite the existing file if set')
+    parser.add_argument("filename", help="Path to release notes document")
+    parser.add_argument(
+        "new_version", help="New version to use when updating the document"
+    )
+    parser.add_argument(
+        "--overwrite",
+        action="store_true",
+        default=False,
+        help="Overwrite the existing file if set",
+    )
     args = parser.parse_args(argv)
 
     new_contents = get_new_contents(args.filename, args.new_version)
     if args.overwrite:
-        with open(args.filename, 'w') as fout:
+        with open(args.filename, "w") as fout:
             fout.write(new_contents)
-        print 'Updated {} for version {}'.format(args.filename, args.new_version)
+        print("Updated {} for version {}".format(args.filename, args.new_version))
     else:
-        print new_contents
+        print(new_contents)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main(sys.argv[1:])
