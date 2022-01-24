96485581096c2a63a8eb4d207ea96029d0785ffa
Arnaud Lacurie <a_lacurie@apple.com>
2022-01-24 15:41:06 +0100

    Simplify cursor and tweak some tests

--- fdb-record-layer-lucene/src/test/java/com/apple/foundationdb/record/lucene/LuceneIndexTest.java
+++ fdb-record-layer-lucene/src/test/java/com/apple/foundationdb/record/lucene/LuceneIndexTest.java
@@ -24,6 +24,7 @@ import com.apple.foundationdb.record.ExecuteProperties;
 import com.apple.foundationdb.record.IndexEntry;
 import com.apple.foundationdb.record.IndexScanType;
 import com.apple.foundationdb.record.RecordCoreArgumentException;
+import com.apple.foundationdb.record.RecordCoreException;
 import com.apple.foundationdb.record.RecordCursor;
 import com.apple.foundationdb.record.RecordCursorProto;
 import com.apple.foundationdb.record.RecordMetaData;
@@ -86,7 +87,11 @@ import static com.apple.foundationdb.record.provider.foundationdb.indexes.TextIn
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.hasKey;
 
 /**
  * Tests for {@code LUCENE} type indexes.
@@ -823,7 +828,7 @@ public class LuceneIndexTest extends FDBRecordStoreTestBase {
         for (IndexEntry suggestion : suggestions) {
             String key = (String)suggestion.getKey().get(0);
             String value = (String)suggestion.getValue().get(0);
-            assertTrue(expectedSuggestions.containsKey(key));
+            assertThat(expectedSuggestions, hasKey(key));
             assertEquals(expectedSuggestions.get(key), value);
         }
     }
@@ -846,9 +851,11 @@ public class LuceneIndexTest extends FDBRecordStoreTestBase {
                     "helps", "text"
                     ));
             spellCheckHelper("hello", Map.of());
-            spellCheckHelper("wrongField:helo", Map.of());
             spellCheckHelper("mous", Map.of("mouse", "text"));
-            spellCheckHelper("hous", Map.of("house", "text"));
+
+            assertThrows(RecordCoreException.class,
+                    () -> spellCheckHelper("wrongField:helo", Map.of()),
+                    "Invalid field name in Lucene index query");
         }
     }
 
