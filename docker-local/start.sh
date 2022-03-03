68df58b32f32db339261519aeaf7a10c14dc435e
Mike McMahon <mike_mcmahon@apple.com>
2022-03-03 07:11:15 -0800

    Adjust the download location for releases as used by docker-local.

--- docker-local/start.sh
+++ docker-local/start.sh
@@ -37,7 +37,7 @@ fi
 RUNDIR=${ROOTDIR}/run
 mkdir -p ${RUNDIR}
 
-FDB_VERSION="${FDB_VERSION:-6.3.18}"
+FDB_VERSION="${FDB_VERSION:-6.3.24}"
 FDB_PORT="${FDB_PORT:-14550}"
 
 FDB_CLUSTER_FILE=${RUNDIR}/docker.cluster
@@ -48,16 +48,25 @@ FDB_LIBRARY_FILE=${RUNDIR}/libfdb_c_${FDB_VERSION}.${SO_SUFFIX}
 if [ ! -x ${FDB_CLI_FILE} ]; then
     cd /tmp
     if [ $OS == 'macOS' ]; then
-        curl -L -O https://www.foundationdb.com/downloads/${FDB_VERSION}/${OS}/installers/FoundationDB-${FDB_VERSION}.pkg
+        curl -L -O https://github.com/apple/foundationdb/releases/download/${FDB_VERSION}/FoundationDB-${FDB_VERSION}.pkg
+        curl -L -O https://github.com/apple/foundationdb/releases/download/${FDB_VERSION}/FoundationDB-${FDB_VERSION}.pkg.sha256
+        echo '' '' FoundationDB-${FDB_VERSION}.pkg >>FoundationDB-${FDB_VERSION}.pkg.sha256
+        shasum -a 256 -c FoundationDB-${FDB_VERSION}.pkg.sha256 || exit 1
         pkgutil --expand-full FoundationDB-${FDB_VERSION}.pkg fdbpkg
         mv fdbpkg/FoundationDB-clients.pkg/Payload/usr/local/bin/fdbcli ${FDB_CLI_FILE}
         mv fdbpkg/FoundationDB-clients.pkg/Payload/usr/local/lib/libfdb_c.${SO_SUFFIX} ${FDB_LIBRARY_FILE}
-        rm -rf fdbpkg FoundationDB-${FDB_VERSION}.pkg
+        rm -rf fdbpkg FoundationDB-${FDB_VERSION}.pkg FoundationDB-${FDB_VERSION}.pkg.sha256
     else
-        curl -L -O https://www.foundationdb.com/downloads/${FDB_VERSION}/${OS}/fdb_${FDB_VERSION}.tar.gz
-        tar fx fdb_${FDB_VERSION}.tar.gz
-        mv fdb_binaries/fdbcli ${FDB_CLI_FILE}
-        rm -rf fdb_binaries fdb_${FDB_VERSION}.tar.gz
+        curl -L -O https://github.com/apple/foundationdb/releases/download/${FDB_VERSION}/fdbcli.x86_64
+        curl -L -O https://github.com/apple/foundationdb/releases/download/${FDB_VERSION}/fdbcli.x86_64.sha256
+        curl -L -O https://github.com/apple/foundationdb/releases/download/${FDB_VERSION}/libfdb_c.x86_64.so
+        curl -L -O https://github.com/apple/foundationdb/releases/download/${FDB_VERSION}/libfdb_c.x86_64.so.sha256
+        echo $(cat fdbcli.x86_64.sha256) '' fdbcli.x86_64 >fdb.sha256
+        echo $(cat libfdb_c.x86_64.so.sha256) '' libfdb_c.x86_64.so >>fdb.sha256
+        shasum -a 256 -c fdb.sha256 || exit 1
+        mv fdbcli.x86_64 ${FDB_CLI_FILE}
+        mv libfdb_c.x86_64.so ${FDB_LIBRARY_FILE}
+        rm -rf fdbcli.x86_64.sha256 libfdb_c.x86_64.so.sha256 fdb.sha256
     fi
 fi
 
