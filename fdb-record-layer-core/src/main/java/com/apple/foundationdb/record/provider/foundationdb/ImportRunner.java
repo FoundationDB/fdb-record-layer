9dbdd2176d51c2a0f826b0f42da9bbdae2155bff
Scott Dugas <sdugas@apple.com>
2022-03-08 17:59:04 -0500

    Rough pass at extracting LimittedRunner
    In essence, this is an attempt to abstract away IndexingThrottle
    so that it could be used for other cross-transaction work that
    may want to limit the amount of work that it is touching. One
    such example of that would be to Export/Import mechanism, which
    would want to do as much work per-transaction as it can, but would
    need to save across many transactions.

---
+++ fdb-record-layer-core/src/main/java/com/apple/foundationdb/record/provider/foundationdb/ImportRunner.java
@@ -0,0 +1,71 @@
+/*
+ * ImportRunner.java
+ *
+ * This source file is part of the FoundationDB open source project
+ *
+ * Copyright 2015-2022 Apple Inc. and the FoundationDB project authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.apple.foundationdb.record.provider.foundationdb;
+
+import com.apple.foundationdb.async.AsyncUtil;
+import com.apple.foundationdb.record.RecordCursor;
+
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+
+public abstract class ImportRunner<T> implements LimittedRunner.Runner {
+    private RecordCursor<T> cursor;
+    private final FDBDatabaseRunner runner;
+    private List<T> buffer;
+    int position = 0;
+
+    public ImportRunner(final RecordCursor<T> cursor, FDBDatabaseRunner runner) {
+        this.cursor = cursor;
+        this.runner = runner;
+    }
+
+    protected abstract void save(T value, FDBRecordContext context);
+
+    @Override
+    public CompletableFuture<Boolean> runAsync(int limit) {
+        final CompletableFuture<Void> fillBufferFuture;
+        if (buffer.size() < limit) {
+            fillBufferFuture = cursor.limitRowsTo(limit - buffer.size())
+                    .forEach(val -> buffer.add(val));
+        } else {
+            fillBufferFuture = CompletableFuture.completedFuture(null);
+        }
+        return fillBufferFuture.thenCompose(ignored -> {
+            if (buffer.size() == 0) {
+                return AsyncUtil.READY_FALSE;
+            }
+            return runner.runAsync(context -> {
+                while (position < limit && position < buffer.size()) {
+                    // TODO allow save to return a future
+                    save(buffer.get(position), context);
+                }
+                return AsyncUtil.READY_TRUE;
+            });
+        });
+    }
+
+    @Override
+    public void onSuccess() {
+        // these entries were successfully saved, so remove them from the buffer
+        buffer.subList(0, position).clear();
+        // TODO metrics
+    }
+}
---
+++ fdb-record-layer-core/src/main/java/com/apple/foundationdb/record/provider/foundationdb/LimittedRunner.java
@@ -0,0 +1,140 @@
+/*
+ * LimittedRunner.java
+ *
+ * This source file is part of the FoundationDB open source project
+ *
+ * Copyright 2015-2022 Apple Inc. and the FoundationDB project authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.apple.foundationdb.record.provider.foundationdb;
+
+import com.apple.foundationdb.FDBError;
+import com.apple.foundationdb.FDBException;
+import com.apple.foundationdb.async.AsyncUtil;
+
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+import java.util.Set;
+import java.util.concurrent.CompletableFuture;
+
+public class LimittedRunner implements AutoCloseable {
+
+    // These error codes represent a list of errors that can occur if there is too much work to be done
+    // in a single transaction.
+    private static final Set<Integer> lessenWorkCodes = Set.of(
+            FDBError.TIMED_OUT.code(),
+            FDBError.TRANSACTION_TOO_OLD.code(),
+            FDBError.NOT_COMMITTED.code(),
+            FDBError.TRANSACTION_TIMED_OUT.code(),
+            FDBError.COMMIT_READ_INCOMPLETE.code(),
+            FDBError.TRANSACTION_TOO_LARGE.code());
+
+    private int currentLimit;
+    private final int maxLimit;
+    private final Runner runner;
+    private int retriesAtMinimum = 0;
+    private int successCount = 0;
+    private int increaseLimitAfter = 10; // maybe this should be configurable
+    private int maxRetriesAtMinimum;
+    private boolean closed = false;
+
+    public LimittedRunner(final int maxLimit, final int maxRetriesAtMinimum, Runner runner) {
+        this.currentLimit = maxLimit;
+        this.maxLimit = maxLimit;
+        this.runner = runner;
+        this.maxRetriesAtMinimum = maxRetriesAtMinimum;
+    }
+
+    public CompletableFuture<Void> runAsync() {
+        final CompletableFuture<Void> overallResult = new CompletableFuture<>();
+        AsyncUtil.whileTrue(() -> {
+            if (closed) {
+                overallResult.completeExceptionally(new FDBDatabaseRunner.RunnerClosed());
+                return AsyncUtil.READY_FALSE;
+            }
+            return runner.runAsync(currentLimit)
+                    .handle((shouldContinue, error) -> handle(overallResult, shouldContinue, error));
+        });
+        return overallResult;
+    }
+
+    private Boolean handle(final CompletableFuture<Void> overallResult, final Boolean shouldContinue, final Throwable error) {
+        if (error == null) {
+            // TODO indexer tracks records scanned only for successful
+            maybeIncreaseLimit();
+            return shouldContinue;
+        } else {
+            successCount = 0;
+            if (!maybeDecreaseLimit(error)) {
+                overallResult.completeExceptionally(error);
+                return false;
+            } else {
+                return true;
+            }
+        }
+    }
+
+    private boolean maybeDecreaseLimit(final Throwable error) {
+        FDBException fdbException = getFDBException(error);
+        if (fdbException != null && lessenWorkCodes.contains(fdbException.getCode())) {
+            if (currentLimit == 1) {
+                retriesAtMinimum++;
+                return retriesAtMinimum < maxRetriesAtMinimum;
+            } else {
+                // TODO should we delay ?
+                // TODO log
+                currentLimit = Math.max(1, (3 * currentLimit) / 4);
+                return true;
+            }
+        } else {
+            // Not something to lessen work, so don't retry
+            return false;
+        }
+    }
+
+    // Finds the FDBException that ultimately caused some throwable or
+    // null if there is none. This can be then used to determine, for
+    // example, the error code associated with this FDBException.
+    @Nullable
+    private FDBException getFDBException(@Nonnull Throwable e) {
+        Throwable curr = e;
+        while (curr != null) {
+            if (curr instanceof FDBException) {
+                return (FDBException)curr;
+            } else {
+                curr = curr.getCause();
+            }
+        }
+        return null;
+    }
+
+    private void maybeIncreaseLimit() {
+        successCount++;
+        if (successCount >= increaseLimitAfter && currentLimit < maxLimit) {
+            currentLimit = Math.min(maxLimit, Math.max(currentLimit + 1, (4 * currentLimit) / 3));
+            // TODO log
+        }
+    }
+
+    @Override
+    public void close() throws Exception {
+        this.closed = true;
+    }
+
+    public interface Runner {
+        CompletableFuture<Boolean> runAsync(int limit);
+        void onSuccess();
+    }
+}
