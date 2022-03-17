eef0ce079624be4120cb3ac1db1f399e5ca7064b
Ohad Zeliger <ozeliger@apple.com>
2022-03-17 15:26:38 -0400

    Migrate MapPlan and rule to new branch

---
+++ fdb-record-layer-core/src/main/java/com/apple/foundationdb/record/query/plan/temp/rules/ImplementMapRule.java
@@ -0,0 +1,79 @@
+/*
+ * ImplementMapRule.java
+ *
+ * This source file is part of the FoundationDB open source project
+ *
+ * Copyright 2015-2022 Apple Inc. and the FoundationDB project authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.apple.foundationdb.record.query.plan.temp.rules;
+
+import com.apple.foundationdb.annotation.API;
+import com.apple.foundationdb.record.query.plan.plans.RecordQueryFilterPlan;
+import com.apple.foundationdb.record.query.plan.plans.RecordQueryMapPlan;
+import com.apple.foundationdb.record.query.plan.plans.RecordQueryPlan;
+import com.apple.foundationdb.record.query.plan.temp.GroupExpressionRef;
+import com.apple.foundationdb.record.query.plan.temp.PlannerRule;
+import com.apple.foundationdb.record.query.plan.temp.PlannerRuleCall;
+import com.apple.foundationdb.record.query.plan.temp.Quantifier;
+import com.apple.foundationdb.record.query.plan.temp.expressions.SelectExpression;
+import com.apple.foundationdb.record.query.plan.temp.matchers.BindingMatcher;
+import com.apple.foundationdb.record.query.plan.temp.matchers.CollectionMatcher;
+import com.apple.foundationdb.record.query.plan.temp.matchers.PlannerBindings;
+import com.apple.foundationdb.record.query.plan.temp.matchers.RecordQueryPlanMatchers;
+
+import javax.annotation.Nonnull;
+import java.util.Collection;
+
+import static com.apple.foundationdb.record.query.plan.temp.matchers.ListMatcher.exactly;
+import static com.apple.foundationdb.record.query.plan.temp.matchers.MultiMatcher.some;
+import static com.apple.foundationdb.record.query.plan.temp.matchers.QuantifierMatchers.forEachQuantifierOverPlans;
+import static com.apple.foundationdb.record.query.plan.temp.matchers.RelationalExpressionMatchers.selectExpression;
+
+/**
+ * A rule that implements a logical filter around a {@link RecordQueryPlan} as a {@link RecordQueryFilterPlan}.
+ */
+@API(API.Status.EXPERIMENTAL)
+public class ImplementMapRule extends PlannerRule<SelectExpression> {
+    private static final CollectionMatcher<RecordQueryPlan> innerPlansMatcher = some(RecordQueryPlanMatchers.anyPlan());
+    private static final BindingMatcher<Quantifier.ForEach> innerQuantifierMatcher = forEachQuantifierOverPlans(innerPlansMatcher);
+    private static final BindingMatcher<SelectExpression> root =
+            selectExpression(CollectionMatcher.empty(), exactly(innerQuantifierMatcher));
+
+    public ImplementMapRule() {
+        super(root);
+    }
+
+    @Override
+    public void onMatch(@Nonnull PlannerRuleCall call) {
+        final PlannerBindings bindings = call.getBindings();
+        final SelectExpression selectExpression = bindings.get(root);
+        final Collection<? extends RecordQueryPlan> innerPlans = bindings.get(innerPlansMatcher);
+        if (innerPlans.isEmpty()) {
+            return;
+        }
+
+        final Quantifier.ForEach innerQuantifier = bindings.get(innerQuantifierMatcher);
+
+        final GroupExpressionRef<? extends RecordQueryPlan> referenceOverPlans = GroupExpressionRef.from(innerPlans);
+
+        call.yield(GroupExpressionRef.of(
+                new RecordQueryMapPlan(
+                        Quantifier.physicalBuilder()
+                                .morphFrom(innerQuantifier)
+                                .build(referenceOverPlans),
+                        selectExpression.getResultValues())));
+    }
+}
