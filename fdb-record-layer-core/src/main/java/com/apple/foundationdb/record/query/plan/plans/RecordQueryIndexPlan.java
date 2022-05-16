FoundationDB/fdb-record-layer

commit 29edea5c3f74be2bb3e15f4cf749da5ed04733e3

Resolves #1560: Index prefetch (#1561)
* Initial commit of POC for index prefetch

* version

* Disable readyourqrites

* Handle embedded keys in index entries

* Small fixes. DisableReadYourWrites for tests, add plan constructor

* Add props to control which API to use. Custom unpacking. range scan in FDB.

* new test

* Finalize implementation:
1. Create IndexedRawRecord to hold indexed unsplit records
2. Unsplit the records in the index maintainer
3. Test reads
4. Deserialize records

* Handle new API

* Code cleanup - move unsplit records to record store
Support fallback execution

* Changes after rebase, style.

* Reimplement integration with planner after rebase, added tests with fallback and comparator plan

* Support old version format, test.

* Changes after getting latest FDB API

* Latest changes from main branch.
Add split record tests, fix performance tests, add prefetch tests to performance framework, cleanup code.

* Code style and findbugs changes.

* Handle old format

* Fixes while running all tests:
- Don't use prefetch when no common primary key
- Don't use prefetch for non-VALUE indexes
- Fix counter event

* Fallback mode for cases where the failure happens in the Async Future's onNext() call.
- Add FallbackCursor
- Use the cursor in fallback mode
- Tests for read-your-writes mode that exhibit this kind of failure

* Added handling of IndexOrphanBehavior: In case there are no record splits for an index entry, return empty entry, skip record or abort. Added tests.

* Changes after rebase from main and checkstyle

* Add better names to test

* Disable failing test

* Add mixed continuations test

* Add comments, javadoc and @Nonnull annotations.
Review comments; Fix type issues with generics: Add serialized and changed <Message> to <M>, removed redundant queriedRecordOfMessage method.
Move tests to foundationdb package.

* Add version protection, fix test names.

fdb-record-layer-core/src/main/java/com/apple/foundationdb/record/query/plan/plans/RecordQueryIndexPlan.java
@@ -30,14 +30,21 @@ import com.apple.foundationdb.record.PlanHashable;
 import com.apple.foundationdb.record.RecordCoreException;
 import com.apple.foundationdb.record.RecordCursor;
 import com.apple.foundationdb.record.RecordMetaData;
+import com.apple.foundationdb.record.cursors.FallbackCursor;
+import com.apple.foundationdb.record.logging.KeyValueLogMessage;
+import com.apple.foundationdb.record.logging.LogMessageKeys;
 import com.apple.foundationdb.record.metadata.Index;
+import com.apple.foundationdb.record.metadata.expressions.KeyExpression;
 import com.apple.foundationdb.record.provider.common.StoreTimer;
+import com.apple.foundationdb.record.provider.foundationdb.APIVersion;
 import com.apple.foundationdb.record.provider.foundationdb.FDBRecordStoreBase;
 import com.apple.foundationdb.record.provider.foundationdb.FDBStoreTimer;
+import com.apple.foundationdb.record.provider.foundationdb.IndexOrphanBehavior;
 import com.apple.foundationdb.record.provider.foundationdb.IndexScanBounds;
 import com.apple.foundationdb.record.provider.foundationdb.IndexScanComparisons;
 import com.apple.foundationdb.record.provider.foundationdb.IndexScanParameters;
 import com.apple.foundationdb.record.query.plan.AvailableFields;
+import com.apple.foundationdb.record.query.plan.RecordQueryPlannerConfiguration;
 import com.apple.foundationdb.record.query.plan.ScanComparisons;
 import com.apple.foundationdb.record.query.plan.cascades.AliasMap;
 import com.apple.foundationdb.record.query.plan.cascades.CorrelationIdentifier;
@@ -54,6 +61,8 @@ import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.ImmutableSet;
 import com.google.protobuf.Message;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
@@ -70,12 +79,17 @@ import java.util.Set;
 @API(API.Status.INTERNAL)
 @SuppressWarnings("OptionalUsedAsFieldOrParameterType")
 public class RecordQueryIndexPlan implements RecordQueryPlanWithNoChildren, RecordQueryPlanWithComparisons, RecordQueryPlanWithIndex, PlannerGraphRewritable {
+    public static final Logger LOGGER = LoggerFactory.getLogger(RecordQueryIndexPlan.class);
     protected static final ObjectPlanHash BASE_HASH = new ObjectPlanHash("Record-Query-Index-Plan");
 
     @Nonnull
     protected final String indexName;
+    @Nullable
+    private final KeyExpression commonPrimaryKey;
     @Nonnull
     protected final IndexScanParameters scanParameters;
+    @Nonnull
+    private RecordQueryPlannerConfiguration.IndexFetchMethod indexFetchMethod;
     protected final boolean reverse;
     protected final boolean strictlySorted;
     @Nonnull
@@ -84,37 +98,107 @@ public class RecordQueryIndexPlan implements RecordQueryPlanWithNoChildren, Reco
     private final Type resultType;
 
     public RecordQueryIndexPlan(@Nonnull final String indexName, @Nonnull final IndexScanParameters scanParameters, final boolean reverse) {
-        this(indexName, scanParameters, reverse, false);
+        this(indexName, null, scanParameters, RecordQueryPlannerConfiguration.IndexFetchMethod.SCAN_AND_FETCH, reverse, false);
     }
 
     public RecordQueryIndexPlan(@Nonnull final String indexName,
+                                @Nullable final KeyExpression commonPrimaryKey,
                                 @Nonnull final IndexScanParameters scanParameters,
+                                @Nonnull final RecordQueryPlannerConfiguration.IndexFetchMethod useIndexPrefetch,
                                 final boolean reverse,
                                 final boolean strictlySorted) {
-        this(indexName, scanParameters, reverse, strictlySorted, Optional.empty(), new Type.Any());
+        this(indexName, commonPrimaryKey, scanParameters, useIndexPrefetch, reverse, strictlySorted, Optional.empty(), new Type.Any());
     }
 
     public RecordQueryIndexPlan(@Nonnull final String indexName,
+                                @Nullable final KeyExpression commonPrimaryKey,
                                 @Nonnull final IndexScanParameters scanParameters,
+                                @Nonnull final RecordQueryPlannerConfiguration.IndexFetchMethod indexFetchMethod,
                                 final boolean reverse,
                                 final boolean strictlySorted,
                                 @Nonnull final ScanWithFetchMatchCandidate matchCandidate,
                                 @Nonnull final Type.Record resultType) {
-        this(indexName, scanParameters, reverse, strictlySorted, Optional.of(matchCandidate), resultType);
+        this(indexName, commonPrimaryKey, scanParameters, indexFetchMethod, reverse, strictlySorted, Optional.of(matchCandidate), resultType);
     }
 
     private RecordQueryIndexPlan(@Nonnull final String indexName,
+                                 @Nullable final KeyExpression commonPrimaryKey,
                                  @Nonnull final IndexScanParameters scanParameters,
+                                 @Nonnull final RecordQueryPlannerConfiguration.IndexFetchMethod indexFetchMethod,
                                  final boolean reverse,
                                  final boolean strictlySorted,
                                  @Nonnull final Optional<? extends ScanWithFetchMatchCandidate> matchCandidateOptional,
                                  @Nonnull final Type resultType) {
         this.indexName = indexName;
+        this.commonPrimaryKey = commonPrimaryKey;
         this.scanParameters = scanParameters;
+        this.indexFetchMethod = indexFetchMethod;
         this.reverse = reverse;
         this.strictlySorted = strictlySorted;
         this.matchCandidateOptional = matchCandidateOptional;
         this.resultType = resultType;
+        if (indexFetchMethod != RecordQueryPlannerConfiguration.IndexFetchMethod.SCAN_AND_FETCH) {
+            if (commonPrimaryKey == null) {
+                logDebug("Index remote fetch cannot be used without a primary key. Falling back to regular scan.");
+                this.indexFetchMethod = RecordQueryPlannerConfiguration.IndexFetchMethod.SCAN_AND_FETCH;
+            }
+            if (scanParameters.getScanType() != IndexScanType.BY_VALUE) {
+                logDebug("Index remote fetch can only be used with VALUE index scan. Falling back to regular scan.");
+                this.indexFetchMethod = RecordQueryPlannerConfiguration.IndexFetchMethod.SCAN_AND_FETCH;
+            }
+        }
+    }
+
+    @Nonnull
+    @Override
+    public <M extends Message> RecordCursor<QueryResult> executePlan(@Nonnull final FDBRecordStoreBase<M> store, @Nonnull final EvaluationContext context,
+                                                                     @Nullable final byte[] continuation, @Nonnull final ExecuteProperties executeProperties) {
+        RecordQueryPlannerConfiguration.IndexFetchMethod fetchMethod = indexFetchMethod;
+        // Check here to allow for the store API_VERSION to change
+        if ((indexFetchMethod != RecordQueryPlannerConfiguration.IndexFetchMethod.SCAN_AND_FETCH) &&
+                !store.getContext().getAPIVersion().isAtLeast(APIVersion.API_VERSION_7_1)) {
+            LOGGER.warn(KeyValueLogMessage.of("Index remote fetch can only be used with API_VERSION of at least 7.1. Falling back to regular scan.",
+                    LogMessageKeys.PLAN_HASH, planHash(PlanHashKind.STRUCTURAL_WITHOUT_LITERALS)));
+            fetchMethod = RecordQueryPlannerConfiguration.IndexFetchMethod.SCAN_AND_FETCH;
+        }
+
+        switch (fetchMethod) {
+            case SCAN_AND_FETCH:
+                // Use the default implementation for index scan
+                return RecordQueryPlanWithIndex.super.executePlan(store, context, continuation, executeProperties);
+            case USE_REMOTE_FETCH:
+                // Use index prefetch without fallback
+                return executeUsingRemoteFetch(store, context, continuation, executeProperties);
+            case USE_REMOTE_FETCH_WITH_FALLBACK:
+                // Use Index prefetch and fall back to regular index scan.
+                // Using the fallback mechanism here separates the execution part from the planning part
+                // (No need to plan again) and from failures at other parts of the execution.
+                try {
+                    // The fallback cursor will handle failures that happen after the executeUsingIndexPrefetch call
+                    return new FallbackCursor<>(
+                            executeUsingRemoteFetch(store, context, continuation, executeProperties),
+                            () -> RecordQueryPlanWithIndex.super.executePlan(store, context, continuation, executeProperties));
+                } catch (Exception ex) {
+                    LOGGER.warn(KeyValueLogMessage.of("Remote Fetch execution failed, falling back to Index scan",
+                            LogMessageKeys.PLAN_HASH, planHash(PlanHashKind.STRUCTURAL_WITHOUT_LITERALS)), ex);
+                    return RecordQueryPlanWithIndex.super.executePlan(store, context, continuation, executeProperties);
+                }
+            default:
+                throw new RecordCoreException("Unknown useIndexPrefetch option").addLogInfo("option", indexFetchMethod);
+        }
+    }
+
+    @Nonnull
+    private <M extends Message> RecordCursor<QueryResult> executeUsingRemoteFetch(@Nonnull final FDBRecordStoreBase<M> store, @Nonnull final EvaluationContext context,
+                                                                                  @Nullable final byte [] continuation, @Nonnull final ExecuteProperties executeProperties) {
+        final RecordMetaData metaData = store.getRecordMetaData();
+        final Index index = metaData.getIndex(indexName);
+        final IndexScanBounds scanBounds = scanParameters.bind(store, index, context);
+
+        // CommonPrimaryKey is nullable but is protected by the constructor in the case pf index prefetch
+        return store.scanIndexRemoteFetch(index, scanBounds, Objects.requireNonNull(getCommonPrimaryKey()), continuation, executeProperties.asScanProperties(isReverse()), IndexOrphanBehavior.ERROR)
+                .map(store::queriedRecord)
+                .map(QueryResult::of);
     }
 
     @Nonnull
@@ -138,10 +222,20 @@ public class RecordQueryIndexPlan implements RecordQueryPlanWithNoChildren, Reco
         return scanParameters;
     }
 
+    @Nullable
+    public KeyExpression getCommonPrimaryKey() {
+        return commonPrimaryKey;
+    }
+
     @Nonnull
     @Override
     public IndexScanType getScanType() {
         return scanParameters.getScanType();
+    }
+
+    @Nonnull
+    public RecordQueryPlannerConfiguration.IndexFetchMethod getIndexFetchMethod() {
+        return indexFetchMethod;
     }
 
     @Override
@@ -193,7 +287,7 @@ public class RecordQueryIndexPlan implements RecordQueryPlanWithNoChildren, Reco
 
     @Override
     public RecordQueryIndexPlan strictlySorted() {
-        return new RecordQueryIndexPlan(indexName, scanParameters, reverse, true);
+        return new RecordQueryIndexPlan(indexName, getCommonPrimaryKey(), scanParameters, getIndexFetchMethod(), reverse, true);
     }
 
     @Override
@@ -211,7 +305,9 @@ public class RecordQueryIndexPlan implements RecordQueryPlanWithNoChildren, Reco
     @Override
     public RecordQueryIndexPlan rebase(@Nonnull final AliasMap translationMap) {
         return new RecordQueryIndexPlan(getIndexName(),
+                getCommonPrimaryKey(),
                 getScanParameters(),
+                getIndexFetchMethod(),
                 isReverse(),
                 isStrictlySorted(),
                 matchCandidateOptional,
@@ -269,13 +365,15 @@ public class RecordQueryIndexPlan implements RecordQueryPlanWithNoChildren, Reco
                 return indexName.hashCode() + scanParameters.planHash(hashKind) + (reverse ? 1 : 0);
             case FOR_CONTINUATION:
             case STRUCTURAL_WITHOUT_LITERALS:
+                int planHash;
                 if (scanParameters instanceof IndexScanComparisons) {
                     // Keep hash stable for change in representation.
                     // TODO: If there is another event that changes hashes or they become less critical in tests, this can be removed.
-                    return PlanHashable.objectsPlanHash(hashKind, BASE_HASH, indexName, getScanType(), getComparisons(), reverse, strictlySorted);
+                    planHash = PlanHashable.objectsPlanHash(hashKind, BASE_HASH, indexName, getScanType(), getComparisons(), reverse, strictlySorted);
                 } else {
-                    return PlanHashable.objectsPlanHash(hashKind, BASE_HASH, indexName, scanParameters, reverse, strictlySorted);
+                    planHash = PlanHashable.objectsPlanHash(hashKind, BASE_HASH, indexName, scanParameters, reverse, strictlySorted);
                 }
+                return planHash;
             default:
                 throw new UnsupportedOperationException("Hash kind " + hashKind.name() + " is not supported");
         }
@@ -360,5 +458,12 @@ public class RecordQueryIndexPlan implements RecordQueryPlanWithNoChildren, Reco
                         PlannerGraph.fromNodeAndChildGraphs(
                                 new PlannerGraph.DataNodeWithInfo(NodeInfo.INDEX_DATA, getResultType(), ImmutableList.copyOf(getUsedIndexes())),
                                 ImmutableList.of())));
+    }
+
+    private void logDebug(final String staticMessage) {
+        if (LOGGER.isDebugEnabled()) {
+            LOGGER.debug(KeyValueLogMessage.of(staticMessage,
+                    LogMessageKeys.PLAN_HASH, planHash(PlanHashKind.STRUCTURAL_WITHOUT_LITERALS)));
+        }
     }
 }

fdb-record-layer-core/src/main/java/com/apple/foundationdb/record/cursors/FallbackCursor.java
@@ -1,0 +1,168 @@
+/*
+ * FallbackCursor.java
+ *
+ * This source file is part of the FoundationDB open source project
+ *
+ * Copyright 2015-2022 Apple Inc. and the FoundationDB project authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.apple.foundationdb.record.cursors;
+
+import com.apple.foundationdb.annotation.API;
+import com.apple.foundationdb.record.RecordCoreException;
+import com.apple.foundationdb.record.RecordCursor;
+import com.apple.foundationdb.record.RecordCursorResult;
+import com.apple.foundationdb.record.RecordCursorVisitor;
+import com.apple.foundationdb.util.LoggableException;
+
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.Executor;
+import java.util.function.Supplier;
+
+/**
+ * Provide an alternative cursor in case the primary cursor fails. This cursor has an <code>inner</code> cursor that is
+ * used to return the results in the sunny day scenarios, and an alternative provider of a <code>fallback</code> cursor
+ * that will be used if the primary cursor encounters an error.
+ *
+ * Note that there are business rules around when a fallback may happen. For example, because of the way records are
+ * returned to the consumer, once a record is returned from the primary cursor, no fallback may be permitted: This is
+ * done in order to prevent the case where a few records are returned, then a failure happens and the fallback cursor
+ * starts again from the beginning, resulting in duplicate records being returned.
+ * In practice, since many errors are observed when the request is sent to FDB (which coincide with the cursor's
+ * first <code>onNext()</code> call, many such failures will be caught by that first result.
+ *
+ * A note about continuations: As written, the cursor assumes that the <code>inner</code> and <code>fallback</code>
+ * cursors each have their own continuation to pick up from. A future enhancement can be to have this cursor store the
+ * state of the failover in its continuation and then package that with the appropriate inner continuation so that it
+ * can continue from that same state.
+ *
+ * @param <T> the type of cursor result returned by the cursor
+ */
+@API(API.Status.MAINTAINED)
+public class FallbackCursor<T> implements RecordCursor<T> {
+    @Nonnull
+    private final Supplier<RecordCursor<T>> fallbackCursorSupplier;
+    @Nonnull
+    private final Executor executor;
+    @Nonnull
+    private RecordCursor<T> inner;
+    @Nullable
+    private CompletableFuture<RecordCursorResult<T>> nextResultFuture;
+
+    private boolean alreadyFailed = false;
+    private boolean allowedToFail = true;
+
+    /**
+     * Creates a new fallback cursor.
+     *
+     * @param inner the primary (default) cursor to be used when results are successfully returned
+     * @param fallbackCursorSupplier the fallback cursor provider to be used when the primary cursor fails
+     */
+    public FallbackCursor(@Nonnull RecordCursor<T> inner, @Nonnull Supplier<RecordCursor<T>> fallbackCursorSupplier) {
+        this.inner = inner;
+        this.fallbackCursorSupplier = fallbackCursorSupplier;
+        this.executor = inner.getExecutor();
+    }
+
+    @Nonnull
+    @Override
+    public CompletableFuture<RecordCursorResult<T>> onNext() {
+        try {
+            if (nextResultFuture != null && nextResultFuture.isDone() && !nextResultFuture.join().hasNext()) {
+                // This is needed to ensure we return the same terminal value once the cursor is exhausted
+                return nextResultFuture;
+            }
+        } catch (Exception ignored) {
+            // This will happen if the future finished exceptionally - just keep going (client will get
+            // the exception when they observe the future).
+        }
+        // The first stage (handle) will calculate the result of the operation if successful, or replace the inner
+        // with the fallback cursor if failed, and store future to the result in nextResultFuture.
+        // The second stage (thenCompose) will return nextResultFuture once the first stage is done.
+        return inner.onNext().handle((result, throwable) -> {
+            if (throwable == null) {
+                nextResultFuture = CompletableFuture.completedFuture(result);
+                // Cannot fail after the first result was delivered
+                allowedToFail = false;
+            } else {
+                if (alreadyFailed) {
+                    nextResultFuture = CompletableFuture.failedFuture(wrapException("Fallback cursor failed, cannot fallback again", throwable));
+                } else if (!allowedToFail) {
+                    nextResultFuture = CompletableFuture.failedFuture(wrapException("Cannot fallback to alternate cursor since inner already produced a record", throwable));
+                } else {
+                    inner.close();
+                    inner = fallbackCursorSupplier.get();
+                    nextResultFuture = inner.onNext();
+                }
+                alreadyFailed = true;
+            }
+            return null; // return value is ignored by next stage
+        }).thenCompose(vignore -> nextResultFuture);
+    }
+
+    @Override
+    public void close() {
+        inner.close();
+    }
+
+    @Nonnull
+    @Override
+    public Executor getExecutor() {
+        return this.executor;
+    }
+
+    private RecordCursor<T> getInner() {
+        return inner;
+    }
+
+    @Override
+    public boolean accept(@Nonnull RecordCursorVisitor visitor) {
+        if (visitor.visitEnter(this)) {
+            getInner().accept(visitor);
+        }
+        return visitor.visitLeave(this);
+    }
+
+    private Throwable wrapException(final String msg, final Throwable ex) {
+        if (ex instanceof LoggableException) {
+            // In the case of loggable exception, maintain the original exception to simplify exception handling across
+            // fallback and non-fallback executions
+            LoggableException loggableException = (LoggableException)ex;
+            loggableException.addLogInfo("fallback_failed", msg);
+            return ex;
+        } else if ((ex.getCause() != null) && (ex.getCause() instanceof LoggableException)) {
+            // Same but in case the throwable is already wrapping the LoggableException
+            LoggableException loggableException = (LoggableException)(ex.getCause());
+            loggableException.addLogInfo("fallback_failed", msg);
+            return ex;
+        } else {
+            return new FallbackExecutionFailedException(msg, ex);
+        }
+    }
+
+    /**
+     * Exception thrown when the fallback cursor fails.
+     */
+    @SuppressWarnings("java:S110")
+    public static class FallbackExecutionFailedException extends RecordCoreException {
+        public static final long serialVersionUID = 1;
+
+        public FallbackExecutionFailedException(@Nonnull final String msg, @Nullable final Throwable cause) {
+            super(msg, cause);
+        }
+    }
+}

fdb-record-layer-core/src/main/java/com/apple/foundationdb/record/metadata/Index.java
@@ -515,6 +515,28 @@ public class Index {
     }
 
     /**
+     * Get the primary key positions of an index entry.
+     * @param primaryKeyLength the number of elements in the primary key for the record
+     * @return a list of the primary key positions for an entry of this index
+     */
+    @Nonnull
+    public List<Integer> getEntryPrimaryKeyPositions(int primaryKeyLength) {
+        List<Integer> primaryKeys = new ArrayList<>(primaryKeyLength);
+        int columnSize = getColumnSize();
+        if (primaryKeyComponentPositions == null) {
+            for (int i = columnSize; i < (columnSize + primaryKeyLength); i++ ) {
+                primaryKeys.add(i);
+            }
+        } else {
+            int after = columnSize;
+            for (int position : primaryKeyComponentPositions) {
+                primaryKeys.add((position < 0) ? after++ : position);
+            }
+        }
+        return primaryKeys;
+    }
+
+    /**
      * Get the version at which the index was first added.
      * @return the added version
      */

fdb-record-layer-core/src/main/java/com/apple/foundationdb/record/provider/foundationdb/FDBIndexedRawRecord.java
@@ -1,0 +1,106 @@
+/*
+ * FDBIndexedRecord.java
+ *
+ * This source file is part of the FoundationDB open source project
+ *
+ * Copyright 2015-2018 Apple Inc. and the FoundationDB project authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.apple.foundationdb.record.provider.foundationdb;
+
+import com.apple.foundationdb.MappedKeyValue;
+import com.apple.foundationdb.annotation.API;
+import com.apple.foundationdb.record.IndexEntry;
+import com.apple.foundationdb.record.metadata.Index;
+
+import javax.annotation.Nonnull;
+import java.util.Objects;
+
+/**
+ * A raw record that has been loaded via an index. This is the case where the record was loaded via the
+ * {@link IndexMaintainer#scanRemoteFetch}. The raw record may contain versions, splits and other raw record data.
+ */
+@API(API.Status.EXPERIMENTAL)
+public class FDBIndexedRawRecord {
+    @Nonnull
+    private final IndexEntry indexEntry;
+    @Nonnull
+    private final MappedKeyValue rawRecord;
+
+    /**
+     * Wrap a stored record with an index entry that pointed to it. This method is internal, and it generally
+     * should not be called be external clients.
+     *
+     * @param indexEntry the index entry that produced this record
+     * @param rawRecord the {@link FDBRawRecord} containing the record's data
+     */
+    @API(API.Status.INTERNAL)
+    public FDBIndexedRawRecord(@Nonnull IndexEntry indexEntry, @Nonnull MappedKeyValue rawRecord) {
+        this.indexEntry = indexEntry;
+        this.rawRecord = rawRecord;
+    }
+
+    /**
+     * Get the index for this record.
+     * @return the index that contained the entry pointing to this record
+     */
+    @Nonnull
+    public Index getIndex() {
+        return indexEntry.getIndex();
+    }
+
+    /**
+     * Get the index entry for this record.
+     * @return the index entry that pointed to this record
+     */
+    @Nonnull
+    public IndexEntry getIndexEntry() {
+        return indexEntry;
+    }
+
+    @Nonnull
+    public MappedKeyValue getRawRecord() {
+        return rawRecord;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+
+        FDBIndexedRawRecord that = (FDBIndexedRawRecord)o;
+
+        if (!indexEntry.equals(that.indexEntry)) {
+            return false;
+        }
+        return Objects.equals(this.rawRecord, that.rawRecord);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = indexEntry.hashCode();
+        result = 31 * result + Objects.hashCode(rawRecord);
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return indexEntry + " -> " + rawRecord;
+    }
+}

commit 287351048f34f94b59e9002831c4b289b50d8378

Fixes #1672: Merge skew causing compilation failure in RecordQueryIndexPlan
This fixes the merge skew error caused by #1561 relying on a method that #1629 removed.

This fixes #1672.

fdb-record-layer-core/src/main/java/com/apple/foundationdb/record/query/plan/plans/RecordQueryIndexPlan.java
@@ -198,7 +198,7 @@ public class RecordQueryIndexPlan implements RecordQueryPlanWithNoChildren, Reco
         // CommonPrimaryKey is nullable but is protected by the constructor in the case pf index prefetch
         return store.scanIndexRemoteFetch(index, scanBounds, Objects.requireNonNull(getCommonPrimaryKey()), continuation, executeProperties.asScanProperties(isReverse()), IndexOrphanBehavior.ERROR)
                 .map(store::queriedRecord)
-                .map(QueryResult::of);
+                .map(QueryResult::fromQueriedRecord);
     }
 
     @Nonnull