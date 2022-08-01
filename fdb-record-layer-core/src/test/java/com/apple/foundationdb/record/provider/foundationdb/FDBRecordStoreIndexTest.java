bfa805b7be9da4cefb55389cc672ffc71035674b
Josef Ezra <jezra@apple.com>
2022-08-01 15:21:56 -0400

    Apple Scott's requested changes

--- fdb-record-layer-core/src/test/java/com/apple/foundationdb/record/provider/foundationdb/FDBRecordStoreIndexTest.java
+++ fdb-record-layer-core/src/test/java/com/apple/foundationdb/record/provider/foundationdb/FDBRecordStoreIndexTest.java
@@ -1345,6 +1345,7 @@ public class FDBRecordStoreIndexTest extends FDBRecordStoreTestBase {
             openSimpleRecordStore(context);
             recordStore.rebuildIndex(recordStore.getRecordMetaData().getIndex(indexName), null, FDBRecordStore.RebuildIndexReason.TEST).get();
             assertEquals(1, timer.getCount(FDBStoreTimer.Events.REBUILD_INDEX_TEST), "should build new index");
+            assertEquals(1, timer.getCount(FDBStoreTimer.Events.REBUILD_INDEX), "should build new index");
             assertTrue(recordStore.isIndexReadable(indexName));
             commit(context);
         }
@@ -2557,6 +2558,7 @@ public class FDBRecordStoreIndexTest extends FDBRecordStoreTestBase {
             uncheckedOpenRecordStore(context, metaData.getRecordMetaData());
             timer.reset();
             recordStore.checkVersion(null, FDBRecordStoreBase.StoreExistenceCheck.ERROR_IF_NOT_EXISTS).join();
+            assertEquals(1, timer.getCount(FDBStoreTimer.Events.REBUILD_INDEX_FEW_RECORDS));
             assertEquals(1, timer.getCount(FDBStoreTimer.Events.REBUILD_INDEX));
             assertEquals(0, timer.getCount(FDBStoreTimer.Events.REMOVE_FORMER_INDEX));
             context.commit();
@@ -2568,6 +2570,7 @@ public class FDBRecordStoreIndexTest extends FDBRecordStoreTestBase {
             uncheckedOpenRecordStore(context, metaData.getRecordMetaData());
             timer.reset();
             recordStore.checkVersion(null, FDBRecordStoreBase.StoreExistenceCheck.ERROR_IF_NOT_EXISTS).join();
+            assertEquals(0, timer.getCount(FDBStoreTimer.Events.REBUILD_INDEX_FEW_RECORDS));
             assertEquals(0, timer.getCount(FDBStoreTimer.Events.REBUILD_INDEX));
             assertEquals(1, timer.getCount(FDBStoreTimer.Events.REMOVE_FORMER_INDEX));
             context.commit();
@@ -2581,6 +2584,7 @@ public class FDBRecordStoreIndexTest extends FDBRecordStoreTestBase {
             uncheckedOpenRecordStore(context, metaData.getRecordMetaData());
             timer.reset();
             recordStore.checkVersion(null, FDBRecordStoreBase.StoreExistenceCheck.ERROR_IF_NOT_EXISTS).join();
+            assertEquals(0, timer.getCount(FDBStoreTimer.Events.REBUILD_INDEX_FEW_RECORDS));
             assertEquals(0, timer.getCount(FDBStoreTimer.Events.REBUILD_INDEX));
             assertEquals(0, timer.getCount(FDBStoreTimer.Events.REMOVE_FORMER_INDEX));
             context.commit();
