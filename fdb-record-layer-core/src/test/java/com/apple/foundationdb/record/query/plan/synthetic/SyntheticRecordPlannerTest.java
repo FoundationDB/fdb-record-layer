4c6327bcd24ea28e47930083c4bbe61de712a968
Alec Grieser <agrieser@apple.com>
2022-10-17 10:43:22 +0100

    adapt tests from #1866 for new join API

--- fdb-record-layer-core/src/test/java/com/apple/foundationdb/record/query/plan/synthetic/SyntheticRecordPlannerTest.java
+++ fdb-record-layer-core/src/test/java/com/apple/foundationdb/record/query/plan/synthetic/SyntheticRecordPlannerTest.java
@@ -43,6 +43,7 @@ import com.apple.foundationdb.record.metadata.JoinedRecordTypeBuilder;
 import com.apple.foundationdb.record.metadata.Key;
 import com.apple.foundationdb.record.metadata.expressions.FunctionKeyExpression;
 import com.apple.foundationdb.record.metadata.expressions.GroupingKeyExpression;
+import com.apple.foundationdb.record.metadata.expressions.InvertibleFunctionKeyExpression;
 import com.apple.foundationdb.record.metadata.expressions.KeyExpression;
 import com.apple.foundationdb.record.metadata.expressions.TupleFieldsHelper;
 import com.apple.foundationdb.record.provider.foundationdb.FDBDatabase;
@@ -97,6 +98,7 @@ import java.util.stream.Collectors;
 import static com.apple.foundationdb.record.metadata.Key.Expressions.concat;
 import static com.apple.foundationdb.record.metadata.Key.Expressions.concatenateFields;
 import static com.apple.foundationdb.record.metadata.Key.Expressions.field;
+import static com.apple.foundationdb.record.metadata.Key.Expressions.function;
 import static com.apple.foundationdb.record.metadata.Key.Expressions.recordType;
 import static com.apple.foundationdb.record.query.plan.ScanComparisons.range;
 import static com.apple.foundationdb.record.query.plan.cascades.matching.structure.RecordQueryPlanMatchers.coveringIndexPlan;
@@ -1042,7 +1044,7 @@ public class SyntheticRecordPlannerTest {
 
     @Test
     void joinOnNestedKeysWithDifferentTypes() throws Exception {
-        metaDataBuilder.getRecordType("CustomerWithHeader").setPrimaryKey(Key.Expressions.concat(field("___header").nest("z_key"), field("___header").nest("rec_id")));
+        metaDataBuilder.getRecordType("CustomerWithHeader").setPrimaryKey(Key.Expressions.concat(field("___header").nest("z_key"), field("___header").nest("int_rec_id")));
         metaDataBuilder.getRecordType("OrderWithHeader").setPrimaryKey(Key.Expressions.concat(field("___header").nest("z_key"), field("___header").nest("rec_id")));
 
         final JoinedRecordTypeBuilder joined = metaDataBuilder.addJoinedRecordType("MultiNestedFieldJoin");
@@ -1051,10 +1053,8 @@ public class SyntheticRecordPlannerTest {
 
         joined.addJoin("order",
                 field("custRef").nest("string_value"),
-                field("custRef").nest(Key.Expressions.function("decanonicalize", field("string_value"))),
                 "cust",
-                field("___header").nest("int_rec_id"),
-                field("___header").nest(Key.Expressions.function("canonicalize", field("int_rec_id")))
+                function("canonicalize", field("___header").nest("int_rec_id"))
         );
 
         metaDataBuilder.addIndex(joined, new Index("joinNestedConcat", concat(
@@ -1243,8 +1243,7 @@ public class SyntheticRecordPlannerTest {
         @Override
         public List<FunctionKeyExpression.Builder> getBuilders() {
             return Lists.newArrayList(
-                    new FunctionKeyExpression.BiFunctionBuilder("canonicalize", StringCanonicalizerFunction::new),
-                    new FunctionKeyExpression.BiFunctionBuilder("decanonicalize", StringDeCanonicalizerFunction::new)
+                    new FunctionKeyExpression.BiFunctionBuilder("canonicalize", StringCanonicalizerFunction::new)
             );
         }
     }
@@ -1252,11 +1251,16 @@ public class SyntheticRecordPlannerTest {
     /**
      * Converts an int into an arbitrary canonical form.
      */
-    public static class StringCanonicalizerFunction extends FunctionKeyExpression {
+    public static class StringCanonicalizerFunction extends InvertibleFunctionKeyExpression {
         private static final ObjectPlanHash BASE_HASH = new ObjectPlanHash("String-Canonicalizer-Function");
 
         protected StringCanonicalizerFunction(@Nonnull final String name, @Nonnull final KeyExpression arguments) {
             super(name, arguments);
+        }
+
+        @Override
+        public boolean isInjective() {
+            return true;
         }
 
         @Override
@@ -1288,54 +1292,13 @@ public class SyntheticRecordPlannerTest {
         }
 
         @Override
-        public boolean createsDuplicates() {
-            return false;
-        }
-
-        @Override
-        public int getColumnSize() {
-            return 1;
-        }
-    }
-
-    /**
-     * converts a "canonical" string into an integer. Inverse of StringCanonicalizerFunction.
-     */
-    public static class StringDeCanonicalizerFunction extends FunctionKeyExpression {
-        private static final ObjectPlanHash BASE_HASH = new ObjectPlanHash("String-DeCanonicalizer-Function");
-
-        protected StringDeCanonicalizerFunction(@Nonnull final String name, @Nonnull final KeyExpression arguments) {
-            super(name, arguments);
-        }
-
-        @Override
-        public int planHash(@Nonnull final PlanHashKind hashKind) {
-            return super.basePlanHash(hashKind, BASE_HASH);
-        }
-
-        @Override
-        public int queryHash(@Nonnull final QueryHashKind hashKind) {
-            return super.baseQueryHash(hashKind, BASE_HASH);
-        }
-
-        @Override
-        public int getMinArguments() {
-            return 1;
-        }
-
-        @Override
-        public int getMaxArguments() {
-            return 1;
-        }
-
-        @Nonnull
-        @Override
-        public <M extends Message> List<Key.Evaluated> evaluateFunction(@Nullable final FDBRecord<M> record,
-                                                                        @Nullable final Message message,
-                                                                        @Nonnull final Key.Evaluated arguments) {
-            String canonicalForm = arguments.getString(0);
-            long strippedForm = Long.parseLong(canonicalForm.substring(2));
-            return Collections.singletonList(Key.Evaluated.scalar(strippedForm));
+        protected List<Key.Evaluated> evaluateInverseInternal(@Nonnull final Key.Evaluated result) {
+            String canonicalForm = result.getString(0);
+            if (canonicalForm != null && canonicalForm.startsWith("i:")) {
+                return Collections.singletonList(Key.Evaluated.scalar(Long.parseLong(canonicalForm, 2, canonicalForm.length(), 10)));
+            } else {
+                return Collections.singletonList(result);
+            }
         }
 
         @Override
