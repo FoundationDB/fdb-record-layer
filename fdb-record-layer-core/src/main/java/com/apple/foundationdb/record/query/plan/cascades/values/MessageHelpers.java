7d32c10327d875e75f4881463fa39b848cafe731
Normen Seemann <nseemann@apple.com>
2022-11-18 10:02:15 -0800

    fix bug in deepCopy

--- fdb-record-layer-core/src/main/java/com/apple/foundationdb/record/query/plan/cascades/values/MessageHelpers.java
+++ fdb-record-layer-core/src/main/java/com/apple/foundationdb/record/query/plan/cascades/values/MessageHelpers.java
@@ -252,7 +252,11 @@ public class MessageHelpers {
      */
     @Nonnull
     @SuppressWarnings("PMD.CompareObjectsWithEquals")
-    public static DynamicMessage deepCopy(@Nonnull final Descriptors.Descriptor targetDescriptor, @Nonnull final Message message) {
+    public static Message deepCopyMessageIfNeeded(@Nonnull final Descriptors.Descriptor targetDescriptor, @Nonnull final Message message) {
+        if (targetDescriptor == message.getDescriptorForType()) {
+            return message;
+        }
+
         final var builder = DynamicMessage.newBuilder(targetDescriptor);
         for (final var entry : message.getAllFields().entrySet()) {
             final Descriptors.FieldDescriptor field = entry.getKey();
@@ -272,7 +276,7 @@ public class MessageHelpers {
                     final var mergedObject =
                             DynamicMessage.newBuilder(targetField.getMessageType())
                                     .mergeFrom(existingValue)
-                                    .mergeFrom((Message)entry.getValue())
+                                    .mergeFrom(deepCopyMessageIfNeeded(field.getMessageType(), (Message)entry.getValue()))
                                     .build();
                     builder.setField(targetField, mergedObject);
                 }
