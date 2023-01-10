f89174d75f7bf93448022a110573d7de84751086
Youssef Hatem <y_hatem@apple.com>
2023-01-10 11:58:50 +0000

    * address comment.

--- fdb-record-layer-core/src/main/java/com/apple/foundationdb/record/util/ProtoUtils.java
+++ fdb-record-layer-core/src/main/java/com/apple/foundationdb/record/util/ProtoUtils.java
@@ -43,7 +43,7 @@ public class ProtoUtils {
      * @return a unique type name.
      */
     public static String uniqueCorrelationName() {
-        return uniqueName("__corr__");
+        return uniqueName("q");
     }
 
     private static String uniqueName(String prefix) {
