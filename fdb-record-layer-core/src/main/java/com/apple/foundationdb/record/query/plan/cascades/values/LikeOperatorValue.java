b42a886684373dda1f54376bd2a414a1b3c2b120
Arnaud Lacurie <a_lacurie@apple.com>
2023-07-05 14:04:15 +0100

    Address comments

--- fdb-record-layer-core/src/main/java/com/apple/foundationdb/record/query/plan/cascades/values/LikeOperatorValue.java
+++ fdb-record-layer-core/src/main/java/com/apple/foundationdb/record/query/plan/cascades/values/LikeOperatorValue.java
@@ -1,5 +1,5 @@
 /*
- * ScalarFunctionValue.java
+ * LikeOperatorValue.java
  *
  * This source file is part of the FoundationDB open source project
  *
--- fdb-record-layer-core/src/main/java/com/apple/foundationdb/record/query/plan/cascades/values/PatternForLikeValue.java
+++ fdb-record-layer-core/src/main/java/com/apple/foundationdb/record/query/plan/cascades/values/PatternForLikeValue.java
@@ -1,5 +1,5 @@
 /*
- * ScalarFunctionValue.java
+ * PatternForLikeValue.java
  *
  * This source file is part of the FoundationDB open source project
  *
