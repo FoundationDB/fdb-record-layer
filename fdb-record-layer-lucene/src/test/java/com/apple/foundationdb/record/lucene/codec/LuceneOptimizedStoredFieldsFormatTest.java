6f08c458f839b4f8aecbb3aac5b9cfb53eb042bb
Scott Dugas <sdugas@apple.com>
2023-08-30 16:43:24 -0400

    Hacky attempt to get testRamBytesUsed to work
    This did seem to work for some other seeds, but it doesn't appear
    to work for @Seed("DB2B502BC81C6045") because the StoredFields is
    lazy, and it doesn't open the IndexInput until it warms the whole
    thing

--- fdb-record-layer-lucene/src/test/java/com/apple/foundationdb/record/lucene/codec/LuceneOptimizedStoredFieldsFormatTest.java
+++ fdb-record-layer-lucene/src/test/java/com/apple/foundationdb/record/lucene/codec/LuceneOptimizedStoredFieldsFormatTest.java
@@ -1,13 +1,23 @@
 package com.apple.foundationdb.record.lucene.codec;
 
 import com.apple.foundationdb.record.provider.foundationdb.FDBDatabaseFactory;
+import com.carrotsearch.randomizedtesting.annotations.Seed;
 import com.carrotsearch.randomizedtesting.annotations.ThreadLeakFilters;
 import org.apache.lucene.codecs.Codec;
 import org.apache.lucene.codecs.compressing.CompressingCodec;
 import org.apache.lucene.index.BaseStoredFieldsFormatTestCase;
 
+import java.io.IOException;
 import java.util.Random;
 
+// Tip: if you see a failure that has something like:
+// 	at __randomizedtesting.SeedInfo.seed([C185081D42F0F43C]:0)
+// or
+// 	at __randomizedtesting.SeedInfo.seed([C185081D42F0F43C:33261A5D888FEB6A]:0)
+// You can add
+// @Seed("C185081D42F0F43C")
+// to rerun the test class with the same seed. That will work even if you then only run one of the tests
+@Seed("DB2B502BC81C6045")
 @ThreadLeakFilters(defaultFilters = true, filters = {
         FDBThreadFilter.class
 })
@@ -24,6 +34,16 @@ public class LuceneOptimizedStoredFieldsFormatTest extends BaseStoredFieldsForma
             return new LuceneOptimizedCodec();
         } else {
             return CompressingCodec.randomInstance(new Random());
+        }
+    }
+
+    @Override
+    public void testRamBytesUsed() throws IOException {
+        TestFDBDirectory.setFullBufferToSurviveDeletes(true);
+        try {
+            super.testRamBytesUsed();
+        } finally {
+            TestFDBDirectory.setFullBufferToSurviveDeletes(false);
         }
     }
 }
--- fdb-record-layer-lucene/src/test/java/com/apple/foundationdb/record/lucene/codec/TestFDBDirectory.java
+++ fdb-record-layer-lucene/src/test/java/com/apple/foundationdb/record/lucene/codec/TestFDBDirectory.java
@@ -4,10 +4,71 @@ import com.apple.foundationdb.record.lucene.directory.FDBDirectory;
 import com.apple.foundationdb.record.provider.foundationdb.FDBDatabaseFactory;
 import com.apple.foundationdb.subspace.Subspace;
 import com.apple.foundationdb.tuple.Tuple;
+import org.apache.lucene.store.ByteBuffersDataInput;
+import org.apache.lucene.store.ByteBuffersIndexInput;
+import org.apache.lucene.store.IOContext;
+import org.apache.lucene.store.IndexInput;
+import org.hamcrest.Matchers;
+
+import javax.annotation.Nonnull;
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.util.List;
+
+import static org.hamcrest.MatcherAssert.assertThat;
 
 public class TestFDBDirectory extends FDBDirectory {
+    private static boolean fullBufferToSurviveDeletes;
+
     public TestFDBDirectory() {
         super(new Subspace(Tuple.from("record-test", "unit", "lucene")),
                 FDBDatabaseFactory.instance().getDatabase().openContext());
     }
+
+    /**
+     * Buffer all {@link IndexInput} content on {@code #openInput} so that it can continue to be read after the
+     * associated file has been deleted.
+     * <p>
+     *     Some tests depend on the following behavior, that is hopefully only a test behavior, and not a real Lucene
+     *     dependency:
+     *     <ol>
+     *         <li>open an {@code IndexInput}</li>
+     *         <li>delete the associated file</li>
+     *         <li>read from the {@code IndexInput}</li>
+     *     </ol>
+     *     This works with {@link org.apache.lucene.store.RAMDirectory} because the {@code IndexInput} that returns just
+     *     contains the whole file in a byte array. It works for the various {@link org.apache.lucene.store.FSDirectory}s
+     *     because, at least on a mac, if you open a file handle, and then delete the associated file, you can continue
+     *     to read from the file (at least as much as is cached).
+     * </p>
+     * <p>
+     *     Right now it only fails for one test, and that test seems suspect, and looks as though it is just a weird
+     *     assumption of the test. Anytime we add a new usage of this we should make sure that it's not a sign that
+     *     Lucene itself has an assumption that you can read files after they are deleted, and thus we could break
+     *     lucene.
+     * </p>
+     * <p>
+     *     If you set this to {@code true} make sure to set it back to {@code false} in a {@code finally} block.
+     * </p>
+     * @param newValue {@code true} to buffer the contents of all returned {@code IndexInput}s.
+     */
+    public static void setFullBufferToSurviveDeletes(final boolean newValue) {
+        TestFDBDirectory.fullBufferToSurviveDeletes = newValue;
+    }
+
+    @Nonnull
+    @Override
+    public IndexInput openInput(@Nonnull final String name, @Nonnull final IOContext ioContext) throws IOException {
+        final IndexInput indexInput = super.openInput(name, ioContext);
+        if (fullBufferToSurviveDeletes) {
+            assertThat("Avoid buffering more than 10MB",
+                    indexInput.length(), Matchers.lessThan(10_000_000L));
+            final byte[] bytes = new byte[(int)indexInput.length()];
+            indexInput.readBytes(bytes, 0, (int)indexInput.length());
+            indexInput.close();
+            return new ByteBuffersIndexInput(new ByteBuffersDataInput(List.of(ByteBuffer.wrap(bytes))), name);
+        }
+        return indexInput;
+    }
+
 }
