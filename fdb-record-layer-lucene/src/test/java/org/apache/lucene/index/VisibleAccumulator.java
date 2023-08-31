d7f8d30d0337a5a9d5b4b08e9f17efec40c8f2b0
Scott Dugas <sdugas@apple.com>
2023-08-31 11:13:46 -0400

    Override testRamBytes, warming before the merge
    I'm making a bet that Lucene proper doesn't depend on being able
    to read a file after it has been deleted, and that this is only
    for this test code. So I changed the test to behave in a more sane
    way

---
+++ fdb-record-layer-lucene/src/test/java/org/apache/lucene/index/VisibleAccumulator.java
@@ -0,0 +1,10 @@
+package org.apache.lucene.index;
+
+public class VisibleAccumulator extends BaseIndexFileFormatTestCase.Accumulator {
+    /**
+     * Constructs a new object.
+     */
+    public VisibleAccumulator(final Object root) {
+        super(root);
+    }
+}
