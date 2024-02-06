6757c59d9faa6141f22a6e17a1623f84bb523997
Arnaud Lacurie <a_lacurie@apple.com>
2024-02-06 18:06:11 +0000

    Bump Shared Dependencies to 2410B41 (REL-647)
    * Bump Shared Dependencies to 2410B41
    
    * fix yaml-tests
    
    ---------
    
    Co-authored-by: Pranjal Gupta <pgupta32@apple.com>

--- yaml-tests/src/test/java/JDBCYamlIntegrationTests.java
+++ yaml-tests/src/test/java/JDBCYamlIntegrationTests.java
@@ -30,6 +30,7 @@ import org.apache.logging.log4j.Logger;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
 
 import javax.annotation.Nullable;
 import java.net.URI;
@@ -86,103 +87,98 @@ public class JDBCYamlIntegrationTests extends YamlIntegrationTests {
     }
 
     @Override
+    @Test
     @Disabled("The field-index-tests-proto.yaml has 'load template' which is not supported")
     public void fieldIndexTestsProto() throws Exception {
         doRun("field-index-tests-proto.yamsql");
     }
 
     @Override
+    @Test
     @Disabled("The standard-tests-proto.yaml has 'load template' which is not supported")
     public void standardTestsWithProto() throws Exception {
         super.standardTestsWithProto();
     }
 
     @Override
+    @Test
     @Disabled("The standard-tests-metadata.yaml has 'load template' which is not supported")
     public void standardTestsWithMetaData() throws Exception {
         super.standardTestsWithMetaData();
     }
 
     @Override
+    @Test
     @Disabled("The disabled-index-tests-proto.yaml has 'load schema template' which is not supported")
     public void disabledIndexWithProto() throws Exception {
         super.disabledIndexWithProto();
     }
 
+    @Override
+    @Test
     @Disabled("The deprecated-fields-tests-proto.yaml has 'load schema template' which is not supported")
     public void deprecatedFieldsTestsWithProto() throws Exception {
         super.deprecatedFieldsTestsWithProto();
     }
 
     @Override
-    public void subqueryTests() throws Exception {
-        super.subqueryTests();
-    }
-
-    @Override
-    public void primaryKey() throws Exception {
-        super.primaryKey();
-    }
-
-    @Override
-    void booleanTypes() throws Exception {
-        super.booleanTypes();
-    }
-
-    @Override
-    public void updateDeleteReturning() throws Exception {
-        doRun("update-delete-returning.yamsql");
-    }
-
-    @Override
+    @Test
     @Disabled("TODO: Flakey")
     public void orderBy() throws Exception {
         super.orderBy();
     }
 
     @Override
+    @Test
     @Disabled("TODO: Flakey")
     public void scenarioTests() throws Exception {
         super.scenarioTests();
     }
 
     @Override
+    @Test
     @Disabled("Requires continuation support")
     public void versionsTests() throws Exception {
         super.versionsTests();
     }
 
     @Override
+    @Test
     @Disabled("TODO: Need to work on supporting labels")
     public void limit() throws Exception {
         super.limit();
     }
 
     @Override
+    @Test
     @Disabled("TODO")
     public void selectAStar() throws Exception {
         super.selectAStar();
     }
 
     @Override
+    @Test
     @Disabled("TODO: Flakey")
     public void aggregateIndexTestsCount() throws Exception {
         super.aggregateIndexTestsCount();
     }
 
     @Override
+    @Test
     @Disabled("TODO: Flakey")
     public void joinTests() throws Exception {
         super.joinTests();
     }
 
     @Override
+    @Test
     @Disabled("TODO: Flakey")
     public void nested() throws Exception {
         super.nested();
     }
 
     @Override
+    @Test
     @Disabled("TODO: Flakey")
     public void showcasingTests() throws Exception {
         super.showcasingTests();
