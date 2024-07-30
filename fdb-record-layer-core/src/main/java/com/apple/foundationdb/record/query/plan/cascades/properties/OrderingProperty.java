cb0b64f5cfdd023f27004f4796710cfcc880c15f
Mike McMahon <mike_mcmahon@apple.com>
2024-07-29 19:25:47 -0700

    Add a plan element to use the new cursor.

--- fdb-record-layer-core/src/main/java/com/apple/foundationdb/record/query/plan/cascades/properties/OrderingProperty.java
+++ fdb-record-layer-core/src/main/java/com/apple/foundationdb/record/query/plan/cascades/properties/OrderingProperty.java
@@ -63,6 +63,7 @@ import com.apple.foundationdb.record.query.plan.plans.RecordQueryPlan;
 import com.apple.foundationdb.record.query.plan.plans.RecordQueryPlanVisitor;
 import com.apple.foundationdb.record.query.plan.plans.RecordQueryPredicatesFilterPlan;
 import com.apple.foundationdb.record.query.plan.plans.RecordQueryRangePlan;
+import com.apple.foundationdb.record.query.plan.plans.RecordQueryRecursivePlan;
 import com.apple.foundationdb.record.query.plan.plans.RecordQueryScanPlan;
 import com.apple.foundationdb.record.query.plan.plans.RecordQueryScoreForRankPlan;
 import com.apple.foundationdb.record.query.plan.plans.RecordQuerySelectorPlan;
@@ -612,6 +613,12 @@ public class OrderingProperty implements PlanProperty<Ordering> {
         @Override
         public Ordering visitSortPlan(@Nonnull final RecordQuerySortPlan element) {
             // TODO
+            return Ordering.empty();
+        }
+
+        @Nonnull
+        @Override
+        public Ordering visitRecursivePlan(@Nonnull final RecordQueryRecursivePlan recursivePlan) {
             return Ordering.empty();
         }
 
