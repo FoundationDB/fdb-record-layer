f5c4da0ae9764b2d3b5172bdab7a02cfba65b26c
ohad <ozeliger@apple.com>
2025-01-16 18:20:31 -0500

    Merge from main

--- yaml-tests/src/main/java/com/apple/foundationdb/relational/yamltests/command/QueryExecutor.java
+++ yaml-tests/src/main/java/com/apple/foundationdb/relational/yamltests/command/QueryExecutor.java
@@ -77,10 +77,6 @@ public class QueryExecutor {
             logger.debug("👍 Skipped for query '{}'", this.toString());
             return continuation;
         }
-        if (!config.shouldExecute()) {
-            logger.debug("👍 Skipped for query '{}'", this.toString());
-            return continuation;
-        }
         final var currentQuery = config.decorateQuery(query);
         if (continuation == null || continuation.atBeginning()) {
             return executeQuery(connection, config, currentQuery, checkCache, maxRows);
