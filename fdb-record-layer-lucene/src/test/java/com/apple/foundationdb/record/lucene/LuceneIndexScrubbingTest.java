d0c7c91ac5accf5610c31391fa05009b3e8d38e8
Josef Ezra <jezra@apple.com>
2025-01-17 18:15:58 -0500

    Implement Scott's requested changes

--- fdb-record-layer-lucene/src/test/java/com/apple/foundationdb/record/lucene/LuceneIndexScrubbingTest.java
+++ fdb-record-layer-lucene/src/test/java/com/apple/foundationdb/record/lucene/LuceneIndexScrubbingTest.java
@@ -20,6 +20,8 @@
 
 package com.apple.foundationdb.record.lucene;
 
+import com.apple.foundationdb.record.IndexState;
+import com.apple.foundationdb.record.RecordMetaDataProvider;
 import com.apple.foundationdb.record.lucene.directory.InjectedFailureRepository;
 import com.apple.foundationdb.record.lucene.directory.MockedLuceneIndexMaintainerFactory;
 import com.apple.foundationdb.record.lucene.directory.TestingIndexMaintainerRegistry;
@@ -27,18 +29,35 @@ import com.apple.foundationdb.record.metadata.Index;
 import com.apple.foundationdb.record.provider.foundationdb.FDBRecordContext;
 import com.apple.foundationdb.record.provider.foundationdb.FDBRecordStore;
 import com.apple.foundationdb.record.provider.foundationdb.OnlineIndexScrubber;
+import com.apple.foundationdb.record.provider.foundationdb.keyspace.KeySpacePath;
 import com.apple.foundationdb.record.query.plan.QueryPlanner;
 import com.apple.foundationdb.record.util.pair.Pair;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import javax.annotation.Nonnull;
+import java.util.Map;
+import java.util.stream.Stream;
 
 import static com.apple.foundationdb.record.lucene.LuceneIndexTestUtils.SIMPLE_TEXT_SUFFIXES_WITH_PRIMARY_KEY_SEGMENT_INDEX;
 import static com.apple.foundationdb.record.lucene.LuceneIndexTestUtils.createComplexDocument;
 import static com.apple.foundationdb.record.lucene.LuceneIndexTestUtils.createSimpleDocument;
-import static com.apple.foundationdb.record.lucene.directory.InjectedFailureRepository.Flags.LUCENE_GET_PRIMARY_KEY_SEGMENT_INDEX_FORCE_NULL;
+import static com.apple.foundationdb.record.lucene.directory.InjectedFailureRepository.Flags.LUCENE_MAINTAINER_SKIP_INDEX_UPDATE;
 import static com.apple.foundationdb.record.provider.foundationdb.indexes.TextIndexTestUtils.SIMPLE_DOC;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class LuceneIndexScrubbingTest extends FDBLuceneTestBase {
+
+    TestingIndexMaintainerRegistry registry;
+
+    @BeforeEach
+    public void beforeEach() {
+        registry = new TestingIndexMaintainerRegistry();
+    }
 
     private void rebuildIndexMetaData(final FDBRecordContext context, final String document, final Index index) {
         Pair<FDBRecordStore, QueryPlanner> pair = LuceneIndexTestUtils.rebuildIndexMetaData(context, path, document, index, isUseCascadesPlanner());
@@ -46,9 +65,61 @@ class LuceneIndexScrubbingTest extends FDBLuceneTestBase {
         this.planner = pair.getRight();
     }
 
+    public static Stream<Arguments> threeBooleanArgs() {
+        return Stream.of(false, true)
+                .flatMap(isSynthetic -> Stream.of(false, true)
+                        .flatMap(isPartitioned -> Stream.of(false, true)
+                                .map(isGrouped -> Arguments.of(isSynthetic, isGrouped, isPartitioned))));
+    }
+
+    @Nonnull
+    protected FDBRecordStore.Builder getStoreBuilderWithRegistry(@Nonnull FDBRecordContext context,
+                                                                 @Nonnull RecordMetaDataProvider metaData,
+                                                                 @Nonnull final KeySpacePath path) {
+        return super.getStoreBuilder(context, metaData, path).setIndexMaintainerRegistry(registry);
+    }
+
+    @ParameterizedTest
+    @MethodSource("threeBooleanArgs")
+    void luceneIndexScrubMissingDataModelNoIssues(boolean isSynthetic, boolean isGrouped, boolean isPartitioned) {
+        // Scrub a valid index, expect zero issues
+        final long seed = 7L;
+
+        final LuceneIndexTestDataModel dataModel = new LuceneIndexTestDataModel.Builder(seed, this::getStoreBuilderWithRegistry, pathManager)
+                .setIsGrouped(isGrouped)
+                .setIsSynthetic(isSynthetic)
+                .setPartitionHighWatermark(isPartitioned ? 10 : 0)
+                .build();
+
+        try (final FDBRecordContext context = openContext()) {
+            FDBRecordStore store = dataModel.createOrOpenRecordStore(context);
+            dataModel.saveRecords(15, 1007, context, 1);
+            context.commit();
+        }
+
+        try (final FDBRecordContext context = openContext()) {
+            FDBRecordStore store = dataModel.createOrOpenRecordStore(context);
+            boolean atLeastOnce = false;
+            for (Map.Entry<Index, IndexState> entry : store.getAllIndexStates().entrySet()) {
+                Index index = entry.getKey();
+                IndexState indexState = entry.getValue();
+                if (index.getType().equalsIgnoreCase("lucene") && indexState.equals(IndexState.READABLE)) {
+                    atLeastOnce = true;
+                    try (OnlineIndexScrubber indexScrubber = OnlineIndexScrubber.newBuilder()
+                            .setRecordStore(store)
+                            .setIndex(index)
+                            .build()) {
+                        final long missingEntriesCount = indexScrubber.scrubMissingIndexEntries();
+                        assertEquals(0, missingEntriesCount);
+                    }
+                }
+            }
+            assertTrue(atLeastOnce);
+        }
+    }
+
     @Test
     void luceneIndexScrubMissingSimpleNoIssues() {
-        // Scrub a valid index, expect zero issues
         Index index = SIMPLE_TEXT_SUFFIXES_WITH_PRIMARY_KEY_SEGMENT_INDEX;
         try (final FDBRecordContext context = openContext()) {
             // Write some records
@@ -79,6 +150,61 @@ class LuceneIndexScrubbingTest extends FDBLuceneTestBase {
         }
     }
 
+
+    @ParameterizedTest
+    @MethodSource("threeBooleanArgs")
+    void luceneIndexScrubMissingDataModel(boolean isSynthetic, boolean isGrouped, boolean isPartitioned) {
+        // Scrub a valid index, expect zero issues
+        final long seed = 207L;
+
+        final LuceneIndexTestDataModel dataModel = new LuceneIndexTestDataModel.Builder(seed, this::getStoreBuilderWithRegistry, pathManager)
+                .setIsGrouped(isGrouped)
+                .setIsSynthetic(isSynthetic)
+                .setPartitionHighWatermark(isPartitioned ? 10 : 0)
+                .build();
+
+        try (final FDBRecordContext context = openContext()) {
+            context.commit();
+        }
+
+        final InjectedFailureRepository injectedFailures = new InjectedFailureRepository();
+        registry.overrideFactory(new MockedLuceneIndexMaintainerFactory(injectedFailures));
+
+        try (final FDBRecordContext context = openContext()) {
+            // Write some documents
+            dataModel.saveRecords(15, 1007, context, 1);
+            // Trigger failures
+            injectedFailures.setFlag(LUCENE_MAINTAINER_SKIP_INDEX_UPDATE);
+            // Some overwrites (with index update failures)
+            dataModel.saveRecords(5, 1007, context, 1);
+            // some new (with index update failures)
+            dataModel.saveRecords(5, 7007, context, 1);
+            injectedFailures.setFlag(LUCENE_MAINTAINER_SKIP_INDEX_UPDATE, false);
+            context.commit();
+        }
+
+        try (final FDBRecordContext context = openContext()) {
+            FDBRecordStore store = dataModel.createOrOpenRecordStore(context);
+            boolean atLeastOnce = false;
+            for (Map.Entry<Index, IndexState> entry : store.getAllIndexStates().entrySet()) {
+                Index index = entry.getKey();
+                IndexState indexState = entry.getValue();
+                if (index.getType().equalsIgnoreCase("lucene") && indexState.equals(IndexState.READABLE)) {
+                    atLeastOnce = true;
+                    try (OnlineIndexScrubber indexScrubber = OnlineIndexScrubber.newBuilder()
+                            .setRecordStore(store)
+                            .setIndex(index)
+                            .build()) {
+                        final long missingEntriesCount = indexScrubber.scrubMissingIndexEntries();
+                        assertEquals(10, missingEntriesCount);
+                    }
+                }
+            }
+            assertTrue(atLeastOnce);
+        }
+    }
+
+
     @Test
     void luceneIndexScrubMissingSimple() {
         // Scrub an index with missing entries
@@ -96,7 +222,6 @@ class LuceneIndexScrubbingTest extends FDBLuceneTestBase {
         }
 
         final InjectedFailureRepository injectedFailures = new InjectedFailureRepository();
-        final TestingIndexMaintainerRegistry registry = new TestingIndexMaintainerRegistry();
         registry.overrideFactory(new MockedLuceneIndexMaintainerFactory(injectedFailures));
 
         try (final FDBRecordContext context = openContext()) {
@@ -104,10 +229,11 @@ class LuceneIndexScrubbingTest extends FDBLuceneTestBase {
             Pair<FDBRecordStore, QueryPlanner> pair = LuceneIndexTestUtils.rebuildIndexMetaData(context, path, SIMPLE_DOC, index, isUseCascadesPlanner(), registry);
             this.recordStore = pair.getLeft();
             this.planner = pair.getRight();
-            injectedFailures.setFlag(LUCENE_GET_PRIMARY_KEY_SEGMENT_INDEX_FORCE_NULL);
+            injectedFailures.setFlag(LUCENE_MAINTAINER_SKIP_INDEX_UPDATE);
             recordStore.saveRecord(createSimpleDocument(1623L, ENGINEER_JOKE, 2));
             recordStore.saveRecord(createSimpleDocument(7771547L, WAYLON, 1));
             recordStore.saveRecord(createSimpleDocument(7772222L, WAYLON + " who?", 1));
+            injectedFailures.setFlag(LUCENE_MAINTAINER_SKIP_INDEX_UPDATE, false);
             context.commit();
         }
 
