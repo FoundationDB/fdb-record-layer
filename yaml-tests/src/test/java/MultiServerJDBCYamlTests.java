0b4b691a67317f3f59b617a327613dab10ac0ada
ohad <ozeliger@apple.com>
2025-02-04 13:37:37 -0500

    PR Comments

--- yaml-tests/src/test/java/MultiServerJDBCYamlTests.java
+++ yaml-tests/src/test/java/MultiServerJDBCYamlTests.java
@@ -87,145 +87,145 @@ public abstract class MultiServerJDBCYamlTests extends JDBCInProcessYamlIntegrat
         }
 
         @Override
-        @Disabled("Infinite continuation loop")
+        @Disabled("Infinite continuation loop (https://github.com/FoundationDB/fdb-record-layer/issues/3095)")
         public void aggregateEmptyTable() throws Exception {
             super.aggregateEmptyTable();
         }
 
         @Override
-        @Disabled("Continuation verification")
+        @Disabled("Continuation verification (https://github.com/FoundationDB/fdb-record-layer/issues/3096)")
         public void aggregateIndexTests() throws Exception {
             super.aggregateIndexTests();
         }
 
         @Override
-        @Disabled("Infinite continuation loop")
+        @Disabled("Infinite continuation loop (https://github.com/FoundationDB/fdb-record-layer/issues/3095)")
         public void aggregateIndexTestsCount() throws Exception {
             super.aggregateIndexTestsCount();
         }
 
         @Override
-        @Disabled("Infinite continuation loop")
+        @Disabled("Infinite continuation loop (https://github.com/FoundationDB/fdb-record-layer/issues/3095)")
         public void aggregateIndexTestsCountEmpty() throws Exception {
             super.aggregateIndexTestsCountEmpty();
         }
 
         @Override
-        @Disabled("Continuation error")
+        @Disabled("Continuation error (https://github.com/FoundationDB/fdb-record-layer/issues/3097)")
         public void bitmap() throws Exception {
             super.bitmap();
         }
 
         @Override
-        @Disabled("Infinite continuation")
+        @Disabled("Infinite continuation (https://github.com/FoundationDB/fdb-record-layer/issues/3095)")
         void booleanTypes() throws Exception {
             super.booleanTypes();
         }
 
         @Override
-        @Disabled("Continuation verification")
+        @Disabled("Continuation verification (https://github.com/FoundationDB/fdb-record-layer/issues/3096)")
         void catalog() throws Exception {
             super.catalog();
         }
 
         @Override
-        @Disabled("Continuation verification")
+        @Disabled("Continuation verification (https://github.com/FoundationDB/fdb-record-layer/issues/3096)")
         void createDrop() throws Exception {
             super.createDrop();
         }
 
         @Override
-        @Disabled("Continuation verification")
+        @Disabled("Continuation verification (https://github.com/FoundationDB/fdb-record-layer/issues/3096)")
         public void fieldIndexTestsProto() throws Exception {
             super.fieldIndexTestsProto();
         }
 
         @Override
-        @Disabled("Continuation mismatch")
+        @Disabled("Continuation mismatch (https://github.com/FoundationDB/fdb-record-layer/issues/3098)")
         void functions() throws Exception {
             super.functions();
         }
 
         @Override
-        @Disabled("Infinite continuation loop")
+        @Disabled("Infinite continuation loop (https://github.com/FoundationDB/fdb-record-layer/issues/3095)")
         public void groupByTests() throws Exception {
             super.groupByTests();
         }
 
         @Override
-        @Disabled("Infinite continuation loop")
+        @Disabled("Infinite continuation loop (https://github.com/FoundationDB/fdb-record-layer/issues/3095)")
         public void joinTests() throws Exception {
             super.joinTests();
         }
 
         @Override
-        @Disabled("Like continuation failure")
+        @Disabled("Like continuation failure (https://github.com/FoundationDB/fdb-record-layer/issues/3099)")
         void like() throws Exception {
             super.like();
         }
 
         @Override
-        @Disabled("continuation verification")
+        @Disabled("continuation verification (https://github.com/FoundationDB/fdb-record-layer/issues/3096)")
         public void nullOperator() throws Exception {
             super.nullOperator();
         }
 
         @Override
-        @Disabled("Infinite loop")
+        @Disabled("Infinite loop (https://github.com/FoundationDB/fdb-record-layer/issues/3095)")
         public void primaryKey() throws Exception {
             super.primaryKey();
         }
 
         @Override
-        @Disabled("maxRows ignored")
+        @Disabled("maxRows ignored (https://github.com/FoundationDB/fdb-record-layer/issues/3100)")
         public void recursiveCte() throws Exception {
             super.recursiveCte();
         }
 
         @Override
-        @Disabled("Infinite continuation loop")
+        @Disabled("Infinite continuation loop (https://github.com/FoundationDB/fdb-record-layer/issues/3095)")
         public void selectAStar() throws Exception {
             super.selectAStar();
         }
 
         @Override
-        @Disabled("continuation verification")
+        @Disabled("continuation verification (https://github.com/FoundationDB/fdb-record-layer/issues/3096)")
         public void standardTests() throws Exception {
             super.standardTests();
         }
 
         @Override
-        @Disabled("continuation verification")
+        @Disabled("continuation verification (https://github.com/FoundationDB/fdb-record-layer/issues/3096)")
         public void standardTestsWithProto() throws Exception {
             super.standardTestsWithProto();
         }
 
         @Override
-        @Disabled("Continuation verification")
+        @Disabled("Continuation verification (https://github.com/FoundationDB/fdb-record-layer/issues/3096)")
         public void standardTestsWithMetaData() throws Exception {
             super.standardTestsWithMetaData();
         }
 
         @Override
-        @Disabled("Infinite continuation loop")
+        @Disabled("Infinite continuation loop (https://github.com/FoundationDB/fdb-record-layer/issues/3095)")
         public void subqueryTests() throws Exception {
             super.subqueryTests();
         }
 
         @Override
-        @Disabled("Infinite continuation loop")
+        @Disabled("Infinite continuation loop (https://github.com/FoundationDB/fdb-record-layer/issues/3095)")
         public void unionEmptyTables() throws Exception {
             super.unionEmptyTables();
         }
 
         @Override
-        @Disabled("Infinite continuation loop")
+        @Disabled("Infinite continuation loop (https://github.com/FoundationDB/fdb-record-layer/issues/3095)")
         public void union() throws Exception {
             super.union();
         }
 
         @Override
-        @Disabled("maxRows ignored on update")
+        @Disabled("maxRows ignored on update (https://github.com/FoundationDB/fdb-record-layer/issues/3100)")
         public void updateDeleteReturning() throws Exception {
             super.updateDeleteReturning();
         }
