b232b7a466943ada72e0224d6a78f4a930936301
ohad <ozeliger@apple.com>
2025-02-12 14:32:47 -0500

    PR Comments: Bring back SHOW_PLAN_ON_DIFF

--- yaml-tests/src/main/java/com/apple/foundationdb/relational/yamltests/YamlTestConfigFilters.java
+++ yaml-tests/src/main/java/com/apple/foundationdb/relational/yamltests/YamlTestConfigFilters.java
@@ -80,6 +80,15 @@ public enum YamlTestConfigFilters {
             return config.getRunnerOptions().getOrDefault(YamlExecutionContext.OPTION_CORRECT_EXPLAIN, false) &&
                     config.getRunnerOptions().getOrDefault(YamlExecutionContext.OPTION_CORRECT_METRICS, false);
         }
+    },
+    /**
+     * Used to show the plan diffs graphically.
+     */
+    SHOW_PLAN_ON_DIFF {
+        @Override
+        boolean filter(final YamlTestConfig config) {
+            return config.getRunnerOptions().getOrDefault(YamlExecutionContext.OPTION_SHOW_PLAN_ON_DIFF, false);
+        }
     };
 
     /**
