b07a1ae39e1af3287c1037d44cf449e06da9c640
ohad <ozeliger@apple.com>
2025-02-19 11:40:36 -0500

    Enable a few tests where the force continuation option does not fail anymore

--- yaml-tests/src/test/java/YamlIntegrationTests.java
+++ yaml-tests/src/test/java/YamlIntegrationTests.java
@@ -170,8 +170,6 @@ public class YamlIntegrationTests {
     }
 
     @TestTemplate
-    @ExcludeYamlTestConfig(value = YamlTestConfigFilters.DO_NOT_FORCE_CONTINUATIONS,
-            reason = "Infinite loop (https://github.com/FoundationDB/fdb-record-layer/issues/3095)")
     public void primaryKey(YamlTest.Runner runner) throws Exception {
         runner.runYamsql("primary-key-tests.yamsql");
     }
