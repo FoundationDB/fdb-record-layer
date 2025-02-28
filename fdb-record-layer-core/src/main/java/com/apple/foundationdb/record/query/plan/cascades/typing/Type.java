825f4c506447b57c1995e0e1fc198dbc4a9319d4
Pranjal Gupta <pgupta32@apple.com>
2025-02-28 15:33:58 +0000

    address concerns

--- fdb-record-layer-core/src/main/java/com/apple/foundationdb/record/query/plan/cascades/typing/Type.java
+++ fdb-record-layer-core/src/main/java/com/apple/foundationdb/record/query/plan/cascades/typing/Type.java
@@ -421,7 +421,7 @@ public interface Type extends Narrowable<Type>, PlanSerializable {
                 final var elementField = messageDescriptor.findFieldByName(NullableArrayTypeUtils.getRepeatedFieldName());
                 final var elementTypeCode = TypeCode.fromProtobufType(elementField.getType());
                 return fromProtoTypeToArray(descriptor, protoType, elementTypeCode, true);
-            } else if (Uuid.MESSAGE_NAME.equals(messageDescriptor.getName())) {
+            } else if (TupleFieldsProto.UUID.getDescriptor().equals(messageDescriptor)) {
                 return Type.uuidType(isNullable);
             } else {
                 return Record.fromFieldDescriptorsMap(isNullable, Record.toFieldDescriptorMap(messageDescriptor.getFields()));
