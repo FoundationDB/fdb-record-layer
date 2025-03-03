e55da51c937c326e2bacf336276afa9cb8c50104
Alec Grieser <agrieser@apple.com>
2025-03-03 12:30:52 +0000

    fix up groupby-tests.yamsql to allow for force_continuations

--- yaml-tests/src/main/java/com/apple/foundationdb/relational/yamltests/command/QueryExecutor.java
+++ yaml-tests/src/main/java/com/apple/foundationdb/relational/yamltests/command/QueryExecutor.java
@@ -55,7 +55,7 @@ public class QueryExecutor {
     private static final Logger logger = LogManager.getLogger(QueryExecutor.class);
     private static final int FORCED_MAX_ROWS = 1; // The maxRows number to use when we are forcing it on the test
     private static final int MAX_CONTINUATIONS_ALLOWED = 100;
-    private static final SemanticVersion STRICT_ASSERTIONS_CUTOFF = SemanticVersion.parse("4.1.4.0");
+    private static final SemanticVersion STRICT_ASSERTIONS_CUTOFF = SemanticVersion.parse("4.1.9.0");
 
     @Nonnull
     private final String query;
