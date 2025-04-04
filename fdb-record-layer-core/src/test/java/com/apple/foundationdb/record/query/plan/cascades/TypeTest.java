33f80d44c5e4b0ed08302ed41ce9f310f3ab6e7e
Pranjal Gupta <pgupta32@apple.com>
2025-04-04 11:05:12 +0100

    removing test to check if UUID is parsable

--- fdb-record-layer-core/src/test/java/com/apple/foundationdb/record/query/plan/cascades/TypeTest.java
+++ fdb-record-layer-core/src/test/java/com/apple/foundationdb/record/query/plan/cascades/TypeTest.java
@@ -27,8 +27,6 @@ import com.apple.foundationdb.record.TestRecords2Proto;
 import com.apple.foundationdb.record.TestRecords3Proto;
 import com.apple.foundationdb.record.TestRecords4Proto;
 import com.apple.foundationdb.record.TestRecords4WrapperProto;
-import com.apple.foundationdb.record.TestRecordsUuidProto;
-import com.apple.foundationdb.record.TupleFieldsProto;
 import com.apple.foundationdb.record.query.plan.cascades.typing.Type;
 import com.apple.foundationdb.record.query.plan.cascades.typing.TypeRepository;
 import com.apple.foundationdb.record.query.plan.cascades.values.LiteralValue;
@@ -128,14 +126,6 @@ class TypeTest {
                                             .setStartDate(random.nextLong())
                                             .setSchoolName("randomString" + random.nextInt()).build()
                                     ).build()
-                    ),
-                    Arguments.of(
-                            "TestRecordsUuidProto.UuidRecord", TestRecordsUuidProto.UuidRecord.newBuilder()
-                                    .setPkey(TupleFieldsProto.UUID.newBuilder()
-                                            .setMostSignificantBits(98452560)
-                                            .setLeastSignificantBits(30900234)
-                                            .build())
-                                    .build()
                     )
             );
         }
