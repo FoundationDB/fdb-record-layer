d22f9d9e74f8d5281857b36c37b684aa18af49cc
Youssef Hatem <y_hatem@apple.com>
2025-05-29 15:30:22 +0200

    fix case-when alternative branches promotion.

--- fdb-record-layer-core/src/main/java/com/apple/foundationdb/record/query/plan/cascades/values/PickValue.java
+++ fdb-record-layer-core/src/main/java/com/apple/foundationdb/record/query/plan/cascades/values/PickValue.java
@@ -30,7 +30,10 @@ import com.apple.foundationdb.record.PlanSerializationContext;
 import com.apple.foundationdb.record.planprotos.PPickValue;
 import com.apple.foundationdb.record.planprotos.PValue;
 import com.apple.foundationdb.record.provider.foundationdb.FDBRecordStoreBase;
+import com.apple.foundationdb.record.query.expressions.Comparisons;
 import com.apple.foundationdb.record.query.plan.cascades.AliasMap;
+import com.apple.foundationdb.record.query.plan.cascades.BuiltInFunction;
+import com.apple.foundationdb.record.query.plan.cascades.typing.Typed;
 import com.apple.foundationdb.record.query.plan.explain.ExplainTokens;
 import com.apple.foundationdb.record.query.plan.explain.ExplainTokensWithPrecedence;
 import com.apple.foundationdb.record.query.plan.cascades.SemanticException;
@@ -46,6 +49,7 @@ import javax.annotation.Nullable;
 import java.util.List;
 import java.util.Objects;
 import java.util.function.Supplier;
+import java.util.stream.Collectors;
 
 /**
  * A value representing multiple "alternative" values.
@@ -216,6 +220,38 @@ public class PickValue extends AbstractValue {
         public PickValue fromProto(@Nonnull final PlanSerializationContext serializationContext,
                                    @Nonnull final PPickValue pickValueProto) {
             return PickValue.fromProto(serializationContext, pickValueProto);
+        }
+    }
+
+    /**
+     * The {@code pick} function.
+     */
+    @AutoService(BuiltInFunction.class)
+    public static class PickValueFn extends BuiltInFunction<Value> {
+        public PickValueFn() {
+            super("pick", List.of(Type.primitiveType(Type.TypeCode.INT), Type.any()), Type.any(), PickValueFn::encapsulate);
+        }
+
+        private static Value encapsulate(@Nonnull BuiltInFunction<Value> builtInFunction,
+                                         @Nonnull final List<? extends Typed> arguments) {
+            Verify.verify(arguments.size() > 1);
+            var selectorValue = (Value)arguments.get(0);
+            final var selectorMaxType = Type.maximumType(selectorValue.getResultType(), Type.primitiveType(Type.TypeCode.INT));
+            SemanticException.check(selectorMaxType != null, SemanticException.ErrorCode.INCOMPATIBLE_TYPE);
+            selectorValue = PromoteValue.inject(selectorValue, selectorMaxType);
+
+            final var firstAlternative = (Value)arguments.get(1);
+            var alternativesMaxType = firstAlternative.getResultType();
+            for (int i = 2; i < arguments.size(); i++) {
+                alternativesMaxType = Type.maximumType(alternativesMaxType, arguments.get(i).getResultType());
+                SemanticException.check(alternativesMaxType != null, SemanticException.ErrorCode.INCOMPATIBLE_TYPE);
+            }
+
+            final var alternativesList = ImmutableList.<Value>builder();
+            for (int i = 1; i < arguments.size(); i++) {
+                alternativesList.add(PromoteValue.inject((Value)arguments.get(i), alternativesMaxType));
+            }
+            return new PickValue(selectorValue, alternativesList.build());
         }
     }
 }
