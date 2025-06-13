4f9c0a5a63d368f0a38c8fdf29a39713dc00004b
Scott Dugas <sdugas@apple.com>
2025-06-13 17:05:57 -0400

    Reduce to 3 servers, since starting servers involves a 3 second sleep

--- yaml-tests/src/test/java/com/apple/foundationdb/relational/yamltests/server/ExternalServerTest.java
+++ yaml-tests/src/test/java/com/apple/foundationdb/relational/yamltests/server/ExternalServerTest.java
@@ -54,7 +54,7 @@ class ExternalServerTest {
     @Test
     void startMultiple() throws Exception {
         final List<ExternalServer> servers = new ArrayList<>();
-        for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < 3; i++) {
             servers.add(new ExternalServer(currentServerPath, null));
         }
         try {
