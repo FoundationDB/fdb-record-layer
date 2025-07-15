48694545ef6aaa2644be3f337bfa26f6a10ea91b
Pranjal Gupta <pgupta32@apple.com>
2025-07-15 09:18:13 +0100

    style

--- fdb-relational-api/src/main/java/com/apple/foundationdb/relational/api/metadata/DataType.java
+++ fdb-relational-api/src/main/java/com/apple/foundationdb/relational/api/metadata/DataType.java
@@ -37,7 +37,6 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
-import java.util.UUID;
 import java.util.function.Supplier;
 import java.util.stream.Collectors;
 
