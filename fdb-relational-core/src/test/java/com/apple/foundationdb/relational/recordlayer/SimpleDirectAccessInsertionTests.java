FoundationDB/fdb-record-layer

commit 8ee1c54556d02a3c2a5b2ba8df7a682c24c4d46e

Remove FieldDescription in Struct/ArrayMetadata in favour of DataType (#3484)
This basically
https://github.com/FoundationDB/fdb-record-layer/pull/3395 put over
main, minus the UUID support. I think bringing this in as a
"enhancement" separately will declutter the future UUID support PR.

fdb-relational-core/src/main/java/com/apple/foundationdb/relational/recordlayer/RecordTypeTable.java
@@ -21,7 +21,6 @@
 package com.apple.foundationdb.relational.recordlayer;
 
 import com.apple.foundationdb.annotation.API;
-
 import com.apple.foundationdb.record.RecordCoreException;
 import com.apple.foundationdb.record.RecordCursor;
 import com.apple.foundationdb.record.RecordMetaData;
@@ -32,22 +31,21 @@ import com.apple.foundationdb.record.metadata.expressions.RecordTypeKeyExpressio
 import com.apple.foundationdb.record.provider.foundationdb.FDBStoredRecord;
 import com.apple.foundationdb.record.query.plan.cascades.typing.Type;
 import com.apple.foundationdb.relational.api.Continuation;
-import com.apple.foundationdb.relational.api.FieldDescription;
 import com.apple.foundationdb.relational.api.ImmutableRowStruct;
 import com.apple.foundationdb.relational.api.Options;
-import com.apple.foundationdb.relational.api.Row;
-import com.apple.foundationdb.relational.api.SqlTypeSupport;
-import com.apple.foundationdb.relational.api.StructMetaData;
-import com.apple.foundationdb.relational.api.Transaction;
 import com.apple.foundationdb.relational.api.RelationalStruct;
 import com.apple.foundationdb.relational.api.RelationalStructMetaData;
+import com.apple.foundationdb.relational.api.Row;
+import com.apple.foundationdb.relational.api.StructMetaData;
+import com.apple.foundationdb.relational.api.Transaction;
 import com.apple.foundationdb.relational.api.exceptions.ErrorCode;
 import com.apple.foundationdb.relational.api.exceptions.RelationalException;
+import com.apple.foundationdb.relational.api.metadata.DataType;
+import com.apple.foundationdb.relational.recordlayer.metadata.DataTypeUtils;
 import com.apple.foundationdb.relational.recordlayer.storage.BackingStore;
 import com.apple.foundationdb.relational.recordlayer.util.ExceptionUtil;
 import com.apple.foundationdb.relational.util.Assert;
 import com.apple.foundationdb.relational.util.NullableArrayUtils;
-
 import com.google.protobuf.ByteString;
 import com.google.protobuf.Descriptors;
 import com.google.protobuf.DynamicMessage;
@@ -55,9 +53,8 @@ import com.google.protobuf.Message;
 
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
-import java.sql.DatabaseMetaData;
 import java.sql.SQLException;
-import java.sql.Types;
+import java.util.List;
 import java.util.Locale;
 import java.util.Map;
 import java.util.Set;
@@ -127,7 +124,7 @@ public class RecordTypeTable extends RecordTypeScannable<FDBStoredRecord<Message
         });
         orderedFieldMap.putAll(descriptorLookupMap);
         final Type.Record record = Type.Record.fromFieldDescriptorsMap(orderedFieldMap);
-        return SqlTypeSupport.recordToMetaData(record);
+        return RelationalStructMetaData.of((DataType.StructType) DataTypeUtils.toRelationalType(record));
     }
 
     @Override
@@ -185,39 +182,50 @@ public class RecordTypeTable extends RecordTypeScannable<FDBStoredRecord<Message
     public static Message toDynamicMessage(RelationalStruct struct, Descriptors.Descriptor descriptor) throws RelationalException {
         DynamicMessage.Builder builder = DynamicMessage.newBuilder(descriptor);
         try {
-            StructMetaData structMetaData = struct.getMetaData();
-            for (int i = 1; i <= structMetaData.getColumnCount(); i++) {
-                String columnName = structMetaData.getColumnName(i);
+            final var type = struct.getMetaData().getRelationalDataType();
+            final var fields = type.getFields();
+            for (int i = 0; i < fields.size(); i++) {
+                final var field = fields.get(i);
+                final var columnName = fields.get(i).getName();
                 Descriptors.FieldDescriptor fd = builder.getDescriptorForType().findFieldByName(columnName);
                 Assert.thatUnchecked(fd != null, ErrorCode.INVALID_PARAMETER, "Cannot find column name: " + columnName);
                 // TODO: Add type checking, nudging, and coercion at this point! Per bfines, good place to do it!
                 //  TODO (Add type checking, nudging, and coercion)
-                switch (structMetaData.getColumnType(i)) {
-                    case Types.BOOLEAN:
-                    case Types.DOUBLE:
-                    case Types.FLOAT:
-                    case Types.INTEGER:
-                    case Types.BIGINT:
-                    case Types.VARCHAR:
-                        final var obj = struct.getObject(i);
+                if (fd.getType() == Descriptors.FieldDescriptor.Type.ENUM) {
+                    final var maybeEnumValue = struct.getString(i + 1);
+                    if (maybeEnumValue != null) {
+                        final var valueDescriptor = fd.getEnumType().findValueByName(maybeEnumValue);
+                        Assert.thatUnchecked(valueDescriptor != null, ErrorCode.CANNOT_CONVERT_TYPE, "Invalid enum value: %s", maybeEnumValue);
+                        builder.setField(fd, valueDescriptor);
+                    }
+                    continue;
+                }
+                switch (field.getType().getCode()) {
+                    case BOOLEAN:
+                    case DOUBLE:
+                    case FLOAT:
+                    case INTEGER:
+                    case LONG:
+                    case STRING:
+                        final var obj = struct.getObject(i + 1);
                         if (obj != null) {
                             builder.setField(fd, obj);
                         }
                         break;
-                    case Types.BINARY:
-                        final var bytes = struct.getBytes(i);
+                    case BYTES:
+                        final var bytes = struct.getBytes(i + 1);
                         if (bytes != null) {
                             builder.setField(fd, ByteString.copyFrom(bytes));
                         }
                         break;
-                    case Types.STRUCT:
-                        var subStruct = struct.getStruct(i);
+                    case STRUCT:
+                        var subStruct = struct.getStruct(i + 1);
                         if (subStruct != null) {
                             builder.setField(fd, toDynamicMessage(subStruct, fd.getMessageType()));
                         }
                         break;
-                    case Types.ARRAY:
-                        var array = struct.getArray(i);
+                    case ARRAY:
+                        var array = struct.getArray(i + 1);
                         // if the fieldDescriptor is repeatable, then it's an unwrapped array
                         if (fd.isRepeated()) {
                             final var arrayItems = (Object[]) (array == null ? new Object[]{} : array.getArray());
@@ -232,33 +240,19 @@ public class RecordTypeTable extends RecordTypeScannable<FDBStoredRecord<Message
                             if (fd.getType() == Descriptors.FieldDescriptor.Type.MESSAGE) {
                                 Assert.thatUnchecked(NullableArrayUtils.isWrappedArrayDescriptor(fd.getMessageType()));
                                 // wrap array in a struct and call toDynamicMessage again
-                                final var wrapper = new ImmutableRowStruct(new ArrayRow(array), new RelationalStructMetaData(
-                                        FieldDescription.array(NullableArrayUtils.REPEATED_FIELD_NAME, DatabaseMetaData.columnNullable, null)));
+                                final var wrapper = new ImmutableRowStruct(new ArrayRow(array), RelationalStructMetaData.of(
+                                        DataType.StructType.from("STRUCT", List.of(
+                                                DataType.StructType.Field.from(NullableArrayUtils.REPEATED_FIELD_NAME, array.getMetaData().asRelationalType(), 0)
+                                        ), true)));
                                 builder.setField(fd, toDynamicMessage(wrapper, fd.getMessageType()));
                             } else {
                                 Assert.failUnchecked("Field Type expected to be of Type ARRAY but is actually " + fd.getType());
                             }
                         }
                         break;
-                    case Types.OTHER:
-                        // if the type is not specific, try checking if it is an ENUM.
-                        // Since the JDBC specifications do not directly provide for ENUM type, we assume it to be defined
-                        // with ENUM value's string-representation having sql.Types OTHER
-                        if (fd.getType() == Descriptors.FieldDescriptor.Type.ENUM) {
-                            final var enumValue = struct.getString(i);
-                            if (enumValue != null) {
-                                final var valueDescriptor = fd.getEnumType().findValueByName(enumValue);
-                                Assert.thatUnchecked(valueDescriptor != null, ErrorCode.CANNOT_CONVERT_TYPE, "Invalid enum value: %s", enumValue);
-                                builder.setField(fd, fd.getEnumType().findValueByName(enumValue));
-                            }
-                        } else {
-                            Assert.failUnchecked(ErrorCode.INTERNAL_ERROR, (String.format(Locale.ROOT, "Cannot interpret value in Column type OTHER for column <%s>",
-                                    columnName)));
-                        }
-                        break;
                     default:
                         Assert.failUnchecked(ErrorCode.INTERNAL_ERROR, (String.format(Locale.ROOT, "Unexpected Column type <%s> for column <%s>",
-                                structMetaData.getColumnType(i), columnName)));
+                                struct.getMetaData().getColumnType(i), columnName)));
                         break;
                 }
             }

fdb-relational-api/src/main/java/com/apple/foundationdb/relational/api/ArrayMetaData.java
@@ -21,7 +21,9 @@
 package com.apple.foundationdb.relational.api;
 
 import com.apple.foundationdb.relational.api.exceptions.ErrorCode;
+import com.apple.foundationdb.relational.api.metadata.DataType;
 
+import javax.annotation.Nonnull;
 import java.sql.SQLException;
 import java.sql.Wrapper;
 
@@ -59,4 +61,13 @@ public interface ArrayMetaData extends Wrapper {
      * @throws SQLException if the type of the column is not an array, or if something else goes wrong.
      */
     ArrayMetaData getElementArrayMetaData() throws SQLException;
+
+    /**
+     * Get the {@link DataType} object equivalent of this metadata.
+     *
+     * @return the datatype object.
+     * @throws SQLException if something goes wrong.
+     */
+    @Nonnull
+    DataType.ArrayType asRelationalType() throws SQLException;
 }

fdb-relational-api/src/main/java/com/apple/foundationdb/relational/api/FieldDescription.java
@@ -1,205 +1,0 @@
-/*
- * FieldDescription.java
- *
- * This source file is part of the FoundationDB open source project
- *
- * Copyright 2021-2024 Apple Inc. and the FoundationDB project authors
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
-package com.apple.foundationdb.relational.api;
-
-import com.apple.foundationdb.annotation.API;
-
-import com.google.common.base.Suppliers;
-
-import javax.annotation.Nonnull;
-import javax.annotation.Nullable;
-import java.sql.Types;
-import java.util.Objects;
-import java.util.function.Supplier;
-
-/**
- * A description of an individual field.
- *
- * Note that this representation is indicative of JDBC-API level information. That is, it is used to hold
- * information that is necessary to represent a given column field in a JDBC MetaData API.
- */
-@API(API.Status.EXPERIMENTAL)
-public final class FieldDescription {
-    private final String fieldName;
-    private final int sqlTypeCode; //taken from java.sql.Types
-
-    private final int nullable;
-
-    private final StructMetaData fieldMetaData;
-
-    private final ArrayMetaData arrayMetaData;
-
-    //indicates a column that isn't part of the DDL for a query, but is part of the returned
-    //tuple (and therefore necessary to keep so that our positional ordering is intact)
-    private final boolean phantom;
-    private final Supplier<Integer> hashCodeSupplier;
-
-    /**
-     * Create a primitive field.
-     *
-     * This is a convenience factory method for a more complicated constructor. Equivalent to
-     * {@link #primitive(String, int, int, boolean)}, where {@code phantom == false}.
-     *
-     * @param fieldName the name of the field.
-     * @param sqlTypeCode the SQL type code for this field. Should match values found in {@link Types}
-     * @param nullable one of {@link java.sql.DatabaseMetaData#columnNoNulls},
-     *      {@link java.sql.DatabaseMetaData#columnNullable}, or {@link java.sql.DatabaseMetaData#columnNullableUnknown}.
-     * @return a FieldDescription for the field.
-     */
-    public static FieldDescription primitive(@Nonnull String fieldName, int sqlTypeCode, int nullable) {
-        return primitive(fieldName, sqlTypeCode, nullable, false);
-    }
-
-    /**
-     * Create a primitive field.
-     *
-     * This is a convenience factory method for a more complicated constructor.
-     *
-     * @param fieldName the name of the field.
-     * @param sqlTypeCode the SQL type code for this field. Should match values found in {@link Types}
-     * @param nullable one of {@link java.sql.DatabaseMetaData#columnNoNulls},
-     *      {@link java.sql.DatabaseMetaData#columnNullable}, or {@link java.sql.DatabaseMetaData#columnNullableUnknown}.
-     * @param phantom if true, this column should be treated as "phantom" in the API. That is, its entries
-     *                  are present in the returned row, but are not represented as part of the "return value". In other
-     *                  words, the values take up space in the physical arrays, but are not considered part of the actual
-     *                  return of the query, requiring the implementation to adjust for the field's position.
-     * @return a FieldDescription for the field.
-     */
-    public static FieldDescription primitive(@Nonnull String fieldName, int sqlTypeCode, int nullable, boolean phantom) {
-        return new FieldDescription(fieldName, sqlTypeCode, nullable, phantom, null, null);
-    }
-
-    /**
-     * Create a struct field.
-     *
-     * This is a convenience factory method for a more complicated constructor.
-     *
-     * @param fieldName the name of the field.
-     * @param nullable one of {@link java.sql.DatabaseMetaData#columnNoNulls},
-     *      {@link java.sql.DatabaseMetaData#columnNullable}, or {@link java.sql.DatabaseMetaData#columnNullableUnknown}.
-     * @param definition the definition of the struct value itself.
-     * @return a FieldDescription for the field.
-     */
-    public static FieldDescription struct(@Nonnull String fieldName, int nullable, StructMetaData definition) {
-        return new FieldDescription(fieldName, Types.STRUCT, nullable, false, definition, null);
-    }
-
-    /**
-     * Create an array field.
-     *
-     * This is a convenience factory method for a more complicated constructor.
-     *
-     * @param fieldName the name of the field.
-     * @param nullable one of {@link java.sql.DatabaseMetaData#columnNoNulls},
-     *      {@link java.sql.DatabaseMetaData#columnNullable}, or {@link java.sql.DatabaseMetaData#columnNullableUnknown}.
-     * @param definition the metadata description of the contents of the array.
-     * @return a FieldDescription for the field.
-     */
-    public static FieldDescription array(@Nonnull String fieldName, int nullable, ArrayMetaData definition) {
-        return new FieldDescription(fieldName, Types.ARRAY, nullable, false, null, definition);
-    }
-
-    private FieldDescription(String fieldName,
-                             int sqlType,
-                             int nullable,
-                             boolean phantom,
-                             @Nullable StructMetaData fieldMetaData,
-                             @Nullable ArrayMetaData arrayMetaData) {
-        this.fieldName = fieldName;
-        this.sqlTypeCode = sqlType;
-        this.nullable = nullable;
-        this.phantom = phantom;
-        this.fieldMetaData = fieldMetaData;
-        this.arrayMetaData = arrayMetaData;
-        this.hashCodeSupplier = Suppliers.memoize(this::calculateHashCode);
-    }
-
-    public String getName() {
-        return fieldName;
-    }
-
-    public int getSqlTypeCode() {
-        return sqlTypeCode;
-    }
-
-    public boolean isStruct() {
-        return sqlTypeCode == Types.STRUCT;
-    }
-
-    public boolean isArray() {
-        return sqlTypeCode == Types.ARRAY;
-    }
-
-    public StructMetaData getFieldMetaData() {
-        return fieldMetaData;
-    }
-
-    public ArrayMetaData getArrayMetaData() {
-        return arrayMetaData;
-    }
-
-    public int isNullable() {
-        return nullable;
-    }
-
-    public boolean isPhantom() {
-        return phantom;
-    }
-
-    @Override
-    public boolean equals(Object other) {
-        if (!(other instanceof FieldDescription)) {
-            return false;
-        }
-        final var otherDescription = (FieldDescription) other;
-        if (otherDescription == this) {
-            return true;
-        }
-        if (!fieldName.equals(otherDescription.fieldName) ||
-                sqlTypeCode != otherDescription.sqlTypeCode ||
-                nullable != otherDescription.nullable) {
-            return false;
-        }
-        if (fieldMetaData == null) {
-            if (otherDescription.fieldMetaData != null) {
-                return false;
-            }
-        } else {
-            if (!fieldMetaData.equals(otherDescription.fieldMetaData)) {
-                return false;
-            }
-        }
-        if (arrayMetaData == null) {
-            return otherDescription.arrayMetaData == null;
-        } else {
-            return arrayMetaData.equals(otherDescription.arrayMetaData);
-        }
-    }
-
-    @Override
-    public int hashCode() {
-        return hashCodeSupplier.get();
-    }
-
-    private int calculateHashCode() {
-        return Objects.hash(fieldName, sqlTypeCode, nullable, fieldMetaData, arrayMetaData);
-    }
-}

fdb-relational-api/src/main/java/com/apple/foundationdb/relational/api/RelationalArrayBuilder.java
@@ -42,7 +42,9 @@ public interface RelationalArrayBuilder {
 
     RelationalArrayBuilder addString(@Nonnull String value) throws SQLException;
 
-    RelationalArrayBuilder addLong(@Nonnull long value) throws SQLException;
+    RelationalArrayBuilder addLong(long value) throws SQLException;
+
+    RelationalArrayBuilder addObject(@Nonnull Object value) throws SQLException;
 
     RelationalArrayBuilder addStruct(RelationalStruct struct) throws SQLException;
 }

fdb-relational-api/src/main/java/com/apple/foundationdb/relational/api/RelationalArrayMetaData.java
@@ -21,15 +21,14 @@
 package com.apple.foundationdb.relational.api;
 
 import com.apple.foundationdb.annotation.API;
-
 import com.apple.foundationdb.relational.api.exceptions.ErrorCode;
 import com.apple.foundationdb.relational.api.exceptions.RelationalException;
-
+import com.apple.foundationdb.relational.api.metadata.DataType;
 import com.google.common.base.Suppliers;
 
 import javax.annotation.Nonnull;
+import java.sql.DatabaseMetaData;
 import java.sql.SQLException;
-import java.sql.Types;
 import java.util.Objects;
 import java.util.function.Supplier;
 
@@ -39,41 +38,42 @@ import java.util.function.Supplier;
 @API(API.Status.EXPERIMENTAL)
 public final class RelationalArrayMetaData implements ArrayMetaData {
 
-    private final FieldDescription element;
+    private final DataType.ArrayType type;
 
     private final Supplier<Integer> hashCodeSupplier;
 
-    private RelationalArrayMetaData(@Nonnull FieldDescription element) {
-        this.element = element;
+    private RelationalArrayMetaData(@Nonnull DataType.ArrayType type) {
+        this.type = type;
         this.hashCodeSupplier = Suppliers.memoize(this::calculateHashCode);
     }
 
-    public static RelationalArrayMetaData ofPrimitive(int sqlType, int nullable) {
-        return new RelationalArrayMetaData(FieldDescription.primitive("VALUE", sqlType, nullable));
-    }
-
-    public static RelationalArrayMetaData ofStruct(@Nonnull StructMetaData metaData, int nullable) {
-        return new RelationalArrayMetaData(FieldDescription.struct("VALUE", nullable, metaData));
+    @Nonnull
+    public static RelationalArrayMetaData of(@Nonnull DataType.ArrayType type) {
+        return new RelationalArrayMetaData(type);
     }
 
     @Override
-    public int isElementNullable() throws SQLException {
-        return element.isNullable();
+    public int isElementNullable() {
+        if (type.getElementType().isNullable()) {
+            return DatabaseMetaData.columnNullable;
+        } else {
+            return DatabaseMetaData.columnNoNulls;
+        }
     }
 
     @Override
     public String getElementName() throws SQLException {
-        return element.getName();
+        return "VALUE";
     }
 
     @Override
     public int getElementType() throws SQLException {
-        return element.getSqlTypeCode();
+        return type.getElementType().getJdbcSqlCode();
     }
 
     @Override
-    public String getElementTypeName() throws SQLException {
-        return SqlTypeNamesSupport.getSqlTypeName(element.getSqlTypeCode());
+    public String getElementTypeName() {
+        return SqlTypeNamesSupport.getSqlTypeName(type.getElementType().getJdbcSqlCode());
     }
 
     /**
@@ -86,10 +86,10 @@ public final class RelationalArrayMetaData implements ArrayMetaData {
      */
     @Override
     public StructMetaData getElementStructMetaData() throws SQLException {
-        if (element.getSqlTypeCode() != Types.STRUCT) {
+        if (type.getElementType().getCode() != DataType.Code.STRUCT) {
             throw new RelationalException("Element is not of STRUCT type", ErrorCode.CANNOT_CONVERT_TYPE).toSqlException();
         }
-        return element.getFieldMetaData();
+        return RelationalStructMetaData.of((DataType.StructType) type.getElementType());
     }
 
     /**
@@ -102,15 +102,21 @@ public final class RelationalArrayMetaData implements ArrayMetaData {
      */
     @Override
     public ArrayMetaData getElementArrayMetaData() throws SQLException {
-        if (element.getSqlTypeCode() != Types.ARRAY) {
+        if (type.getElementType().getCode() != DataType.Code.ARRAY) {
             throw new RelationalException("Element is not of ARRAY type", ErrorCode.CANNOT_CONVERT_TYPE).toSqlException();
         }
-        return element.getArrayMetaData();
+        return RelationalArrayMetaData.of((DataType.ArrayType) type.getElementType());
     }
 
     @Nonnull
-    public FieldDescription getElementField() {
-        return element;
+    @Override
+    public DataType.ArrayType asRelationalType() throws SQLException {
+        return type;
+    }
+
+    @Nonnull
+    public DataType getElementDataType() {
+        return type.getElementType();
     }
 
     @Override
@@ -132,7 +138,7 @@ public final class RelationalArrayMetaData implements ArrayMetaData {
         if (otherMetadata == this) {
             return true;
         }
-        return element.equals(otherMetadata.element);
+        return type.equals(otherMetadata.type);
     }
 
     @Override
@@ -141,7 +147,7 @@ public final class RelationalArrayMetaData implements ArrayMetaData {
     }
 
     private int calculateHashCode() {
-        return Objects.hash(element);
+        return Objects.hash(type);
     }
 
 }

fdb-relational-api/src/main/java/com/apple/foundationdb/relational/api/RelationalStructBuilder.java
@@ -40,8 +40,6 @@ public interface RelationalStructBuilder {
 
     RelationalStructBuilder addBoolean(String fieldName, boolean b) throws SQLException;
 
-    RelationalStructBuilder addShort(String fieldName, short b) throws SQLException;
-
     RelationalStructBuilder addLong(String fieldName, long l) throws SQLException;
 
     RelationalStructBuilder addFloat(String fieldName, float f) throws SQLException;
@@ -52,7 +50,7 @@ public interface RelationalStructBuilder {
 
     RelationalStructBuilder addString(String fieldName, @Nullable String s) throws SQLException;
 
-    RelationalStructBuilder addObject(String fieldName, @Nullable Object obj, int targetSqlType) throws SQLException;
+    RelationalStructBuilder addObject(String fieldName, @Nullable Object obj) throws SQLException;
 
     RelationalStructBuilder addStruct(String fieldName, @Nonnull RelationalStruct struct) throws SQLException;
 

commit 32d0c951937fcd38bde9c2ed82ab70cef4d541c5

Add support for NULL type fields in ExecuteInsert (#3490)
This PR adds support for inserting structs with NULL typed fields into
the table via `ExecuteInsert` in the Direct Access API. Also adds a test
for the same.

This bug was introduced by
https://github.com/FoundationDB/fdb-record-layer/pull/3484, which
essentially shifts the Relational-layer from using Sql Type codes for
internal operations to relying on Relational DataTypes for getting a
richer information.

Fixes: https://github.com/FoundationDB/fdb-record-layer/issues/3491

fdb-relational-core/src/main/java/com/apple/foundationdb/relational/recordlayer/RecordTypeTable.java
@@ -250,6 +250,8 @@ public class RecordTypeTable extends RecordTypeScannable<FDBStoredRecord<Message
                             }
                         }
                         break;
+                    case NULL:
+                        break;
                     default:
                         Assert.failUnchecked(ErrorCode.INTERNAL_ERROR, (String.format(Locale.ROOT, "Unexpected Column type <%s> for column <%s>",
                                 struct.getMetaData().getColumnType(i), columnName)));

fdb-relational-core/src/test/java/com/apple/foundationdb/relational/recordlayer/SimpleDirectAccessInsertionTests.java
@@ -147,6 +147,31 @@ public class SimpleDirectAccessInsertionTests {
     }
 
     @Test
+    void insertWithExplicitNullFields() throws SQLException {
+        try (RelationalConnection conn = DriverManager.getConnection("jdbc:embed://" + db.getDatabasePath().getPath()).unwrap(RelationalConnection.class)) {
+            conn.setSchema(db.getSchemaName());
+
+            try (RelationalStatement s = conn.createStatement()) {
+                final var struct = EmbeddedRelationalStruct.newBuilder()
+                        .addLong("ID", 1L)
+                        .addString("NAME", "Anthony Bourdain")
+                        .addObject("EMAIL", null)
+                        .build();
+                int inserted = s.executeInsert("RESTAURANT_REVIEWER", struct);
+                Assertions.assertThat(inserted).withFailMessage("incorrect insertion number!").isEqualTo(1);
+                KeySet key = new KeySet()
+                        .setKeyColumn("ID", 1L);
+                try (RelationalResultSet rrs = s.executeGet("RESTAURANT_REVIEWER", key, Options.NONE)) {
+                    ResultSetAssert.assertThat(rrs)
+                            .hasNextRow()
+                            .hasColumn("EMAIL", null)
+                            .hasNoNextRow();
+                }
+            }
+        }
+    }
+
+    @Test
     void insertMultipleTablesDontMix() throws SQLException {
         /*
          * Because RecordLayer allows multiple types within the same keyspeace, we need to validate that