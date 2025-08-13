b19042c41d5c8f66ac18a5ab84042f7b0f20f379
Normen Seemann <nseemann@apple.com>
2025-08-13 14:55:18 +0200

    index maintenance code complete

---
+++ fdb-record-layer-core/src/main/java/com/apple/foundationdb/record/provider/foundationdb/VectorIndexScanBounds.java
@@ -0,0 +1,237 @@
+/*
+ * MultidimensionalIndexScanBounds.java
+ *
+ * This source file is part of the FoundationDB open source project
+ *
+ * Copyright 2022 Apple Inc. and the FoundationDB project authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.apple.foundationdb.record.provider.foundationdb;
+
+import com.apple.foundationdb.annotation.API;
+import com.apple.foundationdb.async.rtree.RTree;
+import com.apple.foundationdb.record.IndexScanType;
+import com.apple.foundationdb.record.TupleRange;
+import com.apple.foundationdb.tuple.Tuple;
+import com.google.common.base.Preconditions;
+import com.google.common.collect.ImmutableList;
+
+import javax.annotation.Nonnull;
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * {@link IndexScanBounds} for a multidimensional index scan. A multidimensional scan bounds object contains a
+ * {@link #prefixRange} and a {@link SpatialPredicate} which can be almost arbitrarily complex. The prefix range
+ * is a regular tuple range informing the index maintainer how to constrain the search over the non-multidimensional
+ * fields that can be viewed as a prefix whose data is stored in a regular one-dimensional index. The spatial predicate
+ * implements methods to quickly establish geometric overlap and containment. Spatial predicates do have internal
+ * structure as they can delegate to contained other spatial predicates thus allowing to form e.g. logical conjuncts
+ * or disjuncts.
+ */
+@API(API.Status.EXPERIMENTAL)
+public class MultidimensionalIndexScanBounds implements IndexScanBounds {
+    @Nonnull
+    private final TupleRange prefixRange;
+
+    @Nonnull
+    private final SpatialPredicate spatialPredicate;
+
+    @Nonnull
+    private final TupleRange suffixRange;
+
+    public MultidimensionalIndexScanBounds(@Nonnull final TupleRange prefixRange,
+                                           @Nonnull final SpatialPredicate spatialPredicate,
+                                           @Nonnull final TupleRange suffixRange) {
+        this.prefixRange = prefixRange;
+        this.spatialPredicate = spatialPredicate;
+        this.suffixRange = suffixRange;
+    }
+
+    @Nonnull
+    @Override
+    public IndexScanType getScanType() {
+        return IndexScanType.BY_VALUE;
+    }
+
+    @Nonnull
+    public TupleRange getPrefixRange() {
+        return prefixRange;
+    }
+
+    @Nonnull
+    public SpatialPredicate getSpatialPredicate() {
+        return spatialPredicate;
+    }
+
+    @Nonnull
+    public TupleRange getSuffixRange() {
+        return suffixRange;
+    }
+
+    /**
+     * Method to compute if the rectangle handed in overlaps with this scan bounds object. This method is invoked when
+     * the R-tree data structure of a multidimensional index is searched. Note that this method can be implemented using
+     * a best-effort approach as it is permissible to indicate overlap between {@code mbr} and {@code this} when there
+     * is in fact no overlap. The rate of false-positives directly influences the search performance in the
+     * multidimensional index.
+     * @param mbr the minimum-bounding {@link RTree.Rectangle}
+     * @return {@code true} if {@code this} overlaps with {@code mbr}
+     */
+    public boolean overlapsMbrApproximately(@Nonnull RTree.Rectangle mbr) {
+        return spatialPredicate.overlapsMbrApproximately(mbr);
+    }
+
+    /**
+     * Method to compute if the point handed in is contained by this scan bounds object.
+     * @param position the {@link RTree.Point}
+     * @return {@code true} if {@code position} is contained by {@code this}
+     */
+    public boolean containsPosition(@Nonnull RTree.Point position) {
+        return spatialPredicate.containsPosition(position);
+    }
+
+    /**
+     * Spatial predicate. The implementing classes form a boolean algebra of sorts. Most notably {@link Hypercube}
+     * represents the logical variables, while {@link And} and {@link Or} can be used to build up more complex powerful
+     * bounds.
+     */
+    public interface SpatialPredicate {
+        SpatialPredicate TAUTOLOGY = new SpatialPredicate() {
+            @Override
+            public boolean overlapsMbrApproximately(@Nonnull final RTree.Rectangle mbr) {
+                return true;
+            }
+
+            @Override
+            public boolean containsPosition(@Nonnull final RTree.Point position) {
+                return true;
+            }
+        };
+
+        boolean overlapsMbrApproximately(@Nonnull RTree.Rectangle mbr);
+
+        boolean containsPosition(@Nonnull RTree.Point position);
+    }
+
+    /**
+     * Scan bounds that consists of other {@link SpatialPredicate}s to form a logical OR.
+     */
+    public static class Or implements SpatialPredicate {
+        @Nonnull
+        private final List<SpatialPredicate> children;
+
+        public Or(@Nonnull final List<SpatialPredicate> children) {
+            this.children = ImmutableList.copyOf(children);
+        }
+
+        @Override
+        public boolean overlapsMbrApproximately(@Nonnull final RTree.Rectangle mbr) {
+            return children.stream()
+                    .anyMatch(child -> child.overlapsMbrApproximately(mbr));
+        }
+
+        @Override
+        public boolean containsPosition(@Nonnull final RTree.Point position) {
+            return children.stream()
+                    .anyMatch(child -> child.containsPosition(position));
+        }
+
+        @Override
+        public String toString() {
+            return children.stream().map(Object::toString).collect(Collectors.joining(" or "));
+        }
+    }
+
+    /**
+     * Scan bounds that consists of other {@link SpatialPredicate}s to form a logical AND.
+     */
+    public static class And implements SpatialPredicate {
+        @Nonnull
+        private final List<SpatialPredicate> children;
+
+        public And(@Nonnull final List<SpatialPredicate> children) {
+            this.children = ImmutableList.copyOf(children);
+        }
+
+        @Override
+        public boolean overlapsMbrApproximately(@Nonnull final RTree.Rectangle mbr) {
+            return children.stream()
+                    .allMatch(child -> child.overlapsMbrApproximately(mbr));
+        }
+
+        @Override
+        public boolean containsPosition(@Nonnull final RTree.Point position) {
+            return children.stream()
+                    .allMatch(child -> child.containsPosition(position));
+        }
+
+        @Override
+        public String toString() {
+            return children.stream().map(Object::toString).collect(Collectors.joining(" and "));
+        }
+    }
+
+    /**
+     * Scan bounds describing an n-dimensional hypercube.
+     */
+    public static class Hypercube implements SpatialPredicate {
+        @Nonnull
+        private final List<TupleRange> dimensionRanges;
+
+        public Hypercube(@Nonnull final List<TupleRange> dimensionRanges) {
+            this.dimensionRanges = ImmutableList.copyOf(dimensionRanges);
+        }
+
+        @Override
+        public boolean overlapsMbrApproximately(@Nonnull final RTree.Rectangle mbr) {
+            Preconditions.checkArgument(mbr.getNumDimensions() == dimensionRanges.size());
+
+            for (int d = 0; d < mbr.getNumDimensions(); d++) {
+                final Tuple lowTuple = Tuple.from(mbr.getLow(d));
+                final Tuple highTuple = Tuple.from(mbr.getHigh(d));
+                final TupleRange dimensionRange = dimensionRanges.get(d);
+                if (!dimensionRange.overlaps(lowTuple, highTuple)) {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        @Override
+        public boolean containsPosition(@Nonnull final RTree.Point position) {
+            Preconditions.checkArgument(position.getNumDimensions() == dimensionRanges.size());
+
+            for (int d = 0; d < position.getNumDimensions(); d++) {
+                final Tuple coordinate = Tuple.from(position.getCoordinate(d));
+                final TupleRange dimensionRange = dimensionRanges.get(d);
+                if (!dimensionRange.contains(coordinate)) {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        @Nonnull
+        public List<TupleRange> getDimensionRanges() {
+            return dimensionRanges;
+        }
+
+        @Override
+        public String toString() {
+            return "HyperCube:[" + dimensionRanges + "]";
+        }
+    }
+}
---
+++ fdb-record-layer-core/src/main/java/com/apple/foundationdb/record/provider/foundationdb/VectorIndexScanComparisons.java
@@ -0,0 +1,355 @@
+/*
+ * MultidimensionalIndexScanComparisons.java
+ *
+ * This source file is part of the FoundationDB open source project
+ *
+ * Copyright 2022 Apple Inc. and the FoundationDB project authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.apple.foundationdb.record.provider.foundationdb;
+
+import com.apple.foundationdb.annotation.API;
+import com.apple.foundationdb.annotation.SpotBugsSuppressWarnings;
+import com.apple.foundationdb.record.EvaluationContext;
+import com.apple.foundationdb.record.IndexScanType;
+import com.apple.foundationdb.record.PlanDeserializer;
+import com.apple.foundationdb.record.PlanHashable;
+import com.apple.foundationdb.record.PlanSerializationContext;
+import com.apple.foundationdb.record.TupleRange;
+import com.apple.foundationdb.record.metadata.Index;
+import com.apple.foundationdb.record.planprotos.PIndexScanParameters;
+import com.apple.foundationdb.record.planprotos.PMultidimensionalIndexScanComparisons;
+import com.apple.foundationdb.record.provider.foundationdb.MultidimensionalIndexScanBounds.Hypercube;
+import com.apple.foundationdb.record.query.plan.ScanComparisons;
+import com.apple.foundationdb.record.query.plan.cascades.AliasMap;
+import com.apple.foundationdb.record.query.plan.cascades.CorrelationIdentifier;
+import com.apple.foundationdb.record.query.plan.explain.ExplainTokens;
+import com.apple.foundationdb.record.query.plan.explain.ExplainTokensWithPrecedence;
+import com.apple.foundationdb.record.query.plan.cascades.explain.Attribute;
+import com.apple.foundationdb.record.query.plan.cascades.values.translation.TranslationMap;
+import com.google.auto.service.AutoService;
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.ImmutableSet;
+
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+import java.util.List;
+import java.util.Objects;
+import java.util.Set;
+
+/**
+ * {@link ScanComparisons} for use in a multidimensional index scan.
+ */
+@API(API.Status.UNSTABLE)
+public class MultidimensionalIndexScanComparisons implements IndexScanParameters {
+    @Nonnull
+    private final ScanComparisons prefixScanComparisons;
+    @Nonnull
+    private final List<ScanComparisons> dimensionsScanComparisons;
+    @Nonnull
+    private final ScanComparisons suffixScanComparisons;
+
+    public MultidimensionalIndexScanComparisons(@Nonnull final ScanComparisons prefixScanComparisons,
+                                                @Nonnull final List<ScanComparisons> dimensionsScanComparisons,
+                                                @Nonnull final ScanComparisons suffixKeyComparisonRanges) {
+        this.prefixScanComparisons = prefixScanComparisons;
+        this.dimensionsScanComparisons = dimensionsScanComparisons;
+        this.suffixScanComparisons = suffixKeyComparisonRanges;
+    }
+
+    @Nonnull
+    @Override
+    public IndexScanType getScanType() {
+        return IndexScanType.BY_VALUE;
+    }
+
+    @Nonnull
+    public ScanComparisons getPrefixScanComparisons() {
+        return prefixScanComparisons;
+    }
+
+    @Nonnull
+    public List<ScanComparisons> getDimensionsScanComparisons() {
+        return dimensionsScanComparisons;
+    }
+
+    @Nonnull
+    public ScanComparisons getSuffixScanComparisons() {
+        return suffixScanComparisons;
+    }
+
+    @Nonnull
+    @Override
+    public MultidimensionalIndexScanBounds bind(@Nonnull final FDBRecordStoreBase<?> store, @Nonnull final Index index,
+                                                @Nonnull final EvaluationContext context) {
+        final ImmutableList.Builder<TupleRange> dimensionsTupleRangeBuilder = ImmutableList.builder();
+        for (final ScanComparisons dimensionScanComparison : dimensionsScanComparisons) {
+            dimensionsTupleRangeBuilder.add(dimensionScanComparison.toTupleRange(store, context));
+        }
+        final Hypercube hypercube = new Hypercube(dimensionsTupleRangeBuilder.build());
+        return new MultidimensionalIndexScanBounds(prefixScanComparisons.toTupleRange(store, context),
+                hypercube, suffixScanComparisons.toTupleRange(store, context));
+    }
+
+    @Override
+    public int planHash(@Nonnull PlanHashMode mode) {
+        return PlanHashable.objectsPlanHash(mode, prefixScanComparisons, dimensionsScanComparisons,
+                suffixScanComparisons);
+    }
+
+    @Override
+    public boolean isUnique(@Nonnull Index index) {
+        return prefixScanComparisons.isEquality() && prefixScanComparisons.size() == index.getColumnSize();
+    }
+
+    @Nonnull
+    @Override
+    public ExplainTokensWithPrecedence explain() {
+        @Nullable var tupleRange = prefixScanComparisons.toTupleRangeWithoutContext();
+        final var prefix = tupleRange == null
+                           ? prefixScanComparisons.explain().getExplainTokens()
+                           : new ExplainTokens().addToString(tupleRange);
+
+        final var dimensions =
+                new ExplainTokens().addSequence(() -> new ExplainTokens().addCommaAndWhiteSpace(),
+                        () -> dimensionsScanComparisons.stream()
+                                .map(dimensionScanComparisons -> {
+                                    @Nullable var dimensionTupleRange = dimensionScanComparisons.toTupleRangeWithoutContext();
+                                    return dimensionTupleRange == null
+                                           ? dimensionScanComparisons.explain().getExplainTokens()
+                                           : new ExplainTokens().addToString(dimensionTupleRange);
+                                }).iterator());
+
+
+        tupleRange = suffixScanComparisons.toTupleRangeWithoutContext();
+        final var suffix = tupleRange == null
+                           ? suffixScanComparisons.explain().getExplainTokens()
+                           : new ExplainTokens().addToString(tupleRange);
+
+        return ExplainTokensWithPrecedence.of(prefix.addOptionalWhitespace().addToString(":{").addOptionalWhitespace()
+                .addNested(dimensions).addOptionalWhitespace().addToString("}:").addOptionalWhitespace().addNested(suffix));
+    }
+
+    @Override
+    public void getPlannerGraphDetails(@Nonnull ImmutableList.Builder<String> detailsBuilder, @Nonnull ImmutableMap.Builder<String, Attribute> attributeMapBuilder) {
+        @Nullable TupleRange tupleRange = prefixScanComparisons.toTupleRangeWithoutContext();
+        if (tupleRange != null) {
+            detailsBuilder.add("prefix: " + tupleRange.getLowEndpoint().toString(false) + "{{plow}}, {{phigh}}" + tupleRange.getHighEndpoint().toString(true));
+            attributeMapBuilder.put("plow", Attribute.gml(tupleRange.getLow() == null ? "-∞" : tupleRange.getLow().toString()));
+            attributeMapBuilder.put("phigh", Attribute.gml(tupleRange.getHigh() == null ? "∞" : tupleRange.getHigh().toString()));
+        } else {
+            detailsBuilder.add("prefix comparisons: {{pcomparisons}}");
+            attributeMapBuilder.put("pcomparisons", Attribute.gml(prefixScanComparisons.toString()));
+        }
+
+        for (int d = 0; d < dimensionsScanComparisons.size(); d++) {
+            final ScanComparisons dimensionScanComparisons = dimensionsScanComparisons.get(d);
+            tupleRange = dimensionScanComparisons.toTupleRangeWithoutContext();
+            if (tupleRange != null) {
+                detailsBuilder.add("dim" + d + ": " + tupleRange.getLowEndpoint().toString(false) + "{{dlow" + d + "}}, {{dhigh" + d + "}}" + tupleRange.getHighEndpoint().toString(true));
+                attributeMapBuilder.put("dlow" + d, Attribute.gml(tupleRange.getLow() == null ? "-∞" : tupleRange.getLow().toString()));
+                attributeMapBuilder.put("dhigh" + d, Attribute.gml(tupleRange.getHigh() == null ? "∞" : tupleRange.getHigh().toString()));
+            } else {
+                detailsBuilder.add("dim" + d + " comparisons: " + "{{dcomparisons" + d + "}}");
+                attributeMapBuilder.put("dcomparisons" + d, Attribute.gml(dimensionScanComparisons.toString()));
+            }
+        }
+
+        tupleRange = suffixScanComparisons.toTupleRangeWithoutContext();
+        if (tupleRange != null) {
+            detailsBuilder.add("suffix: " + tupleRange.getLowEndpoint().toString(false) + "{{slow}}, {{shigh}}" + tupleRange.getHighEndpoint().toString(true));
+            attributeMapBuilder.put("slow", Attribute.gml(tupleRange.getLow() == null ? "-∞" : tupleRange.getLow().toString()));
+            attributeMapBuilder.put("shigh", Attribute.gml(tupleRange.getHigh() == null ? "∞" : tupleRange.getHigh().toString()));
+        } else {
+            detailsBuilder.add("suffix comparisons: {{scomparisons}}");
+            attributeMapBuilder.put("scomparisons", Attribute.gml(suffixScanComparisons.toString()));
+        }
+    }
+
+    @Nonnull
+    @Override
+    public Set<CorrelationIdentifier> getCorrelatedTo() {
+        final ImmutableSet.Builder<CorrelationIdentifier> correlatedToBuilder = ImmutableSet.builder();
+        correlatedToBuilder.addAll(prefixScanComparisons.getCorrelatedTo());
+        correlatedToBuilder.addAll(dimensionsScanComparisons.stream()
+                .flatMap(dimensionScanComparison -> dimensionScanComparison.getCorrelatedTo().stream()).iterator());
+        correlatedToBuilder.addAll(suffixScanComparisons.getCorrelatedTo());
+        return correlatedToBuilder.build();
+    }
+
+    @Nonnull
+    @Override
+    public IndexScanParameters rebase(@Nonnull final AliasMap translationMap) {
+        return translateCorrelations(TranslationMap.rebaseWithAliasMap(translationMap), false);
+    }
+
+    @Override
+    @SuppressWarnings("PMD.CompareObjectsWithEquals")
+    public boolean semanticEquals(@Nullable final Object other, @Nonnull final AliasMap aliasMap) {
+        if (this == other) {
+            return true;
+        }
+        if (other == null || getClass() != other.getClass()) {
+            return false;
+        }
+
+        final MultidimensionalIndexScanComparisons that = (MultidimensionalIndexScanComparisons)other;
+
+        if (!prefixScanComparisons.semanticEquals(that.prefixScanComparisons, aliasMap)) {
+            return false;
+        }
+        if (dimensionsScanComparisons.size() != that.dimensionsScanComparisons.size()) {
+            return false;
+        }
+        for (int i = 0; i < dimensionsScanComparisons.size(); i++) {
+            final ScanComparisons dimensionScanComparison = dimensionsScanComparisons.get(i);
+            final ScanComparisons otherDimensionScanComparison = that.dimensionsScanComparisons.get(i);
+            if (!dimensionScanComparison.semanticEquals(otherDimensionScanComparison, aliasMap)) {
+                return false;
+            }
+        }
+        return suffixScanComparisons.semanticEquals(that.suffixScanComparisons, aliasMap);
+    }
+
+    @Override
+    public int semanticHashCode() {
+        int hashCode = prefixScanComparisons.semanticHashCode();
+        for (final ScanComparisons dimensionScanComparison : dimensionsScanComparisons) {
+            hashCode = 31 * hashCode + dimensionScanComparison.semanticHashCode();
+        }
+        return 31 * hashCode + suffixScanComparisons.semanticHashCode();
+    }
+
+    @Nonnull
+    @Override
+    @SuppressWarnings("PMD.CompareObjectsWithEquals")
+    public IndexScanParameters translateCorrelations(@Nonnull final TranslationMap translationMap,
+                                                     final boolean shouldSimplifyValues) {
+        final ScanComparisons translatedPrefixScanComparisons =
+                prefixScanComparisons.translateCorrelations(translationMap, shouldSimplifyValues);
+
+        final ImmutableList.Builder<ScanComparisons> translatedDimensionScanComparisonBuilder = ImmutableList.builder();
+        boolean isSameDimensionsScanComparisons = true;
+        for (final ScanComparisons dimensionScanComparisons : dimensionsScanComparisons) {
+            final ScanComparisons translatedDimensionScanComparison =
+                    dimensionScanComparisons.translateCorrelations(translationMap, shouldSimplifyValues);
+            if (translatedDimensionScanComparison != dimensionScanComparisons) {
+                isSameDimensionsScanComparisons = false;
+            }
+            translatedDimensionScanComparisonBuilder.add(translatedDimensionScanComparison);
+        }
+
+        final ScanComparisons translatedSuffixKeyScanComparisons =
+                suffixScanComparisons.translateCorrelations(translationMap, shouldSimplifyValues);
+
+        if (translatedPrefixScanComparisons != prefixScanComparisons || !isSameDimensionsScanComparisons ||
+                translatedSuffixKeyScanComparisons != suffixScanComparisons) {
+            return withComparisons(translatedPrefixScanComparisons, translatedDimensionScanComparisonBuilder.build(),
+                    translatedSuffixKeyScanComparisons);
+        }
+        return this;
+    }
+
+    @Nonnull
+    protected MultidimensionalIndexScanComparisons withComparisons(@Nonnull final ScanComparisons prefixScanComparisons,
+                                                                   @Nonnull final List<ScanComparisons> dimensionsComparisonRanges,
+                                                                   @Nonnull final ScanComparisons suffixKeyScanComparisons) {
+        return new MultidimensionalIndexScanComparisons(prefixScanComparisons, dimensionsComparisonRanges,
+                suffixKeyScanComparisons);
+    }
+
+    @Override
+    public String toString() {
+        return "BY_VALUE(MD):" + prefixScanComparisons + ":" + dimensionsScanComparisons + ":" + suffixScanComparisons;
+    }
+
+    @Override
+    @SpotBugsSuppressWarnings("EQ_UNUSUAL")
+    @SuppressWarnings("EqualsWhichDoesntCheckParameterClass")
+    public boolean equals(final Object o) {
+        return semanticEquals(o, AliasMap.emptyMap());
+    }
+
+    @Override
+    public int hashCode() {
+        return semanticHashCode();
+    }
+
+    @Nonnull
+    @Override
+    public PMultidimensionalIndexScanComparisons toProto(@Nonnull final PlanSerializationContext serializationContext) {
+        final PMultidimensionalIndexScanComparisons.Builder builder = PMultidimensionalIndexScanComparisons.newBuilder();
+        builder.setPrefixScanComparisons(prefixScanComparisons.toProto(serializationContext));
+        for (final ScanComparisons dimensionsScanComparison : dimensionsScanComparisons) {
+            builder.addDimensionsScanComparisons(dimensionsScanComparison.toProto(serializationContext));
+        }
+        builder.setSuffixScanComparisons(suffixScanComparisons.toProto(serializationContext));
+        return builder.build();
+    }
+
+    @Nonnull
+    @Override
+    public PIndexScanParameters toIndexScanParametersProto(@Nonnull final PlanSerializationContext serializationContext) {
+        return PIndexScanParameters.newBuilder().setMultidimensionalIndexScanComparisons(toProto(serializationContext)).build();
+    }
+
+    @Nonnull
+    public static MultidimensionalIndexScanComparisons fromProto(@Nonnull final PlanSerializationContext serializationContext,
+                                                                 @Nonnull final PMultidimensionalIndexScanComparisons multidimensionalIndexScanComparisonsProto) {
+        final ImmutableList.Builder<ScanComparisons> dimensionScanComparisonsBuilder = ImmutableList.builder();
+        for (int i = 0; i < multidimensionalIndexScanComparisonsProto.getDimensionsScanComparisonsCount(); i ++) {
+            dimensionScanComparisonsBuilder.add(ScanComparisons.fromProto(serializationContext,
+                    multidimensionalIndexScanComparisonsProto.getDimensionsScanComparisons(i)));
+        }
+        return new MultidimensionalIndexScanComparisons(ScanComparisons.fromProto(serializationContext, Objects.requireNonNull(multidimensionalIndexScanComparisonsProto.getPrefixScanComparisons())),
+                dimensionScanComparisonsBuilder.build(),
+                ScanComparisons.fromProto(serializationContext, Objects.requireNonNull(multidimensionalIndexScanComparisonsProto.getSuffixScanComparisons())));
+    }
+
+    @Nonnull
+    public static MultidimensionalIndexScanComparisons byValue(@Nullable ScanComparisons prefixScanComparisons,
+                                                               @Nonnull final List<ScanComparisons> dimensionsComparisonRanges,
+                                                               @Nullable ScanComparisons suffixKeyScanComparisons) {
+        if (prefixScanComparisons == null) {
+            prefixScanComparisons = ScanComparisons.EMPTY;
+        }
+
+        if (suffixKeyScanComparisons == null) {
+            suffixKeyScanComparisons = ScanComparisons.EMPTY;
+        }
+
+        return new MultidimensionalIndexScanComparisons(prefixScanComparisons, dimensionsComparisonRanges, suffixKeyScanComparisons);
+    }
+
+    /**
+     * Deserializer.
+     */
+    @AutoService(PlanDeserializer.class)
+    public static class Deserializer implements PlanDeserializer<PMultidimensionalIndexScanComparisons, MultidimensionalIndexScanComparisons> {
+        @Nonnull
+        @Override
+        public Class<PMultidimensionalIndexScanComparisons> getProtoMessageClass() {
+            return PMultidimensionalIndexScanComparisons.class;
+        }
+
+        @Nonnull
+        @Override
+        public MultidimensionalIndexScanComparisons fromProto(@Nonnull final PlanSerializationContext serializationContext,
+                                                              @Nonnull final PMultidimensionalIndexScanComparisons multidimensionalIndexScanComparisonsProto) {
+            return MultidimensionalIndexScanComparisons.fromProto(serializationContext, multidimensionalIndexScanComparisonsProto);
+        }
+    }
+}
