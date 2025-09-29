FoundationDB/fdb-record-layer

commit 4b2321c5ac14fed9f9f60d94669053b74011f0ff

Extend serialized record encoding to allow for multiple encryption keys (#3522)
A key number is encoded in the serialized record, in a format that is
backward compatible. An interface can be provided to resolve key numbers
into keys for encryption and decryption. The byte format of existing
encrypted data is such that it is interpreted as having key number 0.

Compatibility of this change in persistent data is checked by the
`encrypted.yamsql` test when run against `4.6.1.0`. The YAML framework
still uses the old serializer API to set encryption keys, so this also
demonstrates that this is compatible.

A followup PR will change YAML to use the new serializer API while
continuing to pass mixed tests.

---------

Co-authored-by: Scott Dugas <scott.dugas@gmail.com>

fdb-record-layer-core/src/main/java/com/apple/foundationdb/record/provider/common/TransformedRecordSerializerJCE.java
@@ -38,41 +38,35 @@ import java.security.SecureRandom;
  */
 @API(API.Status.UNSTABLE)
 public class TransformedRecordSerializerJCE<M extends Message> extends TransformedRecordSerializer<M> {
-
     @Nullable
-    protected final String cipherName;
-    @Nullable
-    protected final Key encryptionKey;
-    @Nullable
-    protected final SecureRandom secureRandom;
+    protected final SerializationKeyManager keyManager;
 
     protected TransformedRecordSerializerJCE(@Nonnull RecordSerializer<M> inner,
                                              boolean compressWhenSerializing,
                                              int compressionLevel,
                                              boolean encryptWhenSerializing,
                                              double writeValidationRatio,
-                                             @Nullable String cipherName,
-                                             @Nullable Key encryptionKey,
-                                             @Nullable SecureRandom secureRandom) {
+                                             @Nullable SerializationKeyManager keyManager) {
         super(inner, compressWhenSerializing, compressionLevel, encryptWhenSerializing, writeValidationRatio);
-        this.cipherName = cipherName;
-        this.encryptionKey = encryptionKey;
-        this.secureRandom = secureRandom;
+        this.keyManager = keyManager;
     }
 
     @Override
-    protected void encrypt(@Nonnull TransformState state, @Nullable StoreTimer timer) throws GeneralSecurityException {
-        if (cipherName == null || encryptionKey == null || secureRandom == null) {
-            throw new RecordSerializationException("attempted to encrypt without setting cipher name and key");
+    protected void encrypt(@Nonnull TransformedRecordSerializerState state, @Nullable StoreTimer timer) throws GeneralSecurityException {
+        if (keyManager == null) {
+            throw new RecordSerializationException("attempted to encrypt without setting key manager (cipher name and key)");
         }
         long startTime = System.nanoTime();
 
+        int keyNumber = keyManager.getSerializationKey();
+        state.setKeyNumber(keyNumber);
+
         byte[] ivData = new byte[CipherPool.IV_SIZE];
-        secureRandom.nextBytes(ivData);
+        keyManager.getRandom(keyNumber).nextBytes(ivData);
         IvParameterSpec iv = new IvParameterSpec(ivData);
-        Cipher cipher = CipherPool.borrowCipher(cipherName);
+        Cipher cipher = CipherPool.borrowCipher(keyManager.getCipher(keyNumber));
         try {
-            cipher.init(Cipher.ENCRYPT_MODE, encryptionKey, iv);
+            cipher.init(Cipher.ENCRYPT_MODE, keyManager.getKey(keyNumber), iv);
 
             byte[] plainText = state.getDataArray();
             byte[] cipherText = cipher.doFinal(plainText);
@@ -81,7 +75,7 @@ public class TransformedRecordSerializerJCE<M extends Message> extends Transform
             byte[] serialized = new byte[totalSize];
             System.arraycopy(iv.getIV(), 0, serialized, 0, CipherPool.IV_SIZE);
             System.arraycopy(cipherText, 0, serialized, CipherPool.IV_SIZE, cipherText.length);
-            state.encrypted = true;
+            state.setEncrypted(true);
             state.setDataArray(serialized);
         } finally {
             CipherPool.returnCipher(cipher);
@@ -92,21 +86,21 @@ public class TransformedRecordSerializerJCE<M extends Message> extends Transform
     }
 
     @Override
-    protected void decrypt(@Nonnull TransformState state, @Nullable StoreTimer timer) throws GeneralSecurityException {
-        if (cipherName == null || encryptionKey == null || secureRandom == null) {
+    protected void decrypt(@Nonnull TransformedRecordSerializerState state, @Nullable StoreTimer timer) throws GeneralSecurityException {
+        if (keyManager == null) {
             throw new RecordSerializationException("missing encryption key or provider during decryption");
         }
         long startTime = System.nanoTime();
 
         byte[] ivData = new byte[CipherPool.IV_SIZE];
-        System.arraycopy(state.data, state.offset, ivData, 0, CipherPool.IV_SIZE);
+        System.arraycopy(state.getData(), state.getOffset(), ivData, 0, CipherPool.IV_SIZE);
         IvParameterSpec iv = new IvParameterSpec(ivData);
 
-        byte[] cipherText = new byte[state.length - CipherPool.IV_SIZE];
-        System.arraycopy(state.data, state.offset + CipherPool.IV_SIZE, cipherText, 0, cipherText.length);
-        Cipher cipher = CipherPool.borrowCipher(cipherName);
+        byte[] cipherText = new byte[state.getLength() - CipherPool.IV_SIZE];
+        System.arraycopy(state.getData(), state.getOffset() + CipherPool.IV_SIZE, cipherText, 0, cipherText.length);
+        Cipher cipher = CipherPool.borrowCipher(keyManager.getCipher(state.getKeyNumber()));
         try {
-            cipher.init(Cipher.DECRYPT_MODE, encryptionKey, iv);
+            cipher.init(Cipher.DECRYPT_MODE, keyManager.getKey(state.getKeyNumber()), iv);
 
             byte[] plainText = cipher.doFinal(cipherText);
             state.setDataArray(plainText);
@@ -116,6 +110,12 @@ public class TransformedRecordSerializerJCE<M extends Message> extends Transform
                 timer.recordSinceNanoTime(Events.DECRYPT_SERIALIZED_RECORD, startTime);
             }
         }
+    }
+
+    @Nonnull
+    @Override
+    public RecordSerializer<Message> widen() {
+        return new TransformedRecordSerializerJCE<>(inner.widen(), compressWhenSerializing, compressionLevel, encryptWhenSerializing, writeValidationRatio, keyManager);
     }
 
     /**
@@ -155,6 +155,8 @@ public class TransformedRecordSerializerJCE<M extends Message> extends Transform
      * @param <M> type of {@link Message} that underlying records will use
      */
     public static class Builder<M extends Message> extends TransformedRecordSerializer.Builder<M> {
+        @Nullable
+        protected SerializationKeyManager keyManager;
         @Nullable
         protected String cipherName;
         @Nullable
@@ -273,6 +275,26 @@ public class TransformedRecordSerializerJCE<M extends Message> extends Transform
         }
 
         /**
+         * Sets the key manager used during cryptographic operations.
+         * @param keyManager key manager to use for encrypting and decrypting
+         * @return this <code>Builder</code>
+         */
+        public Builder<M> setKeyManager(@Nonnull SerializationKeyManager keyManager) {
+            this.keyManager = keyManager;
+            return this;
+        }
+
+        /**
+         * Clears a previously set key manager
+         * that might have been passed to this <code>Builder</code>.
+         * @return this <code>Builder</code>
+         */
+        public Builder<M> clearKeyManager() {
+            this.keyManager = null;
+            return this;
+        }
+
+        /**
          * Construct a {@link TransformedRecordSerializerJCE} from the
          * parameters specified by this builder. If one has enabled
          * encryption at serialization time, then this will fail
@@ -282,17 +304,21 @@ public class TransformedRecordSerializerJCE<M extends Message> extends Transform
          */
         @Override
         public TransformedRecordSerializerJCE<M> build() {
-            if (encryptWhenSerializing) {
-                if (encryptionKey == null) {
+            if (keyManager == null) {
+                if (encryptionKey != null) {
+                    keyManager = new FixedZeroKeyManager(encryptionKey, cipherName, secureRandom);
+                } else if (encryptWhenSerializing) {
                     throw new RecordCoreArgumentException("cannot encrypt when serializing if encryption key is not set");
                 }
-            }
-            if (encryptionKey != null) {
-                if (cipherName == null) {
-                    cipherName = CipherPool.DEFAULT_CIPHER;
+            } else {
+                if (encryptionKey != null) {
+                    throw new RecordCoreArgumentException("cannot specify both key manager and encryption key");
                 }
-                if (secureRandom == null) {
-                    secureRandom = new SecureRandom();
+                if (cipherName != null) {
+                    throw new RecordCoreArgumentException("cannot specify both key manager and cipher name");
+                }
+                if (secureRandom != null) {
+                    throw new RecordCoreArgumentException("cannot specify both key manager and secure random");
                 }
             }
             return new TransformedRecordSerializerJCE<>(
@@ -301,10 +327,10 @@ public class TransformedRecordSerializerJCE<M extends Message> extends Transform
                     compressionLevel,
                     encryptWhenSerializing,
                     writeValidationRatio,
-                    cipherName,
-                    encryptionKey,
-                    secureRandom
+                    keyManager
             );
         }
+
     }
+
 }

fdb-record-layer-core/src/main/java/com/apple/foundationdb/record/provider/common/CipherPool.java
@@ -47,4 +47,8 @@ public class CipherPool {
     public static void returnCipher(@Nonnull Cipher cipher) {
         MAPPED_POOL.offer(cipher.getAlgorithm(), cipher);
     }
+
+    public static void invalidateAll() {
+        MAPPED_POOL.invalidateAll();
+    }
 }

fdb-record-layer-core/src/main/java/com/apple/foundationdb/record/provider/common/FixedZeroKeyManager.java
@@ -1,0 +1,77 @@
+/*
+ * FixedZeroKeyManager.java
+ *
+ * This source file is part of the FoundationDB open source project
+ *
+ * Copyright 2015-2025 Apple Inc. and the FoundationDB project authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.apple.foundationdb.record.provider.common;
+
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+import java.security.Key;
+import java.security.SecureRandom;
+import java.util.Random;
+
+/**
+ * A {@link SerializationKeyManager} that always returns the same fixed {@link Key}.
+ */
+public class FixedZeroKeyManager implements SerializationKeyManager {
+    private final Key encryptionKey;
+    private final String cipherName;
+    private final SecureRandom secureRandom;
+
+    public FixedZeroKeyManager(@Nonnull Key encryptionKey, @Nullable String cipherName, @Nullable SecureRandom secureRandom) {
+        if (cipherName == null) {
+            cipherName = CipherPool.DEFAULT_CIPHER;
+        }
+        if (secureRandom == null) {
+            secureRandom = new SecureRandom();
+        }
+        this.encryptionKey = encryptionKey;
+        this.cipherName = cipherName;
+        this.secureRandom = secureRandom;
+    }
+
+    @Override
+    public int getSerializationKey() {
+        return 0;
+    }
+
+    @Override
+    public Key getKey(int keyNumber) {
+        if (keyNumber != 0) {
+            throw new RecordSerializationException("only provide key number 0");
+        }
+        return encryptionKey;
+    }
+
+    @Override
+    public String getCipher(int keyNumber) {
+        if (keyNumber != 0) {
+            throw new RecordSerializationException("only provide key number 0");
+        }
+        return cipherName;
+    }
+
+    @Override
+    public Random getRandom(int keyNumber) {
+        if (keyNumber != 0) {
+            throw new RecordSerializationException("only provide key number 0");
+        }
+        return secureRandom;
+    }
+}

fdb-record-layer-core/src/main/java/com/apple/foundationdb/record/provider/common/MappedPool.java
@@ -104,6 +104,13 @@ public class MappedPool<K, V, E extends Exception> {
     }
 
     /**
+     * Invalidate all entries in the pool.
+     */
+    public void invalidateAll() {
+        pool.invalidateAll();
+    }
+
+    /**
      * Function with Exceptions to provide the pool.
      *
      * @param <K> key

fdb-record-layer-core/src/main/java/com/apple/foundationdb/record/provider/common/SerializationKeyManager.java
@@ -1,0 +1,63 @@
+/*
+ * SerializationKeyManager.java
+ *
+ * This source file is part of the FoundationDB open source project
+ *
+ * Copyright 2015-2025 Apple Inc. and the FoundationDB project authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.apple.foundationdb.record.provider.common;
+
+import com.apple.foundationdb.annotation.API;
+
+import java.security.Key;
+import java.util.Random;
+
+/**
+ * An interface between encrypting serialization, such as {@link TransformedRecordSerializerJCE}, and a source of keys
+ * with associated cipher algorithms. Each key is identified by a unique <em>key number</em>, which is persisted in
+ * the serialization so that the key can be recovered at deserialization time.
+ */
+@API(API.Status.EXPERIMENTAL)
+public interface SerializationKeyManager {
+    /**
+     * Get the key number to be used for <em>serializing</em> a record.
+     * Typically, this would be the <em>latest</em> key.
+     * @return the key number to use
+     */
+    int getSerializationKey();
+
+    /**
+     * Get the key with the given key number.
+     * @param keyNumber the unique key identifier
+     * @return the cipher used with this key
+     */
+    Key getKey(int keyNumber);
+
+    /**
+     * Get the name of the cipher used with the given key number.
+     * @param keyNumber the unique key identifier
+     * @return the cipher used with this key
+     */
+    String getCipher(int keyNumber);
+
+    /**
+     * Get a random generator to fill IVs when encrypting.
+     * Normally this would be a {@link java.security.SecureRandom} and would not depend on the key.
+     */
+    // TODO: Perhaps it would be better to have the KM give out an IvParameterSpec or something?
+    //  Maybe wait until we have another algorithm that's different enough.
+    Random getRandom(int keyNumber);
+}

commit 00902c882d3a290ccf2013cf463f6a9e095dd626

Allow TransformedRecordSerializerJCE.Builder to be reused (#3642)
This fixes a regression that was introduced in
https://github.com/FoundationDB/fdb-record-layer/pull/3522, which
prevented an instance of `TransformedRecordSerializerJCE.Builder<>` to
be reused if it was used to build an instance of a serializer with an
encryption key provided instead of a `SerializationKeyManager`.

fdb-record-layer-core/src/main/java/com/apple/foundationdb/record/provider/common/TransformedRecordSerializerJCE.java
@@ -304,13 +304,19 @@ public class TransformedRecordSerializerJCE<M extends Message> extends Transform
          */
         @Override
         public TransformedRecordSerializerJCE<M> build() {
-            if (keyManager == null) {
-                if (encryptionKey != null) {
-                    keyManager = new FixedZeroKeyManager(encryptionKey, cipherName, secureRandom);
-                } else if (encryptWhenSerializing) {
-                    throw new RecordCoreArgumentException("cannot encrypt when serializing if encryption key is not set");
-                }
-            } else {
+            return new TransformedRecordSerializerJCE<>(
+                    inner,
+                    compressWhenSerializing,
+                    compressionLevel,
+                    encryptWhenSerializing,
+                    writeValidationRatio,
+                    resolveKeyManager()
+            );
+        }
+
+        @Nullable
+        private SerializationKeyManager resolveKeyManager() {
+            if (this.keyManager != null) {
                 if (encryptionKey != null) {
                     throw new RecordCoreArgumentException("cannot specify both key manager and encryption key");
                 }
@@ -320,17 +326,20 @@ public class TransformedRecordSerializerJCE<M extends Message> extends Transform
                 if (secureRandom != null) {
                     throw new RecordCoreArgumentException("cannot specify both key manager and secure random");
                 }
-            }
-            return new TransformedRecordSerializerJCE<>(
-                    inner,
-                    compressWhenSerializing,
-                    compressionLevel,
-                    encryptWhenSerializing,
-                    writeValidationRatio,
-                    keyManager
-            );
-        }
 
+                return keyManager;
+            }
+
+            if (encryptionKey != null) {
+                return new FixedZeroKeyManager(encryptionKey, cipherName, secureRandom);
+            }
+
+            if (!encryptWhenSerializing) {
+                return null;
+            }
+
+            throw new RecordCoreArgumentException("cannot encrypt when serializing if encryption key is not set");
+        }
     }
 
 }

fdb-record-layer-core/src/test/java/com/apple/foundationdb/record/provider/common/TransformedRecordSerializerTest.java
@@ -72,6 +72,7 @@ import static org.hamcrest.Matchers.hasEntry;
 import static org.hamcrest.Matchers.instanceOf;
 import static org.hamcrest.Matchers.not;
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
@@ -770,6 +771,25 @@ public class TransformedRecordSerializerTest {
         builder.setEncryptionKey(keyManager.getKey(keyManager.getSerializationKey()));
         e = assertThrows(RecordCoreArgumentException.class, builder::build);
         assertThat(e.getMessage(), containsString("cannot specify both key manager and encryption key"));
+    }
+
+    @Test
+    void reuseBuilder() throws Exception {
+        RollingTestKeyManager keyManager = new RollingTestKeyManager(0);
+        TransformedRecordSerializerJCE.Builder<Message> builderWithEncryptionKey = TransformedRecordSerializerJCE
+                .newDefaultBuilder()
+                .setEncryptionKey(keyManager.getKey(keyManager.getSerializationKey()));
+        TransformedRecordSerializerJCE.Builder<Message> builderWithKeyManager = TransformedRecordSerializerJCE
+                .newDefaultBuilder()
+                .setKeyManager(keyManager);
+
+        List.of(builderWithEncryptionKey, builderWithKeyManager).forEach((builder) -> {
+            builder.setEncryptWhenSerializing(false);
+            assertDoesNotThrow(builder::build);
+
+            builder.setEncryptWhenSerializing(true);
+            assertDoesNotThrow(builder::build);
+        });
     }
 
     private boolean isCompressed(byte[] serialized) {