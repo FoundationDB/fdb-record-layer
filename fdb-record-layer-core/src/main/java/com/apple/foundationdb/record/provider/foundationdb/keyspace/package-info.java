24846e6d4706df38695db07b8f1a1ade633a75cb
ohad <ozeliger@apple.com>
2025-10-21 17:05:34 -0400

    Add documentation for KeySpace structure

--- fdb-record-layer-core/src/main/java/com/apple/foundationdb/record/provider/foundationdb/keyspace/package-info.java
+++ fdb-record-layer-core/src/main/java/com/apple/foundationdb/record/provider/foundationdb/keyspace/package-info.java
@@ -27,6 +27,59 @@
  * separate pieces of identifying information about the record store.
  * A {@link com.apple.foundationdb.record.provider.foundationdb.keyspace.KeySpace} is a symbolic representation of this layout, giving names -- or constant values -- to each component.
  * A {@link com.apple.foundationdb.record.provider.foundationdb.keyspace.KeySpacePath} is a concrete instance of a {@code KeySpace}, somewhat like a directory pathname in a file system.
+ * A {@link com.apple.foundationdb.record.provider.foundationdb.keyspace.KeySpaceDirectory} is a template, or definition, for a type and (potentially value) that can be assigned to a node in the tree.
+ * </p>
+ * <p>
+ * The purpose of the {@link com.apple.foundationdb.record.provider.foundationdb.keyspace.KeySpace} model is to uniquely
+ * define the structure and format of the data layout for record layer. As a consumer of record layer may find out, data
+ * layout complexity may grow over time and various parts of the data model may become tangled and hard to keep consistent
+ * and separate. The {@link com.apple.foundationdb.record.provider.foundationdb.keyspace.KeySpace} (and through it, the
+ * {@link com.apple.foundationdb.record.provider.foundationdb.keyspace.KeySpaceDirectory}) are there to define and enforce
+ * type safety and consistency allowing data to be uniquely interpreted.
+ * Each {@link com.apple.foundationdb.record.provider.foundationdb.keyspace.KeySpaceDirectory} has a {@code name}, a {@code type}
+ * and a {@code value}. The invariant enforced by the system is that for each {@link com.apple.foundationdb.record.provider.foundationdb.keyspace.KeySpaceDirectory},
+ * there can only be a single value of a certain type. For example, for a directory node named
+ * "database" there can be one child of type {@code long}, one child of type {@code String}, one child of type {@code NULL},
+ * etc. The guarantee that this provides is that when retrieving a {@link com.apple.foundationdb.KeyValue} from the
+ * database and observing the prefix of the {@code Key} - for example, {@code ["root", 8, 15, "user data"]} - we can unambiguously
+ * parse the key and assign the elements of the {@code Tuple} to individual named {@link com.apple.foundationdb.record.provider.foundationdb.keyspace.KeySpaceDirectory}
+ * instances - for example, the first element is of type {@code String} therefore it has to be the "database name", the second
+ * element is a {@code long}, therefore it has to be the "owner ID", etc.
+ * In fact, the implementation is slightly more nuanced: Each {@link com.apple.foundationdb.record.provider.foundationdb.keyspace.KeySpaceDirectory}
+ * can have many children of a certain type as long as they are all defined as {@code Constants} - that is, they have a predefined value,
+ * or a single {@link com.apple.foundationdb.record.provider.foundationdb.keyspace.KeySpaceDirectory.AnyValue} value of that type.
+ * For example, the node for "database" can have children (with {@code String} type)
+ * for "schema", "permissions" and "records" - where these values are known ahead of time. One cannot then add a value of type {@code String}
+ * as a child with the value "user-442233".
+ * </p>
+ * <p>
+ * For example: The following structure lists a hypothetical database data layout definition and example key/value entries that
+ * conform to these values: Each entry is an instance of a {@link com.apple.foundationdb.record.provider.foundationdb.keyspace.KeySpaceDirectory}
+ * with the format of name/type/value
+ *
+ * <pre>
+ *                                          root/String/"root"
+ *                                                 |
+ *                               ---------------------------------------
+ *                               |                                     |
+ *                    database/String/"database"               users/String/"users"
+ *                               |                                     |
+ *                   --------------------------                 userId/long/AnyValue
+ *                   |                        |
+ *          dbId/long/AnyValue         schema/String/"schema"
+ *                   |
+ *        ------------------------------
+ *        |                            |
+ * dbName/String/AnyValue    dbSchema/String/AnyValue
+ * </pre>
+ * In the above example, the FDB key of {@code ["root", "database", 8, "my-db"]} would be interpreted as follows:
+ * <ul>
+ *     <li>"root" -> root</li>
+ *     <li>"database" -> database</li>
+ *     <li>8 -> dbId</li>
+ *     <li>"my-db" -> dbName</li>
+ * </ul>
+ * and {@code ["root", "database", "my-db"]} would be considered illegal, as there is no way to match the "my-db" to a Directory node.
  * </p>
  */
 package com.apple.foundationdb.record.provider.foundationdb.keyspace;
