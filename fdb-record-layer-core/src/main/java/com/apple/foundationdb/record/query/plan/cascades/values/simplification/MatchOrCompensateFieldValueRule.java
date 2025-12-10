9be490e739298bb80f904665237d374423ab8ffd
Pranjal Gupta <pgupta32@apple.com>
2025-12-10 22:45:53 +0000

    nit

--- fdb-record-layer-core/src/main/java/com/apple/foundationdb/record/query/plan/cascades/values/simplification/MatchOrCompensateFieldValueRule.java
+++ fdb-record-layer-core/src/main/java/com/apple/foundationdb/record/query/plan/cascades/values/simplification/MatchOrCompensateFieldValueRule.java
@@ -75,7 +75,7 @@ public class MatchOrCompensateFieldValueRule extends ValueComputationRule<Iterab
             if (toBePulledUpValue instanceof FieldValue) {
                 if (matchedValuesMap == null || !matchedValuesMap.containsKey(toBePulledUpValue)) {
                     final var toBePulledUpFieldValue = (FieldValue)toBePulledUpValue;
-                    //∂
+                    //
                     // If the current field value uses a prefix of the field value we are trying to pull up
                     // (on an equal inValue), then we have found a match. For instance if we are tyring to pull up
                     // $a.x.y.z and the value we are pulling through is b = $a.x.y we can match those with a compensation of
