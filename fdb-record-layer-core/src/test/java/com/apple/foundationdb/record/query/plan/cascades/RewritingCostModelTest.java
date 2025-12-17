c477aadd97c97da5560204c3080faf685166ca5d
Hazem Zaghloul <h_zaghloul@apple.com>
2025-12-17 15:34:15 +0000

    Add an additional unit test for simplified predicates comparison

--- fdb-record-layer-core/src/test/java/com/apple/foundationdb/record/query/plan/cascades/RewritingCostModelTest.java
+++ fdb-record-layer-core/src/test/java/com/apple/foundationdb/record/query/plan/cascades/RewritingCostModelTest.java
@@ -20,9 +20,13 @@
 
 package com.apple.foundationdb.record.query.plan.cascades;
 
+import com.apple.foundationdb.record.query.expressions.Comparisons;
 import com.apple.foundationdb.record.query.plan.RecordQueryPlannerConfiguration;
 import com.apple.foundationdb.record.query.plan.cascades.expressions.SelectExpression;
+import com.apple.foundationdb.record.query.plan.cascades.predicates.AndPredicate;
+import com.apple.foundationdb.record.query.plan.cascades.predicates.ConstantPredicate;
 import com.apple.foundationdb.record.query.plan.cascades.predicates.ExistsPredicate;
+import com.apple.foundationdb.record.query.plan.cascades.predicates.OrPredicate;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
@@ -39,15 +43,15 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 class RewritingCostModelTest {
     /**
-     * Test that simple rewrite of a single parameter predicate with the existence of other predicates is preferred.
+     * Test that the push-down of a single query predicate with other predicates left at the same level is preferred.
      * The following query:
      * </p>
      * <pre>{@code
-     * SELECT a FROM T (SELECT a, b FROM T WHERE a = 42) WHERE EXISTS (SELECT alpha FROM TAU)
+     * SELECT a FROM sq (SELECT a, b FROM T WHERE a = 42) WHERE EXISTS (SELECT alpha FROM TAU)
      * }</pre>
      * should be preferred over the query:
      * <pre>{@code
-     * SELECT a FROM T (SELECT a, b FROM T) WHERE a = 42 AND EXISTS (SELECT alpha FROM TAU)
+     * SELECT a FROM sq (SELECT a, b FROM T) WHERE a = 42 AND EXISTS (SELECT alpha FROM TAU)
      * }</pre>
      * <p>
      */
@@ -81,7 +85,7 @@ class RewritingCostModelTest {
     }
 
     /**
-     * Test that simple rewrite of a single parameter predicate with the existence of other predicates is preferred.
+     * Test that an expression with a simplified query predicate is preferred.
      * The following query:
      * <pre>{@code
      * SELECT a FROM T WHERE a = 42
@@ -107,6 +111,101 @@ class RewritingCostModelTest {
         final GraphExpansion.Builder graphBBuilder = GraphExpansion.builder().addQuantifier(baseQuantifier);
         graphBBuilder.addResultColumn(column(baseQuantifier, "b", "b"));
         graphBBuilder.addAllPredicates(List.of(fieldPredicate(baseQuantifier, "a", EQUALS_42)));
+        final SelectExpression expressionB = graphBBuilder.build().buildSelect();
+
+        assertThat(PlannerPhase.REWRITING
+                .createCostModel(RecordQueryPlannerConfiguration.defaultPlannerConfiguration())
+                .compare(expressionB, expressionA)).isNegative();
+    }
+
+    /**
+     * Test that an expression with a tautology query predicate is preferred.
+     * The following query:
+     * <pre>{@code
+     * SELECT a FROM T WHERE false
+     * }</pre>
+     * <p>
+     * should be preferred over the query:
+     * </p>
+     * <pre>{@code
+     * SELECT a FROM T WHERE a IS NULL
+     * }</pre>
+     */
+    @Test
+    void costModelPrefersEliminatedPredicates() {
+        final Quantifier baseQuantifier = baseT();
+        final GraphExpansion.Builder graphABuilder = GraphExpansion.builder().addQuantifier(baseQuantifier);
+        graphABuilder.addResultColumn(column(baseQuantifier, "a", "a"));
+        graphABuilder.addAllPredicates(List.of(
+                fieldPredicate(baseQuantifier, "a", new Comparisons.NullComparison(Comparisons.Type.IS_NULL))
+        ));
+        final SelectExpression expressionA = graphABuilder.build().buildSelect();
+
+        final GraphExpansion.Builder graphBBuilder = GraphExpansion.builder().addQuantifier(baseQuantifier);
+        graphBBuilder.addResultColumn(column(baseQuantifier, "b", "b"));
+        graphBBuilder.addAllPredicates(List.of(ConstantPredicate.FALSE));
+        final SelectExpression expressionB = graphBBuilder.build().buildSelect();
+
+        assertThat(PlannerPhase.REWRITING
+                .createCostModel(RecordQueryPlannerConfiguration.defaultPlannerConfiguration())
+                .compare(expressionB, expressionA)).isNegative();
+    }
+
+    /**
+     * Test that an expression with a simplified query predicate at a deeper level is preferred.
+     * The following query:
+     * </p>
+     * <pre>{@code
+     * SELECT a FROM sq (SELECT a, b FROM T WHERE a = 42) WHERE EXISTS (SELECT alpha FROM TAU)
+     * }</pre>
+     * should be preferred over the query:
+     * <pre>{@code
+     * SELECT a
+     * FROM sq (
+     *      SELECT a, b
+     *      FROM T
+     *      WHERE a = 42 OR (a = 43 AND false) OR (a is null AND false)
+     * )
+     * WHERE EXISTS (SELECT alpha FROM TAU).
+     * }</pre>
+     * <p>
+     */
+    @Test
+    void costModelPrefersSimplifiedPredicatesAtDeeperLevels() {
+        final Quantifier baseQuantifier = baseT();
+        final Quantifier innerQuantifierA = forEach(selectWithPredicates(
+                baseQuantifier, List.of("a", "b"),
+                OrPredicate.or(
+                    fieldPredicate(baseQuantifier, "a", EQUALS_42),
+                        AndPredicate.and(
+                                fieldPredicate(baseQuantifier, "a",
+                                        new Comparisons.SimpleComparison(Comparisons.Type.EQUALS, 43L)),
+                                ConstantPredicate.FALSE
+                        ),
+                        AndPredicate.and(
+                                fieldPredicate(baseQuantifier, "a",
+                                        new Comparisons.SimpleComparison(Comparisons.Type.EQUALS, 44L)),
+                                ConstantPredicate.FALSE
+                        )
+                )
+        ));
+        final Quantifier existentialQun = exists(selectWithPredicates(baseTau(), List.of("alpha")));
+        final GraphExpansion.Builder graphABuilder = GraphExpansion.builder().addQuantifier(innerQuantifierA).addQuantifier(existentialQun);
+        graphABuilder.addResultColumn(column(innerQuantifierA, "a", "a"));
+        graphABuilder.addAllPredicates(List.of(
+                fieldPredicate(innerQuantifierA, "a", EQUALS_42),
+                new ExistsPredicate(existentialQun.getAlias())
+        ));
+        final SelectExpression expressionA = graphABuilder.build().buildSelect();
+
+        final Quantifier innerQuantifierB = forEach(selectWithPredicates(
+                baseQuantifier, List.of("a", "b"),
+                fieldPredicate(baseQuantifier, "a", EQUALS_42)
+        ));
+        final GraphExpansion.Builder graphBBuilder = GraphExpansion.builder()
+                .addQuantifier(innerQuantifierB).addQuantifier(existentialQun);
+        graphBBuilder.addResultColumn(column(innerQuantifierB, "a", "a"));
+        graphBBuilder.addAllPredicates(List.of(new ExistsPredicate(existentialQun.getAlias())));
         final SelectExpression expressionB = graphBBuilder.build().buildSelect();
 
         assertThat(PlannerPhase.REWRITING
