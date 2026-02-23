aa1f44ca0b4c5324f7ad3c474d7e71850048bf81
Sergei Pustovykh <s_pustovykh@apple.com>
2026-02-23 14:32:10 +0000

    Remove JOIN USING(__ROW_VERSION) test

--- yaml-tests/src/test/java/YamlIntegrationTests.java
+++ yaml-tests/src/test/java/YamlIntegrationTests.java
@@ -193,11 +193,6 @@ public class YamlIntegrationTests {
     }
 
     @TestTemplate
-    public void joinRowVersionTests(YamlTest.Runner runner) throws Exception {
-        runner.runYamsql("join-tests-row-version.yamsql");
-    }
-
-    @TestTemplate
     void like(YamlTest.Runner runner) throws Exception {
         runner.runYamsql("like.yamsql");
     }
