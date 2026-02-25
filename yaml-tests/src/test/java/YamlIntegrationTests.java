b41c58f765e60307a74b471239469d08f20aa230
Youssef Hatem <y_hatem@apple.com>
2026-02-25 17:03:53 +0000

    test reproducing the problem.

--- yaml-tests/src/test/java/YamlIntegrationTests.java
+++ yaml-tests/src/test/java/YamlIntegrationTests.java
@@ -268,6 +268,7 @@ public class YamlIntegrationTests {
     }
 
     @TestTemplate
+    @MaintainYamlTestConfig(YamlTestConfigFilters.CORRECT_EXPLAIN_AND_METRICS)
     public void recursiveCte(YamlTest.Runner runner) throws Exception {
         runner.runYamsql("recursive-cte.yamsql");
     }
