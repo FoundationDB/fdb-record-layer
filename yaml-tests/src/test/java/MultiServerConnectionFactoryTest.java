68a9fbd69d191c5b176c68049abc5431610e64e5
Scott Dugas <sdugas@apple.com>
2026-04-02 17:07:16 -0400

    Add a tests to MultiServerConnectionFactoryTest that cover multiple clusters

--- yaml-tests/src/test/java/MultiServerConnectionFactoryTest.java
+++ yaml-tests/src/test/java/MultiServerConnectionFactoryTest.java
@@ -25,7 +25,6 @@ import com.apple.foundationdb.relational.yamltests.YamlConnection;
 import com.apple.foundationdb.relational.yamltests.YamlConnectionFactory;
 import com.apple.foundationdb.relational.yamltests.connectionfactory.MultiServerConnectionFactory;
 import com.apple.foundationdb.relational.yamltests.server.SemanticVersion;
-import com.apple.foundationdb.test.FDBTestEnvironment;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
@@ -40,11 +39,11 @@ import java.util.Set;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
-// TODO add tests of multiple clusters
 public class MultiServerConnectionFactoryTest {
     private static final SemanticVersion PRIMARY_VERSION = SemanticVersion.parse("2.2.2.0");
     private static final SemanticVersion ALTERNATE_VERSION = SemanticVersion.parse("1.1.1.0");
-    private static final String CLUSTER_FILE = FDBTestEnvironment.randomClusterFile();
+    private static final String CLUSTER_FILE = "cluster0";
+    private static final String CLUSTER_FILE_1 = "cluster1";
 
     @ParameterizedTest
     @CsvSource({"0", "1"})
@@ -62,18 +61,18 @@ public class MultiServerConnectionFactoryTest {
 
         var connection = classUnderTest.getNewConnection(URI.create("Blah"), 0);
         assertConnection(connection, PRIMARY_VERSION, List.of(PRIMARY_VERSION));
-        assertStatement(connection.prepareStatement("SQL"), PRIMARY_VERSION);
-        assertStatement(connection.prepareStatement("SQL"), PRIMARY_VERSION);
+        assertStatement(connection.prepareStatement("SQL"), PRIMARY_VERSION, CLUSTER_FILE);
+        assertStatement(connection.prepareStatement("SQL"), PRIMARY_VERSION, CLUSTER_FILE);
 
         connection = classUnderTest.getNewConnection(URI.create("Blah"), 0);
         assertConnection(connection, PRIMARY_VERSION, List.of(PRIMARY_VERSION));
-        assertStatement(connection.prepareStatement("SQL"), PRIMARY_VERSION);
-        assertStatement(connection.prepareStatement("SQL"), PRIMARY_VERSION);
+        assertStatement(connection.prepareStatement("SQL"), PRIMARY_VERSION, CLUSTER_FILE);
+        assertStatement(connection.prepareStatement("SQL"), PRIMARY_VERSION, CLUSTER_FILE);
 
         connection = classUnderTest.getNewConnection(URI.create("Blah"), 0);
         assertConnection(connection, PRIMARY_VERSION, List.of(PRIMARY_VERSION));
-        assertStatement(connection.prepareStatement("SQL"), PRIMARY_VERSION);
-        assertStatement(connection.prepareStatement("SQL"), PRIMARY_VERSION);
+        assertStatement(connection.prepareStatement("SQL"), PRIMARY_VERSION, CLUSTER_FILE);
+        assertStatement(connection.prepareStatement("SQL"), PRIMARY_VERSION, CLUSTER_FILE);
     }
 
     @ParameterizedTest
@@ -95,9 +94,9 @@ public class MultiServerConnectionFactoryTest {
         // - statement: initial connection (2 statements)
         var connection = classUnderTest.getNewConnection(URI.create("Blah"), 0);
         assertConnection(connection, initialConnectionVersion, List.of(initialConnectionVersion, otherConnectionVersion));
-        assertStatement(connection.prepareStatement("SQL"), initialConnectionVersion);
+        assertStatement(connection.prepareStatement("SQL"), initialConnectionVersion, CLUSTER_FILE);
         // next statement
-        assertStatement(connection.prepareStatement("SQL"), otherConnectionVersion);
+        assertStatement(connection.prepareStatement("SQL"), otherConnectionVersion, CLUSTER_FILE);
 
         // Second run:
         // - Factory current connection: alternate connection
@@ -105,9 +104,9 @@ public class MultiServerConnectionFactoryTest {
         // - statement: alternate connection (2 statements)
         connection = classUnderTest.getNewConnection(URI.create("Blah"), 0);
         assertConnection(connection, otherConnectionVersion, List.of(otherConnectionVersion, initialConnectionVersion));
-        assertStatement(connection.prepareStatement("SQL"), otherConnectionVersion);
+        assertStatement(connection.prepareStatement("SQL"), otherConnectionVersion, CLUSTER_FILE);
         // next statement
-        assertStatement(connection.prepareStatement("SQL"), initialConnectionVersion);
+        assertStatement(connection.prepareStatement("SQL"), initialConnectionVersion, CLUSTER_FILE);
 
         // Third run:
         // - Factory current connection: initial connection
@@ -116,7 +115,7 @@ public class MultiServerConnectionFactoryTest {
         connection = classUnderTest.getNewConnection(URI.create("Blah"), 0);
         assertConnection(connection, initialConnectionVersion, List.of(initialConnectionVersion, otherConnectionVersion));
         // just one statement for this connection
-        assertStatement(connection.prepareStatement("SQL"), initialConnectionVersion);
+        assertStatement(connection.prepareStatement("SQL"), initialConnectionVersion, CLUSTER_FILE);
 
         // Fourth run:
         // - Factory current connection: alternate connection
@@ -124,10 +123,10 @@ public class MultiServerConnectionFactoryTest {
         // - statement: alternate connection (3 statements)
         connection = classUnderTest.getNewConnection(URI.create("Blah"), 0);
         assertConnection(connection, otherConnectionVersion, List.of(otherConnectionVersion, initialConnectionVersion));
-        assertStatement(connection.prepareStatement("SQL"), otherConnectionVersion);
+        assertStatement(connection.prepareStatement("SQL"), otherConnectionVersion, CLUSTER_FILE);
         // next statements
-        assertStatement(connection.prepareStatement("SQL"), initialConnectionVersion);
-        assertStatement(connection.prepareStatement("SQL"), otherConnectionVersion);
+        assertStatement(connection.prepareStatement("SQL"), initialConnectionVersion, CLUSTER_FILE);
+        assertStatement(connection.prepareStatement("SQL"), otherConnectionVersion, CLUSTER_FILE);
     }
 
     @Test
@@ -144,8 +143,58 @@ public class MultiServerConnectionFactoryTest {
                 List.of(dummyConnectionFactory(ALTERNATE_VERSION))));
     }
 
-    private void assertStatement(final RelationalPreparedStatement statement, final SemanticVersion version) throws SQLException {
-        assertEquals("version=" + version, ((RelationalConnection)statement.getConnection()).getPath().getQuery());
+    @ParameterizedTest
+    @CsvSource({"0", "1"})
+    void testDefaultPolicyWithCluster(int initialConnection) throws SQLException {
+        MultiServerConnectionFactory classUnderTest = new MultiServerConnectionFactory(
+                MultiServerConnectionFactory.ConnectionSelectionPolicy.DEFAULT,
+                initialConnection,
+                dummyMultiClusterConnectionFactory(PRIMARY_VERSION, List.of(CLUSTER_FILE, CLUSTER_FILE_1)),
+                List.of(dummyMultiClusterConnectionFactory(ALTERNATE_VERSION, List.of(CLUSTER_FILE, CLUSTER_FILE_1))));
+
+        // Cluster 0 should use the default factory and return CLUSTER_FILE
+        var connection = classUnderTest.getNewConnection(URI.create("Blah"), 0);
+        assertConnection(connection, PRIMARY_VERSION, List.of(PRIMARY_VERSION));
+        assertEquals(CLUSTER_FILE, connection.getClusterFile());
+        assertStatement(connection.prepareStatement("SQL"), PRIMARY_VERSION, CLUSTER_FILE);
+
+        // Cluster 1 should use the default factory and return CLUSTER_FILE_1
+        connection = classUnderTest.getNewConnection(URI.create("Blah"), 1);
+        assertConnection(connection, PRIMARY_VERSION, List.of(PRIMARY_VERSION));
+        assertEquals(CLUSTER_FILE_1, connection.getClusterFile());
+        assertStatement(connection.prepareStatement("SQL"), PRIMARY_VERSION, CLUSTER_FILE_1);
+    }
+
+    @ParameterizedTest
+    @CsvSource({"0", "1"})
+    void testAlternatePolicyWithCluster(int initialConnection) throws SQLException {
+        final SemanticVersion[] versions = new SemanticVersion[] { PRIMARY_VERSION, ALTERNATE_VERSION };
+        final SemanticVersion initialConnectionVersion = versions[initialConnection];
+        final SemanticVersion otherConnectionVersion = versions[(initialConnection + 1) % 2];
+
+        MultiServerConnectionFactory classUnderTest = new MultiServerConnectionFactory(
+                MultiServerConnectionFactory.ConnectionSelectionPolicy.ALTERNATE,
+                initialConnection,
+                dummyMultiClusterConnectionFactory(PRIMARY_VERSION, List.of(CLUSTER_FILE, CLUSTER_FILE_1)),
+                List.of(dummyMultiClusterConnectionFactory(ALTERNATE_VERSION, List.of(CLUSTER_FILE, CLUSTER_FILE_1))));
+
+        // Cluster 0: alternation works and cluster file is CLUSTER_FILE
+        var connection = classUnderTest.getNewConnection(URI.create("Blah"), 0);
+        assertConnection(connection, initialConnectionVersion, List.of(initialConnectionVersion, otherConnectionVersion));
+        assertEquals(CLUSTER_FILE, connection.getClusterFile());
+        assertStatement(connection.prepareStatement("SQL"), initialConnectionVersion, CLUSTER_FILE);
+        assertStatement(connection.prepareStatement("SQL"), otherConnectionVersion, CLUSTER_FILE);
+
+        // Cluster 1: alternation works and cluster file is CLUSTER_FILE_1
+        connection = classUnderTest.getNewConnection(URI.create("Blah"), 1);
+        assertConnection(connection, otherConnectionVersion, List.of(otherConnectionVersion, initialConnectionVersion));
+        assertEquals(CLUSTER_FILE_1, connection.getClusterFile());
+        assertStatement(connection.prepareStatement("SQL"), otherConnectionVersion, CLUSTER_FILE_1);
+        assertStatement(connection.prepareStatement("SQL"), initialConnectionVersion, CLUSTER_FILE_1);
+    }
+
+    private void assertStatement(final RelationalPreparedStatement statement, final SemanticVersion version, final String clusterFile) throws SQLException {
+        assertEquals("version=" + version + "&cluster=" + clusterFile, ((RelationalConnection)statement.getConnection()).getPath().getQuery());
     }
 
     private static void assertConnection(final YamlConnection connection, final SemanticVersion initialVersion, final List<SemanticVersion> expectedVersions) {
@@ -154,17 +203,29 @@ public class MultiServerConnectionFactoryTest {
     }
 
     YamlConnectionFactory dummyConnectionFactory(@Nonnull SemanticVersion version) {
+        return dummyMultiClusterConnectionFactory(version, List.of(CLUSTER_FILE));
+    }
+
+    YamlConnectionFactory dummyMultiClusterConnectionFactory(@Nonnull SemanticVersion version, @Nonnull List<String> clusterFiles) {
         return new YamlConnectionFactory() {
             @Override
             public YamlConnection getNewConnection(@Nonnull URI connectPath, int clusterIndex) throws SQLException {
-                // Add query string to connection so we can tell where it came from
-                URI newPath = URI.create(connectPath + "?version=" + version);
-                return new SimpleYamlConnection(dummyConnection(newPath), version, CLUSTER_FILE);
+                if (clusterIndex < 0 || clusterIndex >= clusterFiles.size()) {
+                    throw new SQLException("Cluster index " + clusterIndex + " not available (only " +
+                            clusterFiles.size() + " clusters configured)");
+                }
+                URI newPath = URI.create(connectPath + "?version=" + version + "&cluster=" + clusterFiles.get(clusterIndex));
+                return new SimpleYamlConnection(dummyConnection(newPath), version, clusterFiles.get(clusterIndex));
             }
 
             @Override
             public Set<SemanticVersion> getVersionsUnderTest() {
                 return Set.of(version);
+            }
+
+            @Override
+            public int getAvailableClusterCount() {
+                return clusterFiles.size();
             }
         };
     }
