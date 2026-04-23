fc507007704347149e525105790131e0396a31de
Hazem Zaghloul <h_zaghloul@apple.com>
2026-04-23 16:45:21 +0100

    Expand comment documenting the workaround for null grouping keys

--- fdb-record-layer-core/src/main/java/com/apple/foundationdb/record/query/plan/plans/RecordQueryStreamingAggregationPlan.java
+++ fdb-record-layer-core/src/main/java/com/apple/foundationdb/record/query/plan/plans/RecordQueryStreamingAggregationPlan.java
@@ -473,7 +473,12 @@ public class RecordQueryStreamingAggregationPlan extends AbstractRelationalExpre
         final var aggregateAlias = CorrelationIdentifier.uniqueId();
         // TODO: this is a temporary workaround for https://github.com/FoundationDB/fdb-record-layer/issues/3979, as
         //       older versions of fdb-record-layer didn't correctly handle a null groupingKeyValue with continuations.
-        //       This can be removed in future versions once backwards compatibility is no longer needed.
+        //       Setting the groupingKeyValue to an empty RecordConstructorValue doesn't change the semantics of the
+        //       query, as any action that needs to be taken to account for an empty grouping value, such as making sure
+        //       that a zero is returned instead of null for a COUNT aggregate query, is done before the
+        //       RecordQueryStreamingAggregationPlan is constructed by adding other expressions on top of it.
+        //       This can be removed in future versions once backwards compatibility is no longer needed, which is tracked
+        //       in https://github.com/FoundationDB/fdb-record-layer/issues/4108.
         final var nonNullGroupingKeyValue = Optional.ofNullable(groupingKeyValue).orElse(
                 RecordConstructorValue.ofColumns(ImmutableList.of()));
 
