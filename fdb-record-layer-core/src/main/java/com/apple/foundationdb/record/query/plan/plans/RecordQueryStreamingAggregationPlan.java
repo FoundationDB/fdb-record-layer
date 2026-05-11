f9d461e80e4280b199a9db74bd5379b1bb49194a
Scott Dugas <sdugas@apple.com>
2026-05-11 12:16:26 -0400

    Remove mixed-mode protection

--- fdb-record-layer-core/src/main/java/com/apple/foundationdb/record/query/plan/plans/RecordQueryStreamingAggregationPlan.java
+++ fdb-record-layer-core/src/main/java/com/apple/foundationdb/record/query/plan/plans/RecordQueryStreamingAggregationPlan.java
@@ -471,23 +471,13 @@ public class RecordQueryStreamingAggregationPlan extends AbstractRelationalExpre
                                                          @Nonnull final BiFunction<Value, Value, Value> resultValueFunction) {
         final var groupingKeyAlias = CorrelationIdentifier.uniqueId();
         final var aggregateAlias = CorrelationIdentifier.uniqueId();
-        // TODO: this is a temporary workaround for https://github.com/FoundationDB/fdb-record-layer/issues/3979, as
-        //       older versions of fdb-record-layer didn't correctly handle a null groupingKeyValue with continuations.
-        //       Setting the groupingKeyValue to an empty RecordConstructorValue doesn't change the semantics of the
-        //       query, as any action that needs to be taken to account for an empty grouping value, such as making sure
-        //       that a zero is returned instead of null for a COUNT aggregate query, is done before the
-        //       RecordQueryStreamingAggregationPlan is constructed by adding other expressions on top of it.
-        //       This can be removed in future versions once backwards compatibility is no longer needed, which is tracked
-        //       in https://github.com/FoundationDB/fdb-record-layer/issues/4108.
-        final var nonNullGroupingKeyValue = Optional.ofNullable(groupingKeyValue).orElse(
-                RecordConstructorValue.ofColumns(ImmutableList.of()));
 
         final var referencedGroupingKeyValue =
                 groupingKeyValue == null
                 ? null
                 : ObjectValue.of(groupingKeyAlias, groupingKeyValue.getResultType());
         final var referencedAggregateValue = ObjectValue.of(aggregateAlias, aggregateValue.getResultType());
-        return new RecordQueryStreamingAggregationPlan(inner, nonNullGroupingKeyValue, aggregateValue, groupingKeyAlias, aggregateAlias,
+        return new RecordQueryStreamingAggregationPlan(inner, groupingKeyValue, aggregateValue, groupingKeyAlias, aggregateAlias,
                 resultValueFunction.apply(referencedGroupingKeyValue, referencedAggregateValue), SerializationMode.TO_NEW);
     }
 
