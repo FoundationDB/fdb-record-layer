c06d3bc81dbc147270e28248c2b14e8b714cf0d4
Scott Dugas <sdugas@apple.com>
2026-06-29 17:56:28 -0400

    Decrease batch size at 800
    It looks like it was getting transaction-too-old.
    Basic hypothesis: With concurrent work fdb takes just a little bit
    longer, and we can't insert the batches of 100 at the end within 5
    seconds, so it gets transaction-too-old. The runAsync retries a few
    times, but with the other load it can't succeed. By shrinking the
    batch size when it gets a bit higher it succeeds.

--- fdb-extensions/src/test/java/com/apple/foundationdb/async/hnsw/OperationsTest.java
+++ fdb-extensions/src/test/java/com/apple/foundationdb/async/hnsw/OperationsTest.java
@@ -342,7 +342,7 @@ class OperationsTest implements BaseTest {
                 for (int i = 0; i < size;) {
                     logger.info("basicInsertBatch " + i);
                     logFile.log("primary insert loop iter=%d size=%d", i, size);
-                    i += TestHelpers.basicInsertBatch(getDb(), hnsw, 100, i,
+                    i += TestHelpers.basicInsertBatch(getDb(), hnsw, i < 800 ? 100 : 50, i,
                             (tr, nextId) -> insertedData.get(Math.toIntExact(nextId)), logFile).size();
                 }
                 logFile.log("primary insert loop complete");
